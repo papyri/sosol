735181797b5be41f2807285942a2d95fbda20c80
Ryan Baumann <ryan.baumann@gmail.com>
2008-11-10 13:16:37 -0500

    Validate against EpiDoc DTD

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,26 @@
+# require 'rexml/document'
+
 module DocumentsHelper
+	require 'xml/libxml'
+=begin
+	def valid_xml?(xml)
+		begin
+			REXML::Document.new(xml)
+		rescue REXML::ParseException
+			# Return nil if an exception is thrown
+		end
+	end
+=end
+
+	def valid_epidoc?(xml)
+		parser = XML::Parser.new
+		parser.string = xml
+		begin
+			document = parser.parse
+		rescue XML::Parser::ParseError
+			return false
+		end
+		dtd = XML::Dtd.new("-//STOA//DTD EPIDOC//EN", "/Users/ryan/source/idp2/git/protosite/app/helpers/tei-epidoc.dtd")
+		document.validate(dtd)
+	end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,14 @@
 class Document < ActiveRecord::Base
+	include DocumentsHelper
+
+	# validate :must_be_valid_xml
+	validate :must_be_valid_epidoc
+
+	def must_be_valid_xml
+		# errors.add_to_base("Content must be valid XML") unless (valid_xml?(content) != nil)
+	end
+
+	def must_be_valid_epidoc
+		errors.add_to_base("Content must be valid EpiDoc") unless valid_epidoc?(content)
+	end
 end
