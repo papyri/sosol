a35f84fb84c66d93346a65cf4dd2cfc61d92ff90
Ryan Baumann <ryan.baumann@gmail.com>
2008-11-11 11:17:25 -0500

    Migrate to events

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081110222056) do
+ActiveRecord::Schema.define(:version => 20081111161656) do
 
   create_table "documents", :force => true do |t|
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
