1455fdf29a3833f7ad17e33ef8a8f608514c5d48
Ryan Baumann <ryan.baumann@gmail.com>
2008-11-19 11:09:13 -0500

    Use local login for cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,6 @@ require 'mongrel_cluster/recipes'
 set :application, "protosite"
 set :repository,  "ssh://halsted.vis.uky.edu/srv/git/protosite.git"
 set :scm, "git"
-set :user, "idp2"
-set :scm_passphrase, "eik9aeDo"
 set :branch, "master"
 
 # If you aren't deploying to /u/apps/#{application} on the target
