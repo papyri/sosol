48c22ce25292d919b3bb52de8bb4e00c0383c6c6
Ryan Baumann <ryan.baumann@gmail.com>
2008-11-24 16:36:42 -0500

    Consistent tab style

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,27 +1,27 @@
 module DocumentsHelper
 =begin
-	def valid_xml?(xml)
-		begin
-			REXML::Document.new(xml)
-		rescue REXML::ParseException
-			# Return nil if an exception is thrown
-		end
-	end
+  def valid_xml?(xml)
+    begin
+      REXML::Document.new(xml)
+    rescue REXML::ParseException
+      # Return nil if an exception is thrown
+    end
+  end
 =end
 
-	def get_title(xml)
-		REXML::XPath.first(REXML::Document.new(xml), "/TEI.2/teiHeader/fileDesc/titleStmt/title").text
-	end
+  def get_title(xml)
+    REXML::XPath.first(REXML::Document.new(xml), "/TEI.2/teiHeader/fileDesc/titleStmt/title").text
+  end
 
-	def valid_epidoc?(xml)
-		parser = XML::Parser.new
-		parser.string = xml
-		begin
-			document = parser.parse
-		rescue XML::Parser::ParseError
-			return false
-		end
-		dtd = XML::Dtd.new("-//STOA//DTD EPIDOC//EN", "/Users/ryan/source/idp2/git/protosite/app/helpers/tei-epidoc.dtd")
-		document.validate(dtd)
-	end
+  def valid_epidoc?(xml)
+    parser = XML::Parser.new
+    parser.string = xml
+    begin
+      document = parser.parse
+    rescue XML::Parser::ParseError
+      return false
+    end
+    dtd = XML::Dtd.new("-//STOA//DTD EPIDOC//EN", "/Users/ryan/source/idp2/git/protosite/app/helpers/tei-epidoc.dtd")
+    document.validate(dtd)
+  end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,16 +1,16 @@
 class Document < ActiveRecord::Base
-	include DocumentsHelper
-	
-	has_many :events
+  include DocumentsHelper
+  
+  has_many :events
 
-	# validate :must_be_valid_xml
-	# validate :must_be_valid_epidoc
+  # validate :must_be_valid_xml
+  # validate :must_be_valid_epidoc
 
-	def must_be_valid_xml
-		# errors.add_to_base("Content must be valid XML") unless (valid_xml?(content) != nil)
-	end
+  def must_be_valid_xml
+    # errors.add_to_base("Content must be valid XML") unless (valid_xml?(content) != nil)
+  end
 
-	def must_be_valid_epidoc
-		errors.add_to_base("Content must be valid EpiDoc") unless valid_epidoc?(content)
-	end
+  def must_be_valid_epidoc
+    errors.add_to_base("Content must be valid EpiDoc") unless valid_epidoc?(content)
+  end
 end
