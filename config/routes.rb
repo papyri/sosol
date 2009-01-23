ddec1780768bfe697cd2c28a4d011def5da5f927
Ryan Baumann <ryan.baumann@gmail.com>
2009-01-23 17:00:29 -0500

    script/destroy scaffold User

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
-
   map.resource :session
 
   map.resources :documents, :member => { :editxml => :get, :preview => :get }
--- db/schema.rb
+++ db/schema.rb
@@ -24,18 +24,4 @@ ActiveRecord::Schema.define(:version => 20090130164716) do
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "name",                      :limit => 100, :default => ""
-    t.string   "email",                     :limit => 100
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_token",            :limit => 40
-    t.datetime "remember_token_expires_at"
-  end
-
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
-
 end
