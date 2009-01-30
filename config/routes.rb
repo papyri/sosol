00e0c3d7f6a50c39c64ad0b2fd11371cdad8db02
Ryan Baumann <ryan.baumann@gmail.com>
2009-01-30 10:10:06 -0500

    Remove restful_authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :events
 
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
+  # map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  # map.login '/login', :controller => 'sessions', :action => 'new'
+  # map.register '/register', :controller => 'users', :action => 'create'
+  # map.signup '/signup', :controller => 'users', :action => 'new'
   map.resource :session
 
   map.resources :documents, :member => { :editxml => :get, :preview => :get }
