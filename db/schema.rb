3634e685d55206c2a3a77e95ed44797e150997e0
Ryan Baumann <ryan.baumann@gmail.com>
2009-01-30 14:45:45 -0500

    Mysterious restful_authentication migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090123200908) do
+ActiveRecord::Schema.define(:version => 20090130164716) do
 
   create_table "documents", :force => true do |t|
     t.text     "content"
