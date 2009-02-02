a4c935b811e64d4737e56b74fe116a4c0c94d280
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-02 14:57:44 -0500

    Pull helpers from example RPX app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def rpx_signin_url
+    dest = url_for :controller => :rpx, :action => :login_return, :only_path => false
+    @rpx.signin_url(dest)
+  end
+
+  def rpx_associate_url
+    dest = url_for :controller => :rpx, :action => :associate_return, :only_path => false
+    @rpx.signin_url(dest)
+  end
+
+  def rpx_widget_url
+    BASE_URL + '/openid/v2/widget'
+  end
 end
