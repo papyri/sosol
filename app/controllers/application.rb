7ef86bdd0a06ab8806d5df384cc4a8cc7b1d0c61
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-02 15:14:28 -0500

    Pull more from RPX example

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include AuthenticatedSystem
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
@@ -13,4 +12,30 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  # Pick a unique cookie name to distinguish our session data from others'
+  session :session_key => '_sosol_session_id'
+  layout 'default'
+
+  before_filter :get_user_id
+  before_filter :rpx_setup
+
+  private
+
+  def get_user_id
+    user_id = session[:user_id]
+    if user_id
+      @current_user = User.find_by_id user_id
+    end
+    return true
+  end
+
+  def rpx_setup
+    unless Object.const_defined?(:API_KEY) && Object.const_defined?(:BASE_URL) && Object.const_defined?(:REALM)
+      render :template => 'const_message'
+      return false
+    end
+    @rpx = Rpx::RpxHelper.new(API_KEY, BASE_URL, REALM)
+    return true
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    if @current_user
+      @identifiers = @rpx.mappings(@current_user.id)
+    end
   end
 
 end
