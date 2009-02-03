123d3f34434b0c67f2ac1d83d4f0b6dbc92091ff
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-03 11:19:19 -0500

    Move RPX var into main config block

--- config/environment.rb
+++ config/environment.rb
@@ -69,7 +69,7 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+  
+  # RPX application configuration
+  BASE_URL = 'https://rpxnow.com'
 end
-
-# RPX application configuration
-BASE_URL = 'https://rpxnow.com'
