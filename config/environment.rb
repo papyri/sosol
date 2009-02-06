d34b2b02f8084051cced72fbce9e67a52a2a8d7e
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-06 10:43:55 -0500

    Initial version of git database branching

--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,11 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'git_conf'
 
 require 'rexml/document'
 
-Rails::Initializer.run do |config|
+Rails::Initializer.run(:process, GitConf.new) do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
