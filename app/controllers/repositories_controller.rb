7b83836c93d9e090f424d10a2797760b2d70acc6
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-09 17:02:14 -0500

    Add repositories controller skeleton

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,28 @@
+class RepositoriesController < ApplicationController
+  layout 'site'
+  
+  before_filter :find_repository
+
+  def index
+    redirect_to :action => 'tree'
+  end
+  
+  def tree
+    tree = @repo.tree
+    
+    @contents = []
+    tree.contents.each do |content|
+      @contents << content
+    end
+  end
+  
+  def blob
+    
+  end
+  
+  protected
+  
+    def find_repository
+      @repo = @current_user.repository.repo
+    end
+end
