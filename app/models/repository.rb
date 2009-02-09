61e9369a7e85ec4342a904714deee688b1555d51
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-09 11:24:20 -0500

    Flesh out repo interface

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -6,5 +6,17 @@ class Repository
     @location = master.name
   end
   
+  def exists?
+    master.has_repository?
+  end
   
+  def create
+    master.update_attribute :has_repository, true
+    # create a git repository
+  end
+  
+  def destroy
+    master.update_attribute :has_repository, false
+    # destroy a git repository
+  end
 end
