311b0242bcbd539c3087414bdc837fa940e30e22
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-11 12:53:36 -0500

    Move RPX_REALM into secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,6 @@ config.action_controller.perform_caching             = true
 # config.action_mailer.raise_delivery_errors = false
 
 # config/environments/production_secret.rb should set
-# RPX_API_KEY for RPX, and possibly other unversioned secrets for production
+# RPX_API_KEY and RPX_REALM (site name) for RPX,
+# and possibly other unversioned secrets for production
 require File.join(File.dirname(__FILE__), 'production_secret')
-
-RPX_REALM = 'sosol-development'
