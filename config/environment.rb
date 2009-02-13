220209dfda6a12553e863c0867555fa3688b3143
Ryan Baumann <ryan.baumann@gmail.com>
2009-02-13 11:11:56 -0500

    Remove libxml-ruby dep

--- config/environment.rb
+++ config/environment.rb
@@ -27,8 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+  # config.gem "libxml-ruby", :lib => "xml/libxml"
 	config.gem "ruby-xslt", :lib => "xml/xslt"
-	config.gem "libxml-ruby", :lib => "xml/libxml"
 	config.gem "haml"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
