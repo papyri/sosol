a1ea5491afe5256688ccd4c32fa19a865d76c2ad
Charles Carpenter <charles@agile-ubuntu.(none)>
2009-02-19 14:52:28 -0500

    Move fixture-loading migration to happen after table rename so that it can use the correct Article model class

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090218224349) do
+ActiveRecord::Schema.define(:version => 20090219194351) do
 
   create_table "articles", :force => true do |t|
     t.text     "content"
