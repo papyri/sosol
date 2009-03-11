97b613af8ee1b1a511b90858eb326a5a0339a037
Ryan Baumann <ryan.baumann@gmail.com>
2009-03-11 13:10:04 -0400

    create placeholder branches on user creation

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -17,11 +17,31 @@ class Repository
   def exists?
     master.has_repository?
   end
-  
+
   def create
     master.update_attribute :has_repository, true
     # create a git repository
     @repo ||= @canonical.fork_bare(path)
+    
+    # create some placeholder branches
+    ["p.genova/p.genova.2/p.genova.2.67.xml",
+    "sb/sb.24/sb.24.16003.xml",
+    "p.lond/p.lond.7/p.lond.7.2067.xml",
+    "p.ifao/p.ifao.2/p.ifao.2.31.xml",
+    "p.gen.2/p.gen.2.1/p.gen.2.1.4.xml",
+    "p.harr/p.harr.1/p.harr.1.109.xml",
+    "p.petr.2/p.petr.2.30.xml",
+    "sb/sb.16/sb.16.12255.xml",
+    "p.harr/p.harr.2/p.harr.2.193.xml",
+    "p.oxy/p.oxy.43/p.oxy.43.3118.xml",
+    "chr.mitt/chr.mitt.12.xml",
+    "sb/sb.12/sb.12.11001.xml",
+    "p.stras/p.stras.9/p.stras.9.816.xml",
+    "sb/sb.6/sb.6.9108.xml",
+    "p.yale/p.yale.1/p.yale.1.43.xml",
+    "p.yale/p.yale.1/p.yale.1.44.xml"].each do |fixture|
+      create_branch("DDB_EpiDoc_XML/" + fixture)
+    end
   end
   
   def destroy
