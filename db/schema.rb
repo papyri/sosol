8d0fd23d6ce43d49f9606a91f6932e5c33d0e6fb
Charles Carpenter <charles@agile-ubuntu.(none)>
2009-04-09 19:06:39 -0400

    Added decimal details to degree trigger for mysql.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090409191823) do
+ActiveRecord::Schema.define(:version => 20090409230112) do
 
   create_table "articles", :force => true do |t|
     t.text     "content"
