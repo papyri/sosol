6be608ce779202efd88736174bd20416d1eb16dc
Charles Carpenter <charles@agile-ubuntu.(none)>
2009-04-14 15:43:16 -0400

    Added ttls to mailer config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = 
 {
+	:enable_starttls_auto => true,
 	:address			=>	'smtp.gmail.com',
 	:port					=>	587,
 	:domain				=>	"www.mybit.net",
