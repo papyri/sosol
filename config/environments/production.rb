abe08b0d68d9cf9084632968862090b4760bd642
Charles Carpenter <charles@agile-ubuntu.(none)>
2009-04-14 13:55:27 -0400

    Returned to cahcing classes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,7 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-#config.cache_classes = true
-
-#to let us do debuging on halsted, make sure to return to true later
-config.cache_classes = false
+config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
