a6258437f748c00d069277f5b102c46e2c8f728d
Ryan Baumann <ryan.baumann@gmail.com>
2009-04-29 17:01:27 -0400

    post-generate migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090429183921) do
+ActiveRecord::Schema.define(:version => 20090429210048) do
 
   create_table "articles", :force => true do |t|
     t.text     "content"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(:version => 20090429183921) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "branch"
   end
 
   create_table "users", :force => true do |t|
