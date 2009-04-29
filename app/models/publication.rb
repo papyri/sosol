1fc54289f15972da955d8f042061f2ab9db8c290
Ryan Baumann <ryan.baumann@gmail.com>
2009-04-29 15:19:55 -0400

    Create branch from publication creation

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -5,4 +5,8 @@ class Publication < ActiveRecord::Base
   has_and_belongs_to_many :identifiers
   
   validates_uniqueness_of :title, :scope => 'user_id'
+  
+  def after_create
+    user.repository.create_branch(title)
+  end
 end
