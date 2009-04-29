6ece1c36b398034cf9bce4215563893e13332321
Ryan Baumann <ryan.baumann@gmail.com>
2009-04-29 14:50:54 -0400

    More constraints for publication and identifier models

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -1,6 +1,10 @@
 class Identifier < ActiveRecord::Base
+  validates_presence_of :name, :type
+  
   has_and_belongs_to_many :publications
   
+  # identifiers of the same type must be unique
+  validates_uniqueness_of :name, :scope => "type"
   validates_inclusion_of :type,
                          :in => %w{ DDB }
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,6 @@
 class Publication < ActiveRecord::Base
+  validates_presence_of :title
+  
   belongs_to :user
   has_and_belongs_to_many :identifiers
 end
