9bc430cbc56376831454377a285c89c7671a2dbd
Ryan Baumann <ryan.baumann@gmail.com>
2009-04-30 14:26:28 -0400

    Dummy in collection mapping.

--- app/models/ddb_identifier.rb
+++ app/models/ddb_identifier.rb
@@ -1,5 +1,7 @@
 class DDBIdentifier < Identifier
   DDB_PATH_PREFIX = 'DDB_EpiDoc_XML'
+  COLLECTION_XML_PATH = 'DDB_SGML/collection.xml'
+  
   
   def to_path
     path_components = [ DDB_PATH_PREFIX ]
@@ -9,6 +11,8 @@ class DDBIdentifier < Identifier
     ddb_volume_number = components[1].to_s
     ddb_document_number = components[2].to_s
     
+    ddb_collection_name = ddb_series_to_collection(ddb_series_number)
+    
     ddb_volume_path = ddb_series_number + '.' + ddb_document_number
     ddb_xml_path = [ddb_series_number, ddb_volume_number, ddb_document_number,
                     'xml'].join('.')
@@ -16,5 +20,11 @@ class DDBIdentifier < Identifier
     path_components << ddb_series_number << ddb_volume_path << ddb_xml_path
     
     return path_components.join('/')
+  end
+  
+  # map DDB series number to DDB collection name using collection.xml
+  def ddb_series_to_collection(ddb_series_number)
+    collection_xml = Repository.new.get_file_from_branch(
+                      COLLECTION_XML_PATH, 'master')
   end
 end
