f92b7fe6b4affcdaa6dfeebf82cb39dd1c959320
Ryan Baumann <ryan.baumann@gmail.com>
2009-04-30 14:45:01 -0400

    Fill in collection mapping, use collection to construct correct path

--- app/models/ddb_identifier.rb
+++ app/models/ddb_identifier.rb
@@ -2,7 +2,6 @@ class DDBIdentifier < Identifier
   DDB_PATH_PREFIX = 'DDB_EpiDoc_XML'
   COLLECTION_XML_PATH = 'DDB_SGML/collection.xml'
   
-  
   def to_path
     path_components = [ DDB_PATH_PREFIX ]
     trimmed_name = name.sub(/^oai:papyri.info:identifiers:ddbdp:/, '')
@@ -11,13 +10,15 @@ class DDBIdentifier < Identifier
     ddb_volume_number = components[1].to_s
     ddb_document_number = components[2].to_s
     
-    ddb_collection_name = ddb_series_to_collection(ddb_series_number)
+    ddb_collection_name = ddb_series_to_collection(ddb_series_number).to_s
     
-    ddb_volume_path = ddb_series_number + '.' + ddb_document_number
-    ddb_xml_path = [ddb_series_number, ddb_volume_number, ddb_document_number,
+    ddb_volume_path = ddb_collection_name + '.' + ddb_document_number
+    ddb_xml_path = [ddb_collection_name,
+                    ddb_volume_number,
+                    ddb_document_number,
                     'xml'].join('.')
     
-    path_components << ddb_series_number << ddb_volume_path << ddb_xml_path
+    path_components << ddb_collection_name << ddb_volume_path << ddb_xml_path
     
     return path_components.join('/')
   end
@@ -26,5 +27,7 @@ class DDBIdentifier < Identifier
   def ddb_series_to_collection(ddb_series_number)
     collection_xml = Repository.new.get_file_from_branch(
                       COLLECTION_XML_PATH, 'master')
+    xpath_result = REXML::XPath.first(REXML::Document.new(collection_xml),
+      "/rdf:RDF/rdf:Description[@rdf:about = 'Perseus:text:1999.05.#{ddb_series_number}']/text[1]/text()")
   end
 end
