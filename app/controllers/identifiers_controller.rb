4f27315254a9d836f20c0215d7ea38c31858ea74
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-01 16:07:45 -0400

    Try to handle any identifier class logically inside IdentifiersController#method_missing

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -1,10 +1,6 @@
 class IdentifiersController < ApplicationController
   def method_missing(method_name, *args)
     identifier = Identifier.find(params[:id])
-    if(identifier.class == DDBIdentifier)
-      redirect_to :controller => :ddb_identifiers, :action => method_name
-    else
-      redirect_to :controller => :welcome, :action => "index"
-    end
+    redirect_to :controller => identifier.class.to_s.pluralize.underscore, :action => method_name
   end
 end
