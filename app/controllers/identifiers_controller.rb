a68a968b42db57409e6cf03274a37b391cd76371
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-01 11:56:39 -0400

    Stub in having parent Identifiers controller redirect unknown methods to appropriate subclass

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -1,2 +1,10 @@
 class IdentifiersController < ApplicationController
+  def method_missing(method_name, *args)
+    identifier = Identifier.find(params[:id])
+    if(identifier.class == DDBIdentifier)
+      redirect_to :controller => :ddb_identifiers, :action => method_name
+    else
+      redirect_to :controller => :welcome, :action => "index"
+    end
+  end
 end
