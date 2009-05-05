9383a42268a4189dfef1d0fb648042ad5179a1b1
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-05 15:13:14 -0400

    Remove get_abs_from_edition from DdbIdentifiersHelper, now pulled from acts_as_leiden_plus

--- app/helpers/ddb_identifiers_helper.rb
+++ app/helpers/ddb_identifiers_helper.rb
@@ -1,11 +1,2 @@
 module DdbIdentifiersHelper
-  def get_abs_from_edition_div(xml)
-    processed = ''
-    REXML::XPath.each(REXML::Document.new(xml), '/TEI.2/text/body/div[@type = "edition"]/ab') do |ab|
-      ab.to_s.each_line do |line|
-        processed += line.chomp.strip
-      end
-    end
-    processed
-  end
 end
