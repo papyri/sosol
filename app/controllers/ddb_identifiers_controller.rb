72b5a481cbfdc7a62f58a8effd870e39a4772f3c
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-07 15:06:26 -0400

    Switch to setting on hash (AR []=) which will work better with to_xml

--- app/controllers/ddb_identifiers_controller.rb
+++ app/controllers/ddb_identifiers_controller.rb
@@ -4,20 +4,21 @@ class DdbIdentifiersController < ApplicationController
   # GET /publications/1/ddb_identifiers/1/edit
   def edit
     editxml
-    @leiden_plus = @identifier.leiden_plus
+    @identifier[:leiden_plus] = @identifier.leiden_plus
   end
   
   # GET /publications/1/ddb_identifiers/1/editxml
   def editxml
     find_identifier
-    @xml_content = @identifier.xml_content
+    @identifier[:xml_content] = @identifier.xml_content
   end
   
   # PUT /publications/1/ddb_identifiers/1/update
   def update
     find_identifier
     # transform back to XML
-    xml_content = @identifier.leiden_plus_to_xml(params[:leiden_plus])
+    xml_content = @identifier.leiden_plus_to_xml(
+      params[:ddb_identifier][:leiden_plus])
     # commit xml to repo
     @identifier.set_xml_content(xml_content,
                                 params[:comment])
@@ -29,7 +30,7 @@ class DdbIdentifiersController < ApplicationController
   def updatexml
     find_identifier
     # strip carriage returns
-    xml_content = params[:xml_content].gsub(/\r\n?/, "\n")
+    xml_content = params[:ddb_identifier][:xml_content].gsub(/\r\n?/, "\n")
     @identifier.set_xml_content(xml_content,
                                 params[:comment])
     redirect_to polymorphic_path([@identifier.publication, @identifier],
@@ -39,7 +40,7 @@ class DdbIdentifiersController < ApplicationController
   # GET /publications/1/ddb_identifiers/1/history
   def history
     find_identifier
-    @commits = 
+    @identifier[:commits] = 
       @identifier.publication.user.repository.get_log_for_file_from_branch(
         @identifier.to_path, @identifier.publication.branch
     )
@@ -51,10 +52,10 @@ class DdbIdentifiersController < ApplicationController
     
     Dir.chdir(File.join(RAILS_ROOT, 'data/xslt/'))
     xslt = XML::XSLT.new()
-    xslt.xml = REXML::Document.new(@xml_content)
+    xslt.xml = REXML::Document.new(@identifier[:xml_content])
     xslt.xsl = REXML::Document.new File.open('start-div-portlet.xsl')
     
-    @transformed = xslt.serve()
+    @identifier[:html_preview] = xslt.serve()
   end
   
   protected
