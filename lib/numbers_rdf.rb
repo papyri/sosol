bf852a180d9ac3c6a62cf8c03ae577f599463534
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-12 10:07:03 -0400

    Sketch in Numbers Server helper

--- lib/numbers_rdf.rb
+++ lib/numbers_rdf.rb
@@ -1,21 +1,56 @@
+require 'net/http'
+
 module NumbersRDF
-  NUMBERS_SERVER_BASE_URL = 'http://apptest.cul.columbia.edu:8082/numbers/'
+  NUMBERS_SERVER_DOMAIN = 'apptest.cul.columbia.edu'
+  NUMBERS_SERVER_PORT = 8082
+  NUMBERS_SERVER_BASE_PATH = '/numbers'
   # OAI identifiers should be in the form scheme ":" namespace-identifier ":" local-identifier
   OAI_SCHEME = 'oai'
   OAI_NAMESPACE_IDENTIFIER = 'papyri.info'
   OAI_IDENTIFIER_PREFIX = "#{OAI_SCHEME}:#{OAI_NAMESPACE_IDENTIFIER}:"
   
-  def identifier_to_local_identifier(identifier)
-    identifier.split(OAI_IDENTIFIER_PREFIX)[1]
-  end
-  
-  def numbers_identifier_to_url(identifier)
-    local_identifier = identifier_to_local_identifier(identifier)
-    url_paths = local_identifier.split(':')
-    return NUMBERS_SERVER_BASE_URL + url_paths.join('/')
-  end
-  
-  class NumbersHelper
+  module NumbersHelper
+    def identifier_to_local_identifier(identifier)
+      identifier.split(OAI_IDENTIFIER_PREFIX)[1]
+    end
+
+    def identifier_to_path(identifier)
+      local_identifier = identifier_to_local_identifier(identifier)
+      url_paths = [NUMBERS_SERVER_BASE_PATH] + local_identifier.split(':')
+      return url_paths.join('/')
+    end
+
+    def identifier_to_url(identifier)
+      return 'http://' + NUMBERS_SERVER_DOMAIN + ':' + 
+              NUMBERS_SERVER_PORT + identifier_to_path(identifier)
+    end
     
+    def identifier_to_numbers_server_response(identifier)
+      path = identifier_to_path(identifier)
+      response = Net::HTTP.get_response(NUMBERS_SERVER_DOMAIN, path,
+                                        NUMBERS_SERVER_PORT)
+    end
+    
+    def identifier_to_identifiers(identifier)
+      response = identifier_to_numbers_server_response(identifier)
+
+      if response.code != '200'
+        return nil
+      else
+        return process_numbers_server_response_body(response.body)
+      end
+    end
+    
+    def process_numbers_server_response_body(rdf_xml)
+      puts rdf_xml
+      doc = REXML::Document.new(rdf_xml)
+      identifiers = []
+      ore_describes_path = "/rdf:RDF/rdf:Description/ore:aggregates/rdf:Description/ore:describes"
+      REXML::XPath.each(doc, ore_describes_path) do |ore_describes|
+        identifiers << ore_describes.attributes['rdf:resource']
+      end
+      
+      return identifiers
+    end
   end
 end
