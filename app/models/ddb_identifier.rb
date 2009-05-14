c92add2bdcdfcea898704d9b6ccb15538607d2a7
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-14 16:22:28 -0400

    Use roman numbers for volume number

--- app/models/ddb_identifier.rb
+++ app/models/ddb_identifier.rb
@@ -4,6 +4,20 @@ class DDBIdentifier < Identifier
   DDB_PATH_PREFIX = 'DDB_EpiDoc_XML'
   COLLECTION_XML_PATH = 'DDB_SGML/collection.xml'
   
+  ROMAN_MAP = { 1 => "I",
+                4 => "IV",
+                5 => "V",
+                9 => "IX",
+                10 => "X",
+                40 => "XL",
+                50 => "L",
+                90 => "XC",
+                100 => "C",
+                400 => "CD",
+                500 => "D",
+                900 => "CM",
+                1000 => "M" }
+  
   acts_as_leiden_plus
   
   def to_components
@@ -16,12 +30,20 @@ class DDBIdentifier < Identifier
     return [ddb_series_number, ddb_volume_number, ddb_document_number]
   end
   
+  def to_roman(arabic)
+    # shamelessly stolen from http://rubyquiz.com/quiz22.html
+    ROMAN_MAP.keys.sort { |a, b| b <=> a }.inject("") do |roman, div|
+      times, arabic = arabic.divmod(div)
+      roman << ROMAN_MAP[div] * times
+    end
+  end
+  
   def titleize
     ddb_series_number, ddb_volume_number, ddb_document_number =
       to_components
     ddb_collection_name = ddb_series_to_human_collection(ddb_series_number)
     title = 
-      [ddb_collection_name, ddb_volume_number, ddb_document_number].join(' ')
+      [ddb_collection_name, to_roman(ddb_volume_number.to_i), ddb_document_number].join(' ')
   end
   
   def to_path
