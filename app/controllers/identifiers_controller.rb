1a9c7daecdbc7553b8078427f0463f23026d6a0c
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-15 12:04:55 -0400

    Kill method_missing hack which obviates need for view inheritance hack

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -1,13 +1,14 @@
 class IdentifiersController < ApplicationController
-  def method_missing(method_name, *args)
-    identifier = Identifier.find(params[:id])
-    redirect_to :controller => identifier.class.to_s.pluralize.underscore, :action => method_name
-  end
+  # def method_missing(method_name, *args)
+  #   identifier = Identifier.find(params[:id])
+  #   redirect_to :controller => identifier.class.to_s.pluralize.underscore, :action => method_name
+  # end
   
   # GET /publications/1/xxx_identifiers/1/editxml
   def editxml
     find_identifier
     @identifier[:xml_content] = @identifier.xml_content
+    render :template => 'identifiers/editxml'
   end
   
   # GET /publications/1/xxx_identifiers/1/history
