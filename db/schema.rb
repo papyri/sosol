929caeba164382733b59d9779ae03d3493b14679
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-27 15:20:21 -0400

    rake db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,8 @@ ActiveRecord::Schema.define(:version => 20090514210236) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "owner_id"
+    t.string   "owner_type"
     t.string   "branch"
   end
 
