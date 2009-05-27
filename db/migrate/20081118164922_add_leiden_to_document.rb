2d47adb5ae13da7ddcb587efa47a0d82f33c6bb2
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-27 13:32:14 -0400

    g rm db/migrate/*document*

--- db/migrate/20081118164922_add_leiden_to_document.rb
+++
@@ -1,9 +0,0 @@
-class AddLeidenToDocument < ActiveRecord::Migration
-  def self.up
-    add_column :documents, :leiden, :text
-  end
-
-  def self.down
-    remove_column :documents, :leiden
-  end
-end
--- db/migrate/20090218224349_rename_document.rb
+++
@@ -1,9 +0,0 @@
-class RenameDocument < ActiveRecord::Migration
-  def self.up
-	rename_table :documents, :articles
-  end
-
-  def self.down
-	rename_table :articles, :documents
-  end
-end
