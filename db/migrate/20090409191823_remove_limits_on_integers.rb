2389694b9e5afa32996df25785239dbf7eee5735
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-27 13:36:54 -0400

    Remove changes to nuked models

--- db/migrate/20090409191823_remove_limits_on_integers.rb
+++ db/migrate/20090409191823_remove_limits_on_integers.rb
@@ -3,18 +3,6 @@ class RemoveLimitsOnIntegers < ActiveRecord::Migration
 	
 	change_column :boards_users, :board_id, :integer, :limit => nil
 
-	change_column :comments, :text, :text, :limit => nil
-
-	change_column :master_articles, :user_id, :integer, :limit => nil
-	change_column :master_articles, :comment_id, :integer, :limit => nil
-
-	change_column :transcriptions, :content, :text, :limit => nil
-	change_column :transcriptions, :leiden, :text, :limit => nil
-
-	change_column :translation_contents, :content, :text, :limit => nil
-	
-	change_column :translations, :epidoc, :text, :limit => nil
-
 	change_column :votes, :user_id, :integer, :limit => nil
 
   end
@@ -22,18 +10,6 @@ class RemoveLimitsOnIntegers < ActiveRecord::Migration
   def self.down
 
 	change_column :boards_users, :board_id, :integer, :limit => 255
-
-	change_column :comments, :text, :text, :limit => 255
-
-	change_column :master_articles, :user_id, :integer, :limit => 255
-	change_column :master_articles, :comment_id, :integer, :limit => 255
-
-	change_column :transcriptions, :content, :text, :limit => 255
-	change_column :transcriptions, :leiden, :text, :limit => 255
-
-	change_column :translation_contents, :content, :text, :limit => 255
-	
-	change_column :translations, :epidoc, :text, :limit => 255
 
 	change_column :votes, :user_id, :integer, :limit => 255
 
