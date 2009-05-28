504860986f115673d9877e396ce56aba4b16aaeb
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-28 13:24:46 -0400

    Add board to new relations, clean out old relations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,12 +1,10 @@
 class Board < ActiveRecord::Base
-  # has_many :articles
-  has_many :users
   has_many :decrees
   has_many :emailers
 
-  #has_many :users
-  #belongs_to :user
   has_and_belongs_to_many :users
+  has_many :publications, :as => :owner
+  has_many :events, :as => :owner
 
   def tally_votes(votes)
     #work in progress
