d6abe4e7f68b50f4dbdc726ab79d96401c5c1a2c
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-28 15:08:10 -0400

    Cleanup commented out relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :name
   
-  # has_many :articles
-  # has_many :comments
-  
-  #has_many :boards
-  #belongs_to :board
   has_and_belongs_to_many :boards
   
   has_and_belongs_to_many :emailers
