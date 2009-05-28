9e5eddbc17cd34b24ddfac9380606566af0e016d
Ryan Baumann <ryan.baumann@gmail.com>
2009-05-28 14:23:25 -0400

    Don't restrict category for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :owner, :polymorphic => true
   belongs_to :target, :polymorphic => true
   
-  validates_inclusion_of :category,
-    :in => %w{ commit submit comment created }
+  # validates_inclusion_of :category,
+  #   :in => %w{ commit submit comment created }
 end
