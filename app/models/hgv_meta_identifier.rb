1b077be7ebac5473e6a6abbf5858a23922075a23
Ryan Baumann <ryan.baumann@gmail.com>
2009-06-20 14:05:55 -0400

    Add HGVMeta titleize, move roman numerals over from DDB

--- app/models/hgv_meta_identifier.rb
+++ app/models/hgv_meta_identifier.rb
@@ -1,5 +1,19 @@
 class HGVMetaIdentifier < Identifier
   HGV_META_PATH_PREFIX = 'HGV_meta_EpiDoc'
+  
+  ROMAN_MAP = { 1 => "I",
+                4 => "IV",
+                5 => "V",
+                9 => "IX",
+                10 => "X",
+                40 => "XL",
+                50 => "L",
+                90 => "XC",
+                100 => "C",
+                400 => "CD",
+                500 => "D",
+                900 => "CM",
+                1000 => "M" }
 
   def to_path
     path_components = [ HGV_META_PATH_PREFIX ]
@@ -15,6 +29,24 @@ class HGVMetaIdentifier < Identifier
 
     # e.g. HGV_meta_EpiDoc/HGV3/2302zzr.xml
     return File.join(path_components)
+  end
+  
+  def to_roman(arabic)
+    # shamelessly stolen from http://rubyquiz.com/quiz22.html
+    ROMAN_MAP.keys.sort { |a, b| b <=> a }.inject("") do |roman, div|
+      times, arabic = arabic.divmod(div)
+      roman << ROMAN_MAP[div] * times
+    end
+  end
+  
+  def titleize
+    trimmed_name = name.sub(/^oai:papyri.info:identifiers:hgv:/, '')
+    components = trimmed_name.split(':')
+    hgv_collection_name = components[0].to_s
+    hgv_volume_number = components[1].to_s
+    hgv_document_number = components[2].to_s
+    
+    [hgv_collection_name, to_roman(hgv_volume_number.to_i), hgv_document_number].join(' ')
   end
   
   def valid_epidoc_attributes
