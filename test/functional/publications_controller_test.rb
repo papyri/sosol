ff865ace8b4415af9fa3b3ce6692e809db6a582f
Ryan Baumann <ryan.baumann@gmail.com>
2009-08-17 13:32:43 -0400

    Update tests for publications

--- test/functional/publications_controller_test.rb
+++ test/functional/publications_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class PublicationsControllerTest < ActionController::TestCase
+  def setup
+    @user = Factory(:user)
+    @request.session[:user_id] = @user.id
+  end
+  
+  def teardown
+    @request.session[:user_id] = nil
+    @user.destroy
+  end
+  
   def test_should_get_index
     get :index
     assert_response :success
