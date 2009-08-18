df720e01d441bea43cf0786728e749afadacef46
Ryan Baumann <ryan.baumann@gmail.com>
2009-08-18 14:26:33 -0400

    Use existing HGV meta path prefix for tests

--- test/unit/hgv_meta_identifier_test.rb
+++ test/unit/hgv_meta_identifier_test.rb
@@ -4,23 +4,23 @@ class HGVMetaIdentifierTest < ActiveSupport::TestCase
   context "identifier mapping" do
     should "map the first identifier" do
       hgv1 = Factory.build(:HGVMetaIdentifier, :alternate_name => 'hgv1')
-      assert_equal "HGV_meta_EpiDoc/HGV1/1.xml", hgv1.to_path
+      assert_equal "#{HGVMetaIdentifier::PATH_PREFIX}/HGV1/1.xml", hgv1.to_path
     end
     
     should "map identifier subfolder edge cases to the correct subfolder" do
       hgv2000 = Factory.build(:HGVMetaIdentifier, :alternate_name => 'hgv2000')
-      assert_equal "HGV_meta_EpiDoc/HGV2/2000.xml", hgv2000.to_path
+      assert_equal "#{HGVMetaIdentifier::PATH_PREFIX}/HGV2/2000.xml", hgv2000.to_path
       
       hgv2001 = Factory.build(:HGVMetaIdentifier, :alternate_name => 'hgv2001')
-      assert_equal "HGV_meta_EpiDoc/HGV3/2001.xml", hgv2001.to_path
+      assert_equal "#{HGVMetaIdentifier::PATH_PREFIX}/HGV3/2001.xml", hgv2001.to_path
     end
     
     should "map identifiers with trailing characters" do
       hgv114252a = Factory.build(:HGVMetaIdentifier, :alternate_name => 'hgv114252a')
-      assert_equal "HGV_meta_EpiDoc/HGV115/114252a.xml", hgv114252a.to_path
+      assert_equal "#{HGVMetaIdentifier::PATH_PREFIX}/HGV115/114252a.xml", hgv114252a.to_path
       
       hgv23403zzr = Factory.build(:HGVMetaIdentifier, :alternate_name => 'hgv23403zzr')
-      assert_equal "HGV_meta_EpiDoc/HGV24/23403zzr.xml", hgv23403zzr.to_path
+      assert_equal "#{HGVMetaIdentifier::PATH_PREFIX}/HGV24/23403zzr.xml", hgv23403zzr.to_path
     end
   end
 end
