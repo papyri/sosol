3a482ec070bfbef52c3dc4f497a63a5553f37a7c
Ryan Baumann <ryan.baumann@gmail.com>
2009-08-18 14:50:40 -0400

    Add DDB test for ambiguous collections (p.petr/2 vs. p.petr.2)

--- test/unit/ddb_identifier_test.rb
+++ test/unit/ddb_identifier_test.rb
@@ -10,5 +10,13 @@ class DDBIdentifierTest < ActiveSupport::TestCase
       bgu_1_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0001:1:1")
       assert_path_equal %w{bgu bgu.1 bgu.1.1.xml}, bgu_1_1.to_path
     end
+    
+    should "map ambiguous collections" do
+      bgu_ppetr_2_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0187:2:1")
+      assert_path_equal %w{p.petr p.petr.2 p.petr.2.1.xml}, bgu_ppetr_2_1.to_path
+      
+      bgu_ppetr2_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0186::1")
+      assert_path_equal %w{p.petr.2 p.petr.2.1.xml}, bgu_ppetr2_1.to_path
+    end
   end
 end
