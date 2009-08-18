c5c7ca1a7e846299336cb67764d4f77e02860e6e
Ryan Baumann <ryan.baumann@gmail.com>
2009-08-18 14:59:52 -0400

    Add test for DDB files with "+" in the name

--- test/unit/ddb_identifier_test.rb
+++ test/unit/ddb_identifier_test.rb
@@ -18,5 +18,10 @@ class DDBIdentifierTest < ActiveSupport::TestCase
       bgu_ppetr2_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0186::1")
       assert_path_equal %w{p.petr.2 p.petr.2.1.xml}, bgu_ppetr2_1.to_path
     end
+    
+    should "map files with '+' in the name" do
+      chla_5_299FrA_B_C = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0279:5:299FrA+B+C")
+      assert_path_equal %w{chla chla.5 chla.5.299FrA+B+C.xml}, chla_5_299FrA_B_C.to_path
+    end
   end
 end
