c5470f15668c6b9d10f906064205adcabb187d60
Ryan Baumann <ryan.baumann@gmail.com>
2009-09-16 14:39:14 -0400

    Add test for master tip update during branch creation

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
-  context "The canonical Repository" do
+  context "the canonical Repository" do
     setup do
       @repo = Repository.new
     end
@@ -20,6 +20,38 @@ class RepositoryTest < ActiveSupport::TestCase
       assert_equal CANONICAL_REPOSITORY, @repo.path
     end
   
+  end
+  
+  context "a User Repository" do
+    setup do
+      @user = Factory(:user)
+    end
+    
+    teardown do
+      @user.destroy
+    end
+    
+    should "update master tip before branch creation" do
+      # rewind the head first
+      first_canon_commit = Repository.new.repo.log().last
+      original_master = @user.repository.repo.get_head('master').commit
+      @user.repository.repo.update_ref('master', first_canon_commit.id)
+      # verify that we rewound the head
+      assert_equal @user.repository.repo.get_head('master').commit.id,
+        first_canon_commit.id
+      assert_not_equal @user.repository.repo.get_head('master').commit.id,
+        original_master.id
+      
+      # create a branch
+      @user.repository.create_branch('test')
+      # verify that the master head was updated and used for the new branch
+      assert_not_equal @user.repository.repo.get_head('master').commit.id,
+        first_canon_commit.id
+      assert_equal @user.repository.repo.get_head('master').commit.id,
+        Repository.new.repo.get_head('master').commit.id
+      assert_equal @user.repository.repo.get_head('test').commit.id,
+        Repository.new.repo.get_head('master').commit.id
+    end
   end
   
 end
