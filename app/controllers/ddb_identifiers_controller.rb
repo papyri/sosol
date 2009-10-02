56c5f15097a05b5184888f4ed3ba38fee1869c40
Ryan Baumann <ryan.baumann@gmail.com>
2009-10-02 15:18:13 -0400

    Initial pass at Saxon XSLT

--- app/controllers/ddb_identifiers_controller.rb
+++ app/controllers/ddb_identifiers_controller.rb
@@ -28,7 +28,18 @@ class DdbIdentifiersController < IdentifiersController
     # xslt.xsl = REXML::Document.new File.open('start-div-portlet.xsl')
     # xslt.serve()
     
-    @identifier[:html_preview] = ""
+    java.lang.System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl")
+    transformer_factory = javax.xml.transform.TransformerFactory.newInstance()
+    transformer = transformer_factory.newTransformer(javax.xml.transform.stream.StreamSource.new(File.join(RAILS_ROOT, 'data', 'xslt', 'start-div-portlet.xsl')))
+    
+    string_writer = java.io.StringWriter.new()
+    result = javax.xml.transform.stream.StreamResult.new(string_writer)
+    xml_source = javax.xml.transform.stream.StreamSource.new(
+      java.io.StringReader.new(@identifier.xml_content))
+    
+    transformer.transform(xml_source, result)
+    
+    @identifier[:html_preview] = string_writer.toString()
   end
   
   protected
