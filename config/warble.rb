f3974f0908523df5edf481dcd7873eff289b421e
Ryan Baumann <ryan.baumann@gmail.com>
2009-10-13 16:15:49 -0400

    Add production sqlite, git db, and data to warbler config

--- config/warble.rb
+++ config/warble.rb
@@ -11,6 +11,7 @@ Warbler::Config.new do |config|
 
   # Additional files/directories to include, above those in config.dirs
   # config.includes = FileList["db"]
+  config.includes = FileList["db/production.sqlite3", "db/git", "data"]
 
   # Additional files/directories to exclude
   # config.excludes = FileList["lib/tasks/*"]
