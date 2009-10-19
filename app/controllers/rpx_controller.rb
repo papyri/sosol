f7e758f704e33bd0ebdc9cf79e84f38cccb8ce06
Ryan Baumann <ryan.baumann@gmail.com>
2009-10-19 15:35:43 -0400

    Update RPX code for multiple OpenIDs with local mapping

--- app/controllers/rpx_controller.rb
+++ app/controllers/rpx_controller.rb
@@ -4,12 +4,29 @@ class RpxController < ApplicationController
   layout 'site'
   
   def remove_openid
-    identifier = params[:openid]
-
-    @rpx.unmap identifier, @current_user.id
-    flash[:notice] = "OpenID #{identifier} removed"
-
-    redirect_to :controller => :welcome, :action => "index"
+    user_identifier = UserIdentifier.find_by_id(params[:openid])
+    if user_identifier.nil?
+      flash[:error] = "OpenID Disassociation Failed: No such OpenID"
+      redirect_to :controller => "user", :action => "account"
+      return
+    end
+    
+    user = user_identifier.user
+    
+    if user.id == @current_user.id
+      if user.user_identifiers.length > 1
+        flash[:notice] = "OpenID #{user_identifier.identifier} removed"
+        user_identifier.destroy
+        redirect_to :controller => "user", :action => "account"
+      else
+        flash[:error] = "OpenID Disassociation Failed: Your account must have at least one OpenID"
+        redirect_to :controller => "user", :action => "account"
+      end
+    else
+      flash[:error] = "OpenID Disassociation Failed: ID not owned by user"
+      redirect_to :controller => "user", :action => "account"
+      return
+    end
   end
 
   def associate_return
@@ -28,25 +45,27 @@ class RpxController < ApplicationController
     data = @rpx.auth_info(params[:token], request.url)
 
     identifier = data["identifier"]
-    primary_key = data["primaryKey"]
+    user_identifier = UserIdentifier.find_by_identifier(identifier)
 
-    if primary_key.nil?
+    if user_identifier.nil?
       if @current_user.nil?
         flash[:notice] = "you are not signed in"
       else
-        @rpx.map identifier, @current_user.id
+        @current_user.user_identifiers << UserIdentifier.create(:identifier => identifier)
         flash[:notice] = "#{identifier} added to your account"
       end
-      redirect_to :controller => "welcome", :action => "index"
+      redirect_to :controller => "user", :action => "account"
     else
-      if @current_user.id == primary_key.to_i
+      if @current_user.id == user_identifier.user.id
         flash[:notice] = "That OpenID was already associated with this account"
-        redirect_to :controller => "welcome", :action => "index"
+        redirect_to :controller => "user", :action => "account"
       else
         # The OpenID was already associated with a different user account.
-        @page_title = "Replace OpenID Account"
-        session[:identifier] = identifier
-        @other_user = User.find_by_id primary_key
+        # @page_title = "Replace OpenID Account"
+        # session[:identifier] = identifier
+        # @other_user = User.find_by_id primary_key
+        flash[:error] = "OpenID #{identifier} is already associated with a different user account."
+        redirect_to :controller => "user", :action => "account"
       end
     end
   end
@@ -114,7 +133,7 @@ class RpxController < ApplicationController
       # be sure to recover from it and roll back any changes made up
       # to this point.  Otherwise, the user account will have been
       # created with no identifier associated with it.
-      user.user_identifiers.push(UserIdentifier.create(:identifier => identifier))
+      user.user_identifiers << UserIdentifier.create(:identifier => identifier)
       user.save!
     rescue Exception => e
       user.destroy
