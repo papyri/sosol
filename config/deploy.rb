c257daf82b8f5d78b4fd7220660345b8521f8639
Ryan Baumann <ryan.baumann@gmail.com>
2009-10-26 14:26:54 -0400

    Use release path for deploy start, shared path for PID

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,12 +35,12 @@ namespace :deploy do
   
   desc "Starts a server running Glassfish Gem"
   task :start, :roles => :app do
-    run "CLASSPATH='#{current_path}/lib/java/saxon9he.jar' #{jruby_location}bin/jruby -S glassfish --contextroot #{context_root} --port #{gf_port} --environment #{environment} --runtimes #{jruby_runtimes} --runtimes-min #{jruby_min_runtimes} --runtimes-max #{jruby_max_runtimes} -P #{current_path}/capistrano-#{application} --daemon #{current_path}"
+    run "CLASSPATH='#{release_path}/lib/java/saxon9he.jar' #{jruby_location}bin/jruby -S glassfish --contextroot #{context_root} --port #{gf_port} --environment #{environment} --runtimes #{jruby_runtimes} --runtimes-min #{jruby_min_runtimes} --runtimes-max #{jruby_max_runtimes} -P #{shared_path}/capistrano-#{application} --daemon #{release_path}"
   end
 
   desc "Stop a server running Glassfish Gem"
   task :stop, :roles => :app do
-    run "kill -INT $(cat #{current_path}/capistrano-#{application})"  
+    run "kill -INT $(cat #{shared_path}/capistrano-#{application})"  
   end
 
   desc "Restart Application"
