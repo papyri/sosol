b55d988b0b5567f9b53ee8df460f0cd7955bc87e
Ryan Baumann <ryan.baumann@gmail.com>
2009-10-29 14:58:30 -0400

    Revert to 2f0255f58d741f6bad1638da2d9cf4b1b0237687 (delete new files)

--- db/migrate/20091026160035_add_publication_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddPublicationToComments < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :publication_id, :string
-  end
-
-  def self.down
-    remove_column :comments, :publication_id, :string
-  end
-end
