800afb3f8f63abb6fdd74a6f70fd77fd5ed09c83
Ryan Baumann <ryan.baumann@gmail.com>
2009-12-02 11:31:41 -0500

    Add unit tests for Publication#copy_to_owner

--- test/unit/publication_test.rb
+++ test/unit/publication_test.rb
@@ -22,4 +22,37 @@ class PublicationTest < ActiveSupport::TestCase
       end
     end
   end
+  
+  context "a publication copied to another owner" do
+    setup do
+      @original_owner = Factory(:user)
+      @new_owner = Factory(:user)
+      @publication = Publication.new_from_templates(@original_owner)
+      @publication_copy = @publication.copy_to_owner(@new_owner)
+    end
+    
+    teardown do
+      @publication_copy.destroy
+      @new_owner.destroy
+      
+      @publication.destroy
+      @original_owner.destroy
+    end
+    
+    should "retain the original creator" do
+      assert_equal @publication_copy.creator, @original_owner
+    end
+    
+    should "belong to the new owner" do
+      assert_equal @publication_copy.owner, @new_owner
+    end
+    
+    should "have its source as a parent" do
+      assert_equal @publication_copy.parent, @publication
+    end
+    
+    should "be a child of its parent" do
+      assert @publication.children.include?(@publication_copy)
+    end
+  end
 end
