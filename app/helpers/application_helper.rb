cc16ddb1158f37e15289ea1429eaf8106ade1d90
Ryan Baumann <ryan.baumann@gmail.com>
2010-01-14 10:12:08 -0500

    Fix adding new identifiers to accounts by using the RPX JS widget

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,13 @@ module ApplicationHelper
     @rpx.signin_url(dest, signin_method)
   end
 
-  def rpx_associate_url
+  def rpx_associate_url(signin_method='signin')
     dest = url_for :controller => :rpx, :action => :associate_return, :only_path => false
-    @rpx.signin_url(dest)
+    @rpx.signin_url(dest, signin_method)
   end
 
   def rpx_widget_url
-    BASE_URL + '/openid/v2/widget'
+    @rpx.base_url + '/openid/v2/widget'
   end
 end
 
