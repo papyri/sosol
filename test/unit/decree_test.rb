9daf9f3d6a11d40dcf4eab85d37266c20cc32b77
Ryan Baumann <ryan.baumann@gmail.com>
2010-01-21 16:05:45 -0500

    Test decrees under both percent and count tally methods

--- test/unit/decree_test.rb
+++ test/unit/decree_test.rb
@@ -1,59 +1,64 @@
 require 'test_helper'
 
 class DecreeTest < ActiveSupport::TestCase
-  context "a percent decree" do
-    setup do
-      @decree = Factory(:decree,
-                        :action => "approve",
-                        :trigger => "0.5",
-                        :tally_method => Decree::TALLY_METHODS[:percent],
-                        :choices => "yes")
-      # add some users to this decree's board
-      3.times do |i|
-        @decree.board.users << Factory(:user)
-      end
-    end
-    
-    teardown do
-      @decree.board.users.each {|u| u.destroy}
-      @decree.board.destroy
-      @decree.destroy
-    end
-    
-    should "have choices" do
-      assert @decree.get_choice_array.length > 0
-    end
-    
-    context "with votes on a publication/identifier" do
+  [{:tally_method => Decree::TALLY_METHODS[:percent],
+    :trigger => 0.5},
+   {:tally_method => Decree::TALLY_METHODS[:count],
+    :trigger => 2}].each do |method|
+    context "a #{method[:tally_method]} decree" do
       setup do
-        @publication = Factory(:publication, :owner => @decree.board.users.first)
-        @ddb_identifier = Factory(:DDBIdentifier, :publication => @publication)
+        @decree = Factory(:decree,
+                          :action => "approve",
+                          :trigger => method[:trigger],
+                          :tally_method => method[:tally_method],
+                          :choices => "yes")
+        # add some users to this decree's board
+        3.times do |i|
+          @decree.board.users << Factory(:user)
+        end
       end
-      
+    
       teardown do
-        @publication.destroy
+        @decree.board.users.each {|u| u.destroy}
+        @decree.board.destroy
+        @decree.destroy
       end
+    
+      should "have choices" do
+        assert @decree.get_choice_array.length > 0
+      end
+    
+      context "with votes on a publication/identifier" do
+        setup do
+          @publication = Factory(:publication, :owner => @decree.board.users.first)
+          @ddb_identifier = Factory(:DDBIdentifier, :publication => @publication)
+        end
       
-      should "perform action when trigger is met" do
-        2.times do |v|
+        teardown do
+          @publication.destroy
+        end
+      
+        should "perform action when trigger is met" do
+          2.times do |v|
+            Factory(:vote,
+                    :publication => @publication,
+                    :identifier => @ddb_identifier,
+                    :user => @decree.board.users[v],
+                    :choice => "yes")
+          end
+
+          assert @decree.perform_action?(@ddb_identifier.votes)
+        end
+      
+        should "not perform action when trigger is not met" do
           Factory(:vote,
                   :publication => @publication,
                   :identifier => @ddb_identifier,
-                  :user => @decree.board.users[v],
+                  :user => @decree.board.users[0],
                   :choice => "yes")
-        end
-
-        assert @decree.perform_action?(@ddb_identifier.votes)
-      end
-      
-      should "not perform action when trigger is not met" do
-        Factory(:vote,
-                :publication => @publication,
-                :identifier => @ddb_identifier,
-                :user => @decree.board.users[0],
-                :choice => "yes")
         
-        assert !@decree.perform_action?(@ddb_identifier.votes)
+          assert !@decree.perform_action?(@ddb_identifier.votes)
+        end
       end
     end
   end
