02abcff9ba3787a8591e683840931bc2e4ecfe75
Charles <charles@charles-jackalope.(none)>
2010-01-28 16:00:34 -0500

    Commented out after_save.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,12 @@ class Vote < ActiveRecord::Base
   belongs_to :identifier
   belongs_to :user
   belongs_to :board
+  
+ 
+=begin
+  def after_save
+    #need to tally votes and see if any action will take place
+    self.publication.tally_votes(@vote.identifier.votes)
+  end
+=end
 end
