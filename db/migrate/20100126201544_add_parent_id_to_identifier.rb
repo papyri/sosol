041f7d2368be7bfa4a31562556d7aebc9ff384a5
Ryan Baumann <ryan.baumann@gmail.com>
2010-02-02 16:20:20 -0500

    Remove identifier lineage migration

--- db/migrate/20100126201544_add_parent_id_to_identifier.rb
+++
@@ -1,9 +0,0 @@
-class AddParentIdToIdentifier < ActiveRecord::Migration
-  def self.up
-    add_column :identifiers, :parent_id, :integer
-  end
-
-  def self.down
-    remove_column :identifiers, :parent_id
-  end
-end
