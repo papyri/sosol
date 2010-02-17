504d9d63f06fd8464f83cfb28ac96c3a8c64c0bd
Carmen Lanz <InstPap@calgacus.papy.uni-heidelberg.de>
2010-02-17 11:42:35 +0100

    outsourced javascripts of bibliography editor into seperate file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-
 function showHide(id)
 {
   var element = document.getElementById(id);
@@ -12,58 +11,5 @@ function showHide(id)
   else
   {
     element.style.display = '';
-  }
-}
-
-function discardBiblbiography(jsonSourceElementId)
-{
-  $(jsonSourceElementId).value = '';
-  $(jsonSourceElementId).form.reset();
-  return false;
-}
-
-function applyBiblbiography(jsonSourceElementId, targetElementIdPrefix)
-{
-  var json = deserialiseBibliography($(jsonSourceElementId).value);
-
-  if(json)
-  {
-  	for (i in json)
-	{
-      var targetElement = $(targetElementIdPrefix + i);
-	  if(targetElement && !targetElement.disabled)
-      {
-        targetElement.value = json[i]
-      }
-  	}
-	$(jsonSourceElementId).value = '';
-  }
-  else
-  {
-  	alert('The data you entered into the zotero field cannot be read. \n\n Try again and drag a single data record from your\n zotero library into the text field.');
-  }
-  
-  return false;
-}
-
-function deserialiseBibliography(json)
-{
-  try
-  {
-	json = json.replace(/^[\s]+|[\s]+$/g, '').replace(/'/g, "\\'").replace(/\|/g, "'").evalJSON();
-	
-	var page = json['page'];
-
-    if(page)
-    {
-	  json['paginationStart'] = page.substr(0, page.indexOf('-'));
-	  json['paginationEnd']   = page.substr(page.indexOf('-') + 1);
-    }
-	
-	return json;
-  }
-  catch(e)
-  {
-  	return false;
   }
 }
