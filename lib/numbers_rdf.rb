ecc244d2974d627f11ee96627609bb75b9238b61
Ryan Baumann <ryan.baumann@gmail.com>
2010-02-17 16:39:30 -0500

    First pass at new dev version of NumbersRDF

--- lib/numbers_rdf.rb
+++ lib/numbers_rdf.rb
@@ -3,9 +3,10 @@ require 'jruby_xml'
 require 'iconv'
 
 module NumbersRDF
-  NUMBERS_SERVER_DOMAIN = 'papyri.info'
+  NUMBERS_SERVER_DOMAIN = 'dev.papyri.info'
   NUMBERS_SERVER_PORT = 80
   NUMBERS_SERVER_BASE_PATH = '/numbers'
+  
   # OAI identifiers should be in the form scheme ":" namespace-identifier ":" local-identifier
   OAI_SCHEME = 'oai'
   OAI_NAMESPACE_IDENTIFIER = 'papyri.info'
@@ -14,24 +15,31 @@ module NumbersRDF
   
   module NumbersHelper
     class << self
+      # TODO: after move from dev.papyri.info to papyri.info can probably use NUMBERS_SERVER_DOMAIN
       def identifier_to_local_identifier(identifier)
-        identifier.sub(/^#{OAI_IDENTIFIER_PREFIX}:/, '')
+        identifier.sub(/^#{OAI_NAMESPACE_IDENTIFIER}/, '')
+      end
+      
+      def identifier_url_to_identifier(identifier)
+        no_scheme = identifier.sub(/^http:\/\//,'')
+        no_decorator = no_scheme.sub(/\/(rdf|source)$/,'')
       end
     
       def identifier_to_components(identifier)
-        identifier.split(':')
+        identifier.split('/')
       end
 
       def identifier_to_path(identifier)
         local_identifier = identifier_to_local_identifier(identifier)
-        url_paths = [NUMBERS_SERVER_BASE_PATH] + 
-                    identifier_to_components(local_identifier)
+        url_paths = identifier_to_components(local_identifier)
+        url_paths << 'rdf'
         return url_paths.join('/')
       end
 
+      # FIXME: this should eventually go to e.g. /source or http://papyri.info/navigator/full/apis_columbia_p204 or something. Will be used to replace "View in PN" link constructed in app/views/identifiers/_pn_link.haml
       def identifier_to_url(identifier)
         return 'http://' + NUMBERS_SERVER_DOMAIN + ':' + 
-                NUMBERS_SERVER_PORT + identifier_to_path(identifier)
+                NUMBERS_SERVER_PORT.to_s + identifier_to_path(identifier)
       end
     
       def identifier_to_numbers_server_response(identifier)
@@ -46,7 +54,7 @@ module NumbersRDF
         if response.code != '200'
           return nil
         else
-          return process_numbers_server_response_body(
+          return [identifier] + process_numbers_server_response_body(
             Iconv.iconv('UTF-8','LATIN1',response.body).join)
         end
       end
@@ -66,9 +74,10 @@ module NumbersRDF
     
       def process_numbers_server_response_body(rdf_xml)
         identifiers = []
-        ore_describes_path = "/rdf:RDF/rdf:Description/*/rdf:Description/ore:describes"
+        ore_describes_path = "/rdf:RDF/rdf:Description/ns1:relation"
         JRubyXML.apply_xpath(rdf_xml, ore_describes_path, true).each do |ore_describes|
-          identifiers << ore_describes[:attributes]['rdf:resource']
+          resource = ore_describes[:attributes]['rdf:resource']
+          identifiers << identifier_url_to_identifier(resource)
         end
         
         return identifiers
