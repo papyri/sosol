f073c4b60b7a9af91215e5b5415e89ceb4b19d94
Ryan Baumann <ryan.baumann@gmail.com>
2010-02-18 16:55:04 -0500

    First pass at updating DDB/HGVMeta unit tests for new identifier scheme

--- test/unit/ddb_identifier_test.rb
+++ test/unit/ddb_identifier_test.rb
@@ -31,44 +31,47 @@ class DDBIdentifierTest < ActiveSupport::TestCase
     # (e.g. #{DDBIdentifier.collection_to_series('bgu')} instead of 0001)
     
     should "map the first identifier" do
-      bgu_1_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0001:1:1")
+      bgu_1_1 = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;1;1")
       assert_path_equal %w{bgu bgu.1 bgu.1.1.xml}, bgu_1_1.to_path
     end
     
     should "map ambiguous collections" do
-      bgu_ppetr_2_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0187:2:1")
+      bgu_ppetr_2_1 = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr;2;1")
       assert_path_equal %w{p.petr p.petr.2 p.petr.2.1.xml}, bgu_ppetr_2_1.to_path
       
-      bgu_ppetr2_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0186::1")
+      bgu_ppetr2_1 = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr.2;;1")
       assert_path_equal %w{p.petr.2 p.petr.2.1.xml}, bgu_ppetr2_1.to_path
     end
     
-    should "map files with '+' in the identifier" do
-      chla_5_299FrA_B_C = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0279:5:299FrA+B+C")
-      assert_path_equal %w{chla chla.5 chla.5.299FrA+B+C.xml}, chla_5_299FrA_B_C.to_path
-    end
+    # TODO: update this test for + solution
+    # should "map files with '+' in the identifier" do
+    #   chla_5_299FrA_B_C = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/0279:5:299FrA+B+C")
+    #   assert_path_equal %w{chla chla.5 chla.5.299FrA+B+C.xml}, chla_5_299FrA_B_C.to_path
+    # end
     
     should "map files with ',' in the identifier" do
-      bgu_13_2230_1 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0001:13:2230,1")
+      bgu_13_2230_1 = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;13;2230,1")
       assert_path_equal %w{bgu bgu.13 bgu.13.2230-1.xml}, bgu_13_2230_1.to_path
     end
     
-    should "map files with '/' in the identifier" do
-      o_bodl_2_1964_1967 = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:0014:2:1964/1967")
-      assert_path_equal %w{o.bodl o.bodl.2 o.bodl.2.1964_1967.xml}, o_bodl_2_1964_1967.to_path
-    end
+    # TODO: update this test for / solution
+    # should "map files with '/' in the identifier" do
+    #   o_bodl_2_1964_1967 = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/0014:2:1964/1967")
+    #   assert_path_equal %w{o.bodl o.bodl.2 o.bodl.2.1964_1967.xml}, o_bodl_2_1964_1967.to_path
+    # end
     
-    should "raise an error if series is non-existent" do
-      no_series = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp::1:1")
-      assert_raise RuntimeError do
-        no_series.to_path
-      end
-      
-      nonsense_series = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:3735928559:1:1")
-      assert_raise RuntimeError do
-        nonsense_series.to_path
-      end
-    end
+    # Irrelevant now?
+    # should "raise an error if series is non-existent" do
+    #   no_series = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/;1;1")
+    #   assert_raise RuntimeError do
+    #     no_series.to_path
+    #   end
+    #   
+    #   nonsense_series = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/3735928559:1:1")
+    #   assert_raise RuntimeError do
+    #     nonsense_series.to_path
+    #   end
+    # end
     
     # TODO: populate this test
     should "raise an error for illegal characters" do
@@ -88,7 +91,7 @@ class DDBIdentifierTest < ActiveSupport::TestCase
     #         # Grit timeout; possible Grit bug/leak?
     #         xml_content = REXML::Document.new(Repository.new.get_file_from_branch(filename))
     #         n_attribute = REXML::XPath.first(xml_content, '/TEI.2').attributes["n"]
-    #         this_ddb = Factory.build(:DDBIdentifier, :name => "oai:papyri.info:identifiers:ddbdp:#{n_attribute.tr(';',':')}")
+    #         this_ddb = Factory.build(:DDBIdentifier, :name => "papyri.info/ddbdp/#{n_attribute}")
     #         assert_equal filename, this_ddb.to_path
     #       end
     #     end
