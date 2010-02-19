1aae3cb18b05fe0194d8f761cfe18237698c0db1
Jon Fox <jfox@jfox-laptop.(none)>
2010-02-19 13:01:31 -0500

    update helpers to use XMLParseError and NonXMLParseError objects and put the rescue in the controller

--- app/models/leiden.rb
+++ app/models/leiden.rb
@@ -5,38 +5,24 @@ class Leiden < DDBIdentifier
   def self.xml_leiden_plus(content)
     #add <wrapab> tag
     abs = DDBIdentifier.preprocess_abs(content)
-    begin
-      #call to convert
-      transformed = DDBIdentifier.xml2nonxml(abs)
-      
-      #remove <= and => that represents <wrapab>
-      transformed.slice!(/^<=/)
-      transformed.slice!(/=>$/)
+    #call to convert
+    transformed = DDBIdentifier.xml2nonxml(abs)
+    
+    #remove <= and => that represents <wrapab>
+    transformed.slice!(/^<=/)
+    transformed.slice!(/=>$/)
 
-    rescue Exception => e
-      if e.message.to_s =~ /^dk\.brics\.grammar\.parser\.ParseException: parse error at character (\d+)/
-        return e.message.to_s + "\n" + 
-          DDBIdentifier.parse_exception_pretty_print(abs, $1.to_i)
-      end
-    end
     return transformed
   end
   
   def self.leiden_plus_xml(content)
     # add Leiden+ grammar for <ab> tag so will meet minimun xSugar grammar requirement
     abs = "<=" + content + "=>"
-    begin
-      #call to convert
-      tempTrans = DDBIdentifier.nonxml2xml(abs)
-      # pull out only the XML tags inside the <ab> tag
-      transformed = REXML::XPath.match(REXML::Document.new(tempTrans), '/wrapab/ab/*')
+    #call to convert
+    tempTrans = DDBIdentifier.nonxml2xml(abs)
+    # pull out XML inside the <ab> tag
+    transformed = REXML::XPath.match(REXML::Document.new(tempTrans), '/wrapab/ab/[not(self::ab)]')
     
-    rescue Exception => e
-      if e.message.to_s =~ /^dk\.brics\.grammar\.parser\.ParseException: parse error at character (\d+)/
-        return e.message.to_s + "\n" + 
-          DDBIdentifier.parse_exception_pretty_print(abs, $1.to_i)
-      end
-    end
     return transformed
   end
   
