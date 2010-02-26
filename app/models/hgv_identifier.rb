799501bd5d5f45339d55f27241c8848f99199fec
Ryan Baumann <ryan.baumann@gmail.com>
2010-02-26 09:54:01 -0500

    Kill to_roman for converting Arabic to Roman numerals for HGV collection numbers

--- app/models/hgv_identifier.rb
+++ app/models/hgv_identifier.rb
@@ -8,28 +8,6 @@ class HGVIdentifier < Identifier
   
   FRIENDLY_NAME = "HGV Identifier"
   
-  ROMAN_MAP = { 1 => "I",
-                4 => "IV",
-                5 => "V",
-                9 => "IX",
-                10 => "X",
-                40 => "XL",
-                50 => "L",
-                90 => "XC",
-                100 => "C",
-                400 => "CD",
-                500 => "D",
-                900 => "CM",
-                1000 => "M" }
-  
-  def to_roman(arabic)
-    # shamelessly stolen from http://rubyquiz.com/quiz22.html
-    ROMAN_MAP.keys.sort { |a, b| b <=> a }.inject("") do |roman, div|
-      times, arabic = arabic.divmod(div)
-      roman << ROMAN_MAP[div] * times
-    end
-  end
-  
   def temporary_path
     # path constructor for born-digital temporary SoSOL identifiers
     trimmed_name = name.sub(/papyri.info\/hgv\//, '')
