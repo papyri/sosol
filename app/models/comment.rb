0122f61f6f6bbc838ae91077d62fd17b25ec1822
Charles <charles@charles-jackalope.(none)>
2010-03-09 13:18:12 -0500

    Simple user stats based on comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :publication
   belongs_to :identifier
+  
+  #create named scope for each type of reason
+  named_scope :finalizing, :conditions => { :reason => 'finalizing' }
+  named_scope :submit, :conditions => { :reason => 'submit' }
+  named_scope :general, :conditions => { :reason => 'general' }
+  named_scope :vote, :conditions => { :reason => 'vote' }
+  
+  
+  
 end
