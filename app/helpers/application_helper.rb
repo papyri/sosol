23e429f43bb9367e19491e5f76de6e1ebcb12271
Ryan Baumann <ryan.baumann@gmail.com>
2010-03-19 12:06:02 -0400

    First pass at Clippy support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,33 @@ module ApplicationHelper
   def rpx_widget_url
     @rpx.base_url + '/openid/v2/widget'
   end
+  
+  def clippy(textarea_id, bgcolor='#FFFFFF')
+    html = <<-EOF
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+              width="110"
+              height="14"
+              id="clippy" >
+      <param name="movie" value="/flash/clippy.swf"/>
+      <param name="allowScriptAccess" value="always" />
+      <param name="quality" value="high" />
+      <param name="scale" value="noscale" />
+      <param name="FlashVars" value="id=#{textarea_id}" />
+      <param name="bgcolor" value="#{bgcolor}">
+      <embed src="/flash/clippy.swf"
+             width="110"
+             height="14"
+             name="clippy"
+             quality="high"
+             allowScriptAccess="always"
+             type="application/x-shockwave-flash"
+             pluginspage="http://www.macromedia.com/go/getflashplayer"
+             FlashVars="id=#{textarea_id}"
+             bgcolor="#{bgcolor}"
+      />
+      </object>
+    EOF
+  end
 end
 
 require 'rexml/document'
