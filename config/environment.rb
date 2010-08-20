bc94a59d020681a72ab75a76005d288313d92ea4
Ryan Baumann <ryan.baumann@gmail.com>
2010-08-20 12:40:12 -0400

    Bump hoptoad_notifier to 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -52,7 +52,7 @@ Rails::Initializer.run do |config|
   
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   
-  config.gem 'hoptoad_notifier'
+  config.gem 'hoptoad_notifier', :version => ">= 2.3.5"
   
   config.gem 'grit',
     :lib     => 'grit',
