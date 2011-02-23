fa0c59ecae5d00d27a366c1c56b2b3b7636b5ba3
Carmen Lanz <InstPap@calgacus.papy.uni-heidelberg.de>
2011-02-23 16:03:44 +0100

    new: support for »unknown« dates, metadata editor: saving feature for HGV dates, refactoring: HgvDate.dateInformation ist now called HgvDate.epidocToHgv, ananlogously theres the function HgvDate.hgvToEpidoc

--- app/helpers/hgv_meta_identifier_helper.rb
+++ app/helpers/hgv_meta_identifier_helper.rb
@@ -5,6 +5,7 @@ module HgvMetaIdentifierHelper
       [['', ''], 
         [I18n.t('date.ca'), :ca]]
     end
+
     def HgvDate.monthOptions
       [['', ''], 
         [I18n.t('date.beginning'), :beginning],
@@ -14,6 +15,7 @@ module HgvMetaIdentifierHelper
         [I18n.t('date.end'), :end], 
         [I18n.t('date.endCirca'), :endCirca]]
     end
+
     def HgvDate.yearOptions
       [['', ''], 
         [I18n.t('date.beginning'), :beginning], 
@@ -31,6 +33,7 @@ module HgvMetaIdentifierHelper
         [I18n.t('date.end'), :end],
         [I18n.t('date.endCirca'), :endCirca]]
     end
+
     def HgvDate.centuryOptions
       [['', ''], 
         [I18n.t('date.beginning'), :beginning], 
@@ -52,6 +55,7 @@ module HgvMetaIdentifierHelper
         [I18n.t('date.end'), :end], 
         [I18n.t('date.endCirca'), :endCirca]]
     end
+
     def HgvDate.offsetOptions
       [['', ''], 
         [I18n.t('date.before'), :before], 
@@ -59,6 +63,7 @@ module HgvMetaIdentifierHelper
         [I18n.t('date.beforeUncertain'), :beforeUncertain], 
         [I18n.t('date.afterUncertain'), :afterUncertain]]
     end
+
     def HgvDate.certaintyOptions
       [['', ''], 
         [I18n.t('date.certaintyLow'), :low], 
@@ -70,9 +75,99 @@ module HgvMetaIdentifierHelper
         [I18n.t('date.dayAndYearUncertain'), :day_year],
         [I18n.t('date.dayMonthAndYearUncertain'), :day_month_year]]
     end
-    def HgvDate.attributeBase date_index, date_type
-      'hgv_meta_identifier[textDate][' + date_index.to_s + '][children][' + date_type + 'Date][attributes]'
+
+    def HgvDate.getYearIso century, centuryQualifier, chron
+      century = century.to_i != 0 ? century.to_i : nil
+      
+      yearModifier = {
+        :chronMin => {
+          nil => 0,
+          :beginning => 0,
+          :beginningToMiddle => 0,
+          :firstHalf => 0,
+          :firstHalfToMiddle => 25,
+          :middle => 25,
+          :middleToSecondHalf => 50,
+          :secondHalf => 50,
+          :middleToEnd => 25,
+          :end => 75
+        },
+        :chronMax => {
+          nil => 0,
+          :beginning => -75,
+          :beginningToMiddle => -50,
+          :firstHalf => -50,
+          :firstHalfToMiddle => -50,
+          :middle => -25,
+          :middleToSecondHalf => -25,
+          :secondHalf => 0,
+          :middleToEnd => 0,
+          :end => 0
+        }
+      }[chron][centuryQualifier ? centuryQualifier.to_s.sub('Circa', '').to_sym : nil]
+
+      if chron == :chronMax
+        year = century > 0 ? (century * 100 + yearModifier).to_s.rjust(4, '0') : ((century + 1) * 100 + yearModifier - 1).abs.to_s.rjust(4, '0')
+      else
+        year = century < 0 ? (century * 100 + yearModifier).abs.to_s.rjust(4, '0') : ((century - 1) * 100 + yearModifier + 1).to_s.rjust(4, '0')
+      end
+
+      (century < 0 ? '-' : '') + year
     end
+    
+    def HgvDate.getMonthIso month, yearQualifier, chron
+      if month
+        month.rjust(2, '0')
+      else
+        {
+          :chronMin => {
+            nil => nil,
+            :beginning => '01',
+            :firstHalf => '01',
+            :firstHalfToMiddle => '04',
+            :middle => '04',
+            :middleToSecondHalf => '07',
+            :secondHalf => '07',
+            :end => '10'
+          },
+          :chronMax => {
+            nil => nil,
+            :beginning => '03',
+            :firstHalf => '06',
+            :firstHalfToMiddle => '06',
+            :middle => '09',
+            :middleToSecondHalf => '09',
+            :secondHalf => '12',
+            :end => '12'
+          }
+        }[chron][yearQualifier ? yearQualifier.to_s.sub('Circa', '').to_sym : nil]
+      end
+    end
+    
+    def HgvDate.getDayIso day, month, monthQualifier, chron
+      if day
+        day.to_s.rjust(2, '0')
+      else
+        m = month.to_i
+        day_max = m ? (m != 2 ? (m < 8 ? ((m % 2) == 0 ? 30 : 31) : ((m % 2) == 0 ? 31 : 30) ) : (y && ((y % 4) == 0) && (((y % 100) != 0) || ((y % 400) == 0)) ? 29 : 28)) : 31
+
+        {
+          :chronMin => {
+            nil => nil,
+            :beginning => '01',
+            :middle => '11',
+            :end => '21'
+          },
+          :chronMax => {
+            nil => nil,
+            :beginning => '10',
+            :middle => '20',
+            :end => day_max.to_s
+          }
+        }[chron][monthQualifier ? monthQualifier.to_s.sub('Circa', '').to_sym : nil]
+      end
+    end
+    
     def HgvDate.getCentury year
       if !year
         nil
@@ -80,6 +175,7 @@ module HgvMetaIdentifierHelper
         (year.abs / 100 + ((year.abs % 100) == 0 ? 0 : 1)) * (year > 0 ? 1 : -1)
       end
     end
+
     def HgvDate.getCenturyQualifier year, year2
       if !year || !year2
         return nil
@@ -139,7 +235,7 @@ module HgvMetaIdentifierHelper
         ]
       end
     end
-    
+
     def HgvDate.getYearQualifier month = nil, month2 = nil
       if month && month2
         return {
@@ -207,15 +303,65 @@ module HgvMetaIdentifierHelper
       end
     end
 
-    def HgvDate.dateInformation date_item      
-      t = {
+    def HgvDate.extractFromIso iso, regex 
+      if iso
+        iso =~ regex ? iso[regex, 1].to_i  : nil
+      else
+        nil
+      end
+    end
+    
+    def HgvDate.yearFromIso iso
+      HgvDate.extractFromIso iso, /\A(-?\d\d\d\d)/
+    end
+    
+    def HgvDate.monthFromIso iso
+      HgvDate.extractFromIso iso, /\A-?\d\d\d\d-(\d\d)/
+    end
+    
+    def HgvDate.dayFromIso iso
+      HgvDate.extractFromIso iso, /\A-?\d\d\d\d-\d\d-(\d\d)\Z/
+    end
+    
+    def HgvDate.getEmptyHgvItem
+      {
         :c => nil, :y => nil, :m => nil, :d => nil, :cx => nil, :yx => nil, :mx => nil, :offset => nil, :precision => nil, :ca => false,
         :c2 => nil, :y2 => nil, :m2 => nil, :d2 => nil, :cx2 => nil, :yx2 => nil, :mx2 => nil, :offset2 => nil, :precision2 => nil, :ca2 => false,
         :certainty => nil,
         :unknown => nil,
         :error => nil,
-        :empty => nil}
-      
+        :empty => nil
+      }
+    end
+    
+    def HgvDate.getEmptyEpidocItem
+      {
+        :value => nil,
+        :attributes => {
+          :id => nil,
+          :when => nil,
+          :notBefore => nil,
+          :notAfter => nil,
+          :certainty => nil,
+          :precision => nil
+        },
+        :children => {
+          :offset => [],
+          :precision => [],
+          :certainty => []
+        }
+      }
+    end
+    
+    def HgvDate.getPrecision precision, cx, yx, mx
+      ca = precision || (cx  && cx.to_s.include?('Circa')) || (yx  && yx.to_s.include?('Circa')) || (mx  && mx.to_s.include?('Circa'))
+      vague = cx || yx || mx
+      ca && vague ? :lowlow : (ca ? :medium : (vague ? :low : nil))
+    end
+    
+    def HgvDate.epidocToHgv date_item      
+      t = HgvDate.getEmptyHgvItem
+
       if date_item == nil # simple case: no date
         t[:empty] = true
         return t
@@ -231,24 +377,16 @@ module HgvMetaIdentifierHelper
         if date_item && date_item[:attributes]
 
           # date1
-          dateIso = date_item[:attributes][:when] ? date_item[:attributes][:when] : (date_item[:attributes][:notBefore] ? date_item[:attributes][:notBefore] : date_item[:attributes][:notAfter])
-          if dateIso
-
-            regexYear  = /\A(-?\d\d\d\d)/
-            regexMonth = /\A-?\d\d\d\d-(\d\d)/
-            regexDay   = /\A-?\d\d\d\d-\d\d-(\d\d)\Z/
-
-            t[:y] = dateIso =~ regexYear  ? dateIso[regexYear, 1].to_i  : nil
-            t[:m] = dateIso =~ regexMonth ? dateIso[regexMonth, 1].to_i : nil
-            t[:d] = dateIso =~ regexDay   ? dateIso[regexDay, 1].to_i   : nil
+          iso = date_item[:attributes][:when] ? date_item[:attributes][:when] : (date_item[:attributes][:notBefore] ? date_item[:attributes][:notBefore] : date_item[:attributes][:notAfter])
+          if iso
+            t[:y] = yearFromIso iso
+            t[:m] = monthFromIso iso
+            t[:d] = dayFromIso iso
 
             # date2
-            dateIso2 = date_item[:attributes][:notAfter]
-            if dateIso2
-              t[:y2] = dateIso2 =~ regexYear  ? dateIso2[regexYear, 1].to_i  : nil
-              t[:m2] = dateIso2 =~ regexMonth ? dateIso2[regexMonth, 1].to_i : nil
-              t[:d2] = dateIso2 =~ regexDay   ? dateIso2[regexDay, 1].to_i   : nil
-            end
+            t[:y2] = yearFromIso date_item[:attributes][:notAfter]
+            t[:m2] = monthFromIso date_item[:attributes][:notAfter]
+            t[:d2] = dayFromIso date_item[:attributes][:notAfter]
 
             if date_item[:children]
               # ca.
@@ -406,6 +544,12 @@ module HgvMetaIdentifierHelper
                   t[:certainty] = cert.delete_if{|k,v| v == 0}.keys.collect{|i| i.to_s[0..-2] }.join('_').to_sym # CL support for plurals goes here
                 end
               end
+              
+              # kill doublets
+
+              t[:y2] = (t[:y2] == t[:y] ? nil : t[:y2])
+              t[:m2] = (t[:m2] == t[:m] ? nil : t[:m2])
+              t[:d2] = (t[:d2] == t[:d] ? nil : t[:d2])
 
             end
 
@@ -418,30 +562,149 @@ module HgvMetaIdentifierHelper
   
       t
     end
-    def HgvDate.BACKUPdateInformation date_item
-
-      data = {:century => {:value => '', :extent => '', :certainty => ''}, :year => {:value => '', :extent => '', :certainty => ''}, :month => {:value => '', :extent => '', :certainty => ''}, :day => {:value => '', :extent => '', :certainty => ''}, :offset => '', :certainty => '', :certaintyPicker => ''}
-      
-      
-      
-      
-      data[:certainty] = date_item && date_item[:attributes] && date_item[:attributes][:certainty] ? date_item[:attributes][:certainty] : ''
-
-      if date_item && date_item[:children]
-        data.each_key { |key|
-          if date_item[:children][key]
-            data[key][:value] = date_item[:children][key][:value] ? date_item[:children][key][:value] : ''
-            data[key][:extent] = date_item[:children][key][:attributes][:extent] ? date_item[:children][key][:attributes][:extent] : ''
-            data[key][:certainty] = date_item[:children][key][:attributes][:certainty] ? date_item[:children][key][:attributes][:certainty] : ''
-          end
+    
+    def HgvDate.getPrecisionItem degree, match = nil 
+      {
+        :value => nil,
+        :children => {},
+        :attributes => {
+          :match => match,
+          :degree => degree
         }
-        data[:offset] = date_item[:children][:offset] && date_item[:children][:offset][:value] ? date_item[:children][:offset][:value] : ''
+      }   
+    end
+    
+    def HgvDate.getCertaintyItem match 
+      {
+        :value => nil,
+        :children => {},
+        :attributes => {
+          :match => match
+        }
+      }   
+    end
+    
+    def  HgvDate.getOffsetItem offset
+      offset = offset.to_sym
+      {
+        :value => {:before => 'vor', :after => 'nach', :beforeUncertain => 'vor (?)', :afterUncertain => 'nach (?)'}[offset],
+        :children => {},
+        :attributes => {
+          :type => offset.to_s.sub('Uncertain', '')
+        }
+      }
+    end
+    
+    def HgvDate.hgvToEpidoc date_item
+      t = HgvDate.getEmptyEpidocItem
+        
+      # date it X, Y, Z
+      t[:attributes][:id] = date_item[:id]
 
+      # unknown
+      if date_item[:unknown]
+        t[:value] = 'unbekannt'
+        return t
       end
 
-      data[:certaintyPicker] = data[:certainty] != '' ? data[:certainty] : {:day => data[:day][:certainty], :month => data[:month][:certainty], :year => data[:year][:certainty]}.reject{|k, v| v.empty? }.keys.join('_')
+      # centuries
+      if date_item[:c]
+        t[:attributes][:notBefore] = HgvDate.getYearIso date_item[:c], date_item[:cx], :chronMin
+        if date_item[:c2]
+          t[:attributes][:notAfter] = HgvDate.getYearIso date_item[:c2], date_item[:cx2], :chronMax
+        else
+          t[:attributes][:notAfter] = HgvDate.getYearIso date_item[:c], date_item[:cx], :chronMax
+        end
 
-      data
+        ca = date_item[:cx] && date_item[:cx].to_s.include?('Circa') ? true : false
+        ca2 = date_item[:cx2] && date_item[:cx2].to_s.include?('Circa') ? true : false
+
+        if ca && ca2
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.1'
+        elsif ca
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.1', '../@notBefore'
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.3', '../@notAfter'
+        elsif ca2
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.3', '../@notBefore'
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.1', '../@notAfter'
+        else
+          t[:attributes][:precision] = 'low'
+        end
+      else
+        y = {nil => '', 0 => '-'}[date_item[:y] =~ /-/] + date_item[:y].sub('-', '').rjust(4, '0')
+        m = HgvDate.getMonthIso date_item[:m], date_item[:yx], :chronMin
+        d = HgvDate.getDayIso date_item[:d], date_item[:m], date_item[:mx], :chronMin
+        
+        date = y + (m ? '-' + m + (d ? '-' + d : '') : '')
+        
+        # only one date
+        if !date_item[:y2] && !date_item[:m2] && !date_item[:d2] && !date_item[:yx] && !date_item[:mx]
+          attribute = date_item[:offset] ? (date_item[:offset].include?('before') ? :notAfter : :notBefore) : :when
+          t[:attributes][attribute] = date
+        else
+          t[:attributes][:notBefore] = date
+          
+          y2 = date_item[:y2] ? {nil => '', 0 => '-'}[date_item[:y2] =~ /-/] + date_item[:y2].sub('-', '').rjust(4, '0') : y
+          m2 = HgvDate.getMonthIso((date_item[:m2] ? date_item[:m2] : (date_item[:d2] ? date_item[:m] : nil)), (date_item[:yx2] ? date_item[:yx2] : date_item[:yx]), :chronMax)
+          d2 = HgvDate.getDayIso date_item[:d2], (date_item[:m] ? date_item[:m] : nil), (date_item[:mx] ? date_item[:mx] : date_item[:mx2]), :chronMax
+          
+          date2 = y2 + (m2 ? '-' + m2 + (d2 ? '-' + d2 : '') : '')
+          
+          t[:attributes][:notAfter] = date2
+        end
+
+        # precision
+        precision = HgvDate.getPrecision(date_item[:precision], date_item[:cx], date_item[:yx], date_item[:mx])
+        precision2 = HgvDate.getPrecision(date_item[:precision2], date_item[:cx2], date_item[:yx2], date_item[:mx2])
+  
+        if precision && ((precision == precision2) || ([t[:attributes][:when], t[:attributes][:notBefore], t[:attributes][:notAfter]].compact.length == 1))
+          if precision == :lowlow
+            t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem '0.1'
+          else
+            t[:attributes][:precision] = precision
+          end
+        end
+        if precision
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem(precision == :low ? nil : (precision == :medium ? '0.5' : '0.1'), t[:attributes][:when] ? '../@when' : '../@notBefore')
+        end
+        if precision2
+          t[:children][:precision][t[:children][:precision].length] = HgvDate.getPrecisionItem(precision2 == :low ? nil : (precision2 == :medium ? '0.5' : '0.1'), '../@notAfer')
+        end
+
+        
+      end
+            
+      # offset
+      if date_item[:offset]
+        t[:children][:offset][t[:children][:offset].length] = HgvDate.getOffsetItem date_item[:offset]
+      end
+
+      if date_item[:offset2]
+        t[:children][:offset][t[:children][:offset].length] = HgvDate.getOffsetItem date_item[:offset2]
+      end
+
+      # offset certainty
+      if date_item[:offset] && date_item[:offset].to_s.include?('Uncertain')
+        t[:children][:certainty][t[:children][:certainty].length] = HgvDate.getCertaintyItem "../offset[@type='"+date_item[:offset].to_s.sub('Uncertain', '')+"']"
+      end
+
+      if date_item[:offset2] && date_item[:offset2].to_s.include?('Uncertain')
+        t[:children][:certainty][t[:children][:certainty].length] = HgvDate.getCertaintyItem "../offset[@type='"+date_item[:offset2].to_s.sub('Uncertain', '')+"']"
+      end
+      
+      # certainty
+      if date_item[:certainty] # global uncertainty
+        if date_item[:certainty] == :low
+          t[:attributes][:certainty] = 'low'
+        else # uncertainty for day, month or year
+          date_item[:certainty].to_s.split('_').each{|dayMonthYear|
+            match = '../' + dayMonthYear + '-from-date(@' + (t[:attributes][:when] ? 'when' : 'notBefore') + ')' # cl: support for plurals would go here 
+            t[:children][:certainty][t[:children][:certainty].length] = HgvDate.getCertaintyItem match
+          }
+        end
+      end
+
+      t
     end
   end
 
@@ -500,7 +763,38 @@ module HgvMetaIdentifierHelper
       }
       hashOut
     end
-    
+
+    def HgvFormat.formatHgv date_item
+      certainty = ''
+      
+      date1 = formatDatePart(
+        date_item[:c],
+        date_item[:y],
+        date_item[:m],
+        date_item[:d],
+        date_item[:cx],
+        date_item[:yx],
+        date_item[:mx],
+        date_item[:offset],
+        date_item[:certainty]
+      )
+
+      date2 = formatDatePart(
+        date_item[:c2],
+        date_item[:y2],
+        date_item[:m2],
+        date_item[:d2],
+        date_item[:cx2],
+        date_item[:yx2],
+        date_item[:mx2],
+        date_item[:offset2],
+        date_item[:certainty2]
+      )
+
+      return (date2 && date2.include?(' v.Chr.') ? date1.sub(/ v\.Chr\./, '') : date1) + 
+             (date2 && !date2.empty? ? ' - ' + date2 : '') + 
+             (!certainty.empty? ? ' ' + certainty : '')
+    end
     
     def HgvFormat.formatDate date1, date2 = nil
       #{:century => {:value => '', :extent => '', :certainty => ''}, :year => {:value => '', :extent => '', :certainty => ''}, :month => {:value => '', :extent => '', :certainty => ''}, :day => {:value => '', :extent => '', :certainty => ''}, :offset => '', :certainty => '', :certaintyPicker => ''}
