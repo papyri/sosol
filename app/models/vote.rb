f1c5fbf2c9f1a3f95c7bfa66a44e9b41369d66eb
charles <charles@charles-VirtualBox.(none)>
2011-03-14 15:07:32 -0400

    Let the publication determine which votes to consider for tally_votes.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,15 +13,18 @@ class Vote < ActiveRecord::Base
     if self.identifier # && self.identifier.status == "editing"
       #need to tally votes and see if any action will take place
       #should only be voting while the publication is owned by the correct board
-      related_votes = self.identifier.votes
+      #related_votes = self.identifier.votes
       
       #choose vote based on publication votes
       #TODO add votes to be related to publication 
-      related_votes = self.publication.votes
+      #related_votes = self.publication.votes
       
       #todo add check to ensure board is correct
       #decree_action = self.publication.tally_votes(related_votes)
-      self.publication.tally_votes(related_votes)
+      #self.publication.tally_votes(related_votes)
+      
+      #let publication decide how to access votes
+      self.publication.tally_votes()
     end    
     return nil
   end
