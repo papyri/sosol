8a3bafd3285f5a13bf54b9cccdf3c03af3e53e58
Ryan Baumann <ryan.baumann@gmail.com>
2011-09-07 16:52:59 -0400

    Revert to factory_girl 1.2.2

--- config/environment.rb
+++ config/environment.rb
@@ -50,7 +50,7 @@ Rails::Initializer.run do |config|
   
   config.gem "shoulda", :version => ">= 2.11.3"
   
-  config.gem "factory_girl", :version => ">= 2.0.4"
+  config.gem "factory_girl", :version => "~> 1.2.2"
   
   config.gem 'hoptoad_notifier', :version => ">= 2.4.10"
   
