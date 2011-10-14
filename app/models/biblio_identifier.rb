7c210b625481dee605326124219dedcefb42d22b
Carmen Lanz <elemmire@ossiriand.de>
2011-10-14 15:05:08 +0200

    write biblio data (that has been entered via the editor`s interface) back to EpiDoc

--- app/models/biblio_identifier.rb
+++ app/models/biblio_identifier.rb
@@ -137,6 +137,358 @@ class BiblioIdentifier < HGVIdentifier
     populateFromEpiDoc
   end
 
+  def updateFromPost params
+    @post = params
+
+    updateFromPostSimple :title
+    updateFromPostSimple :journalTitle
+    updateFromPostSimple :bookTitle
+    updateFromPostSimple :type
+    updateFromPostSimple :subtype
+    updateFromPostSimple :language
+    
+    updateFromPostSimple :bp
+    updateFromPostSimple :bpOld
+    updateFromPostSimple :idp
+    updateFromPostSimple :isbn
+    updateFromPostSimple :sd
+    updateFromPostSimple :checklist
+
+    updateFromPostSimple :date
+    updateFromPostSimple :edition
+    updateFromPostSimple :issue
+    updateFromPostSimple :distributor
+    updateFromPostSimple :paginationFrom
+    updateFromPostSimple :paginationTo
+    updateFromPostSimple :paginationTotal
+    updateFromPostSimple :paginationPreface
+    updateFromPostSimple :illustration
+    updateFromPostSimple :no
+    updateFromPostSimple :col
+    updateFromPostSimple :tome
+    updateFromPostSimple :link
+    updateFromPostSimple :fasc
+    updateFromPostSimple :reedition
+    
+    updateFromPostPerson :authorList
+    updateFromPostPerson :editorList
+    
+    updateFromPostShortTitle :journalTitleShort
+    updateFromPostShortTitle :bookTitleShort
+    
+    updateFromPostNote
+   
+    updateFromPostPublisher
+   
+    updateFromPostReviewee
+    
+    updateFromPostContainer
+   
+    updateFromPostRelatedArticle
+
+    updateEpiDoc
+    
+    # write back to a string
+    formatter = REXML::Formatters::Pretty.new
+    formatter.compact = true
+    formatter.width = 512
+    modified_xml_content = ''
+    formatter.write @epiDoc, modified_xml_content
+    
+    modified_xml_content
+  end
+  
+  def updateFromPostSimple key
+    self[key] = @post[key] && @post[key].kind_of?(String) && !@post[key].strip.empty? ? @post[key].strip : nil
+  end
+  
+  def updateFromPostPerson key
+    list = []
+    if @post[key] &&  @post[key].kind_of?(Hash)
+      @post[key].each_pair {|index, person|
+        newbie = PublicationPerson.new
+        if person[:firstName] && !person[:firstName].strip.empty?
+          newbie.firstName = person[:firstName].strip
+        end
+        if person[:lastName] && !person[:lastName].strip.empty?
+          newbie.lastName = person[:lastName].strip
+        end
+        if person[:name] && !person[:name].strip.empty?
+          newbie.name = person[:name].strip
+        end
+        list[list.length] = newbie
+      }
+    end
+    self[key] = list
+  end
+    
+  def updateFromPostShortTitle key
+    list = []
+    if @post[key] && @post[key].kind_of?(Hash)
+      @post[key].each_pair{|index, title|
+        if !title[:title].strip.empty?
+          list[list.length] = ShortTitle.new(title[:title].strip, !title[:responsibility].strip.empty? ? title[:responsibility].strip : nil)
+        end
+      }
+    end
+    self[key] = list
+  end
+
+  def updateFromPostNote
+    self[:note] = []
+    if @post[:note] && @post[:note].kind_of?(Hash)
+      @post[:note].each_pair{|index, note|
+        if !note[:annotation].strip.empty?
+          self[:note][self[:note].length] = Note.new(!note[:responsibility].strip.empty? ? note[:responsibility].strip : nil, note[:annotation].strip)
+        end
+      }
+    end
+  end
+
+  def updateFromPostPublisher
+    self[:publisherList] = []
+    if @post[:publisherList] && @post[:publisherList].kind_of?(Hash)
+      @post[:publisherList].each_pair{|index, publisher|
+        if !publisher[:name].strip.empty? && ['pubPlace', 'publisher'].include?(publisher[:place].strip)
+          self[:publisherList][self[:publisherList].length] = Publisher.new(publisher[:place].strip, publisher[:name].strip)
+        end
+      }
+    end
+  end
+
+  def updateFromPostReviewee
+    updateFromPostRelatedItem :revieweeList, Reviewee
+  end
+
+  def updateFromPostContainer
+    updateFromPostRelatedItem :containerList, Container
+  end
+
+  def updateFromPostRelatedItem typeX, classX
+    self[typeX] = []
+    if @post[typeX] && @post[typeX].kind_of?(Hash)
+      @post[typeX].each_pair{|key, pointer|
+        self[typeX][self[typeX].length] = classX.new(pointer)
+    }
+    end
+  end
+
+  def updateFromPostRelatedArticle
+    if @post[:relatedArticleList] && @post[:relatedArticleList].kind_of?(Hash)
+      @post[:relatedArticleList].each_pair{|key, article|
+        series    = article[:series]
+        volume    = article[:volume]
+        number    = article[:number]
+        ddb       = article[:ddb]
+        tm        = article[:tm]
+        inventory = article[:inventory]
+  
+        self[:relatedArticleList][self[:relatedArticleList].length] = RelatedArticle.new(series, volume, number, ddb, tm, inventory)
+      }
+    end
+  end
+  
+  def updateEpiDoc
+
+    [:title, :journalTitle, :bookTitle, :type, :subtype, :language, :bp, :bpOld, :isbn, :sd, :checklist, :date, :edition, :issue, :distributor, :paginationFrom, :paginationTo, :paginationTotal, :paginationPreface, :illustration, :no, :col, :tome, :link, :fasc, :reedition].each{|key|
+      attribute = nil
+      path = XPATH[key]
+      if path =~ /(.+)\/@([^\[\]\/]+)\Z/
+        path = $1
+        attribute = $2
+      end
+      
+      if self[key] && !self[key].strip.empty?
+        element = @epiDoc.bulldozePath path
+        if attribute
+          element.attributes[attribute] = self[key]
+        else
+          element.text = self[key]
+        end
+      else
+        @epiDoc.elements.delete_all path
+      end
+    }
+
+    [:authorList, :editorList].each{|key|
+      @epiDoc.elements.delete_all XPATH[key]
+      index = 1
+      self[key].each{|person|
+        element = @epiDoc.bulldozePath XPATH[key] + "[@n='" + index.to_s + "']"
+        if person.name && !person.name.empty?
+          element.text = person.name
+        end
+        if person.firstName && !person.firstName.empty?
+          child = REXML::Element.new 'forename'
+          child.text = person.firstName
+          element.add child
+        end
+        if person.lastName && !person.lastName.empty?
+          child = REXML::Element.new 'surname'
+          child.text = person.lastName
+          element.add child
+        end
+        index += 1
+      }
+    }
+
+    @epiDoc.elements.delete_all XPATH[:publisherList]
+    if self[:publisherList] && self[:publisherList].kind_of?(Array)
+      basePath = XPATH[:publisherList][/\A(.+)node\(\).+\Z/, 1]
+      
+      index = {'pubPlace' => 1, 'publisher' => 1}
+      self[:publisherList].each{|publisher|
+        if publisher.value && !publisher.value.empty?
+          element = @epiDoc.bulldozePath basePath + publisher.type + "[@n='" + index[publisher.type].to_s + "']"
+          element.text = publisher.value
+          index[publisher.type] += 1
+        end
+      }
+    end
+
+
+   [:journalTitleShort, :bookTitleShort].each{|key|
+     @epiDoc.elements.delete_all XPATH[key]
+     index = 1
+     self[key].each{|shorty|
+       if shorty.title && !shorty.title.empty?
+         element = @epiDoc.bulldozePath XPATH[key] + "[@n='" + index.to_s + "']"
+         element.text = shorty.title
+         if shorty.responsibility && !shorty.responsibility.empty?
+           element.attributes['resp'] = shorty.responsibility
+         end
+         index += 1
+       end
+     }
+     
+   }
+   
+   @epiDoc.elements.delete_all XPATH[:note]
+    if self[:note] && self[:note].kind_of?(Array)
+      path = XPATH[:note][/\A(.+)\[@resp\]\Z/, 1]
+      index = 1
+      self[:note].each{|note|
+        if note.annotation && !note.annotation.empty?
+          element = @epiDoc.bulldozePath path + "[@n='" + index.to_s + "']"
+          element.text = note.annotation
+         if note.responsibility && !note.responsibility.empty?
+           element.attributes['resp'] = note.responsibility
+         end
+         index += 1
+        end
+      }
+    end
+
+    updateEpiDocRelatedItem :revieweeList
+    updateEpiDocRelatedItem :containerList
+    
+    xpathBase = XPATH[:relatedArticleList][/\A(.+)(\/bibl)\Z/, 1]
+    xpathBibl = $2
+    @epiDoc.elements.delete_all xpathBase
+    if self[:relatedArticleList] && self[:relatedArticleList].kind_of?(Array)
+      index = 1
+      self[:relatedArticleList].each{|relatedArticle|
+
+        element = @epiDoc.bulldozePath xpathBase + "[@n='" + index.to_s + "']" + xpathBibl
+
+        if relatedArticle.series && !relatedArticle.series.empty?
+          child = REXML::Element.new 'title', element
+          child.attributes['level'] = 's'
+          child.attributes['type'] = 'short'
+          child.text = relatedArticle.series
+        end
+
+        if relatedArticle.volume && !relatedArticle.volume.empty?
+          child = REXML::Element.new 'biblScope', element
+          child.attributes['type'] = 'vol'
+          child.text = relatedArticle.volume
+        end
+        
+        if relatedArticle.number && !relatedArticle.number.empty?
+          child = REXML::Element.new 'biblScope', element
+          child.attributes['type'] = 'number'
+          child.text = relatedArticle.number
+        end
+        
+        if relatedArticle.ddb && !relatedArticle.ddb.empty?
+          child = REXML::Element.new 'idno', element
+          child.attributes['type'] = 'ddb'
+          child.text = relatedArticle.ddb
+        end
+        
+        if relatedArticle.tm && !relatedArticle.tm.empty?
+          child = REXML::Element.new 'idno', element
+          child.attributes['type'] = 'tm'
+          child.text = relatedArticle.tm
+        end
+        
+        if relatedArticle.inventory && !relatedArticle.inventory.empty?
+          child = REXML::Element.new 'idno', element
+          child.attributes['type'] = 'invNo'
+          child.text = relatedArticle.inventory
+        end        
+
+        index += 1
+      }
+    end
+
+  end
+  
+  def updateEpiDocRelatedItem key
+    xpathBase = XPATH[key][/\A(.+)(\/bibl)\Z/, 1]
+    xpathBibl = $2
+    @epiDoc.elements.delete_all xpathBase
+    if self[key] && self[key].kind_of?(Array)
+      index = 1
+      self[key].each{|relatedItem|
+        element = @epiDoc.bulldozePath xpathBase + "[@n='" + index.to_s + "']" + xpathBibl
+        ptr = REXML::Element.new('ptr')
+        ptr.attributes['target'] = relatedItem.pointer
+        element.add ptr
+        REXML::Comment.new(' ignore - start, i.e. SoSOL users may not edit this ', element)
+        getRelatedItemElements(relatedItem.pointer).each{|ignore|
+          element.add ignore
+        }
+        REXML::Comment.new(' ignore - stop ', element)
+        index += 1
+      }
+    end
+  end
+  
+  def getRelatedItemElements biblioId
+    biblioId = biblioId[/\A[^\d]*([\d\-]+)\Z/, 1] # expecting sth like http://papyri.info/biblio/12345 or like http://papyri.info/biblio/2010-123345 or just the id, i.e. 12345 or 2010-12345
+
+    git = Grit::Repo.new(CANONICAL_REPOSITORY).commits.first.tree
+    biblio = git / getBiblioPath(biblioId)
+    relatedItem = REXML::Document.new(biblio.data)
+
+    result = []
+    
+    result[result.length] = if relatedItem.elements["//title[@type='short']"]
+      relatedItem.elements["//title[@type='short']"]
+    else
+      relatedItem.elements["//title"]
+    end
+
+    ["//author", "//pubPlace", "//date", "//editor"].each{|xpath|
+      if relatedItem.elements[xpath]
+        result[result.length] = relatedItem.elements[xpath]
+      end
+    }
+
+    result
+  end
+
+  def getBiblioPath biblioId
+    if biblioId.include?('-')
+      biblioId = biblioId[/\A(\d+)-(\d+)\Z/, 1]
+      folder = $2
+      'Biblio/' + folder + '/'  + biblioId.to_s + '.xml'
+    else
+      'Biblio/' + (biblioId.to_i / 1000.0).ceil.to_s + '/'  + biblioId.to_s + '.xml'
+    end
+  end
+
   protected
 
     def populateFromEpiDoc
