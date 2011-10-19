b2169c292afa26a004df07281dfdc80ba904cb45
Jon Fox <jfox@jfox-laptop.(none)>
2011-10-19 16:41:47 -0400

    add doco remove unused methods

--- app/controllers/translation_helper_controller.rb
+++ app/controllers/translation_helper_controller.rb
@@ -2,6 +2,7 @@ class TranslationHelperController < ApplicationController
   
   layout false
   
+  # Helper Terms
 	def terms
 		#get the terms 
 		gloss = HGVTransGlossary.new
@@ -13,24 +14,15 @@ class TranslationHelperController < ApplicationController
 	def new_lang
 	end
 	
+  # Helper Linebreak
 	def linebreak
 	end
   
-  def gaplost
-  end
-  
-  def gapelliplang
-  end
-  
-  def gapellipNT
-  end
-  
-  def gapilleg
-  end
-  
+  # Helper Division Other  
   def division
   end
   
+  # Helper Tryit
   def tryit
   end
 
