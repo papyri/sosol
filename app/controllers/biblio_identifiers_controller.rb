2a6c6825ac8af2a4a587092d82c5d17d0c626769
Ryan Baumann <ryan.baumann@gmail.com>
2011-11-09 11:14:44 -0500

    Default PN layout now set in application controller

--- app/controllers/biblio_identifiers_controller.rb
+++ app/controllers/biblio_identifiers_controller.rb
@@ -1,7 +1,5 @@
 include BiblioIdentifierHelper
 class BiblioIdentifiersController < IdentifiersController
-  layout 'site'
-
   # @RB: please see to that there all the standard actions available, such as update, preview and edit and that they have access to the identifier record as well as the EpiDoc
   def edit
     find_identifier    
