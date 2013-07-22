82721d7dc78962816fc49690326cba889fc194e1
Ryan Baumann <ryan.baumann@gmail.com>
2013-07-22 10:23:46 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,8 +17,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.string   "title"
     t.string   "category"
     t.integer  "decree_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "finalizer_user_id"
     t.text     "identifier_classes"
     t.decimal  "rank"
@@ -28,8 +29,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "boards_users", :id => false, :force => true do |t|
     t.integer  "board_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "boards_users", ["board_id", "user_id"], :name => "index_boards_users_on_board_id_and_user_id", :unique => true
@@ -39,8 +40,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.integer  "user_id"
     t.integer  "identifier_id"
     t.string   "reason"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "git_hash"
     t.integer  "publication_id"
   end
@@ -51,8 +52,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.integer  "members"
     t.integer  "admins"
     t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.integer  "board_id"
     t.integer  "publication_id"
     t.integer  "end_user_id"
@@ -61,15 +62,15 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "communities_admins", :id => false, :force => true do |t|
     t.integer  "community_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "communities_members", :id => false, :force => true do |t|
     t.integer  "community_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "decrees", :force => true do |t|
@@ -77,8 +78,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.decimal  "trigger",      :precision => 5, :scale => 2
     t.string   "choices"
     t.integer  "board_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
     t.string   "tally_method"
   end
 
@@ -90,8 +91,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.string   "leiden"
     t.string   "xml"
     t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                    :null => false
+    t.datetime "updated_at",                                                    :null => false
     t.string   "urldisplay"
     t.text     "note"
     t.string   "docotype",                                  :default => "text", :null => false
@@ -107,8 +108,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.string   "when_to_send"
     t.boolean  "include_document"
     t.text     "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.boolean  "send_to_owner"
     t.boolean  "send_to_all_board_members", :default => false
     t.boolean  "include_comments",          :default => false
@@ -117,16 +118,16 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "emailers_users", :id => false, :force => true do |t|
     t.string   "emailer_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "emailers_users", ["emailer_id", "user_id"], :name => "index_emailers_users_on_emailer_id_and_user_id", :unique => true
 
   create_table "events", :force => true do |t|
     t.string   "category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "owner_id"
     t.string   "owner_type"
     t.integer  "target_id"
@@ -136,8 +137,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "identifiers", :force => true do |t|
     t.string   "name"
     t.string   "type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.integer  "publication_id"
     t.string   "alternate_name"
     t.boolean  "modified",       :default => false
@@ -148,8 +149,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
 
   create_table "publications", :force => true do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
     t.integer  "owner_id"
     t.string   "owner_type"
     t.string   "branch"
@@ -163,14 +164,14 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "user_identifiers", :force => true do |t|
     t.string   "identifier"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.boolean  "has_repository",            :default => false
     t.string   "language_prefs"
     t.boolean  "admin"
@@ -186,8 +187,8 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
   create_table "votes", :force => true do |t|
     t.string   "choice"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.integer  "publication_id"
     t.integer  "identifier_id"
     t.integer  "board_id"
