b32ede7692aeddc3227504fe96fd381f5b3c2d5b
Ryan Baumann <ryan.baumann@gmail.com>
2013-08-13 14:02:34 -0400

    Fix diff

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -121,14 +121,16 @@ class IdentifiersController < ApplicationController
     @prev_commit = commit_index > 0 ? @identifier.get_commits()[commit_index-1] : nil
     @next_commit = commit_index < (@identifier.get_commits.length - 1) ? @identifier.get_commits()[commit_index+1] : nil
     
-    @diff = @identifier.owner.repository.repo.git.diff({:unified => 5000}, "#{params[:commit_id]}^",params[:commit_id],"--",@identifier.to_path)
+    @diff = `git --git-dir="#{@identifier.owner.repository.path}" diff --unified=5000 #{params[:commit_id]}^ #{params[:commit_id]} -- "#{@identifier.to_path}"`
+    # @diff = @identifier.owner.repository.repo.git.diff({:unified => 5000}, "#{params[:commit_id]}^",params[:commit_id],"--",@identifier.to_path)
     if @diff.blank?
       # empty diff, probably pre-rename; go ahead and show the whole diff
       # TODO: actually track down renames? If an identifier is modified by
       # a repo-wide commit and then renamed, this will currently load the
       # entire (giant) commit. But most of our renames will be from new
       # texts coming in with no prior history.
-      @diff = @identifier.owner.repository.repo.git.diff({:unified => 5000}, "#{params[:commit_id]}^",params[:commit_id])
+      @diff = `git --git-dir="#{@identifier.owner.repository.path}" diff --unified=5000 #{params[:commit_id]}^ #{params[:commit_id]}`
+      # @diff = @identifier.owner.repository.repo.git.diff({:unified => 5000}, "#{params[:commit_id]}^",params[:commit_id])
     end
     Rails.logger.info(@commit.inspect)
     @is_editor_view = true
