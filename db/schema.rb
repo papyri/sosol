ecd6cd33e7029557f8418f80bf07c1fa8c39862f
Ryan Baumann <ryan.baumann@gmail.com>
2014-03-05 14:04:55 -0500

    remigrate

--- db/schema.rb
+++ db/schema.rb
@@ -95,11 +95,11 @@ ActiveRecord::Schema.define(:version => 20110811204557) do
     t.datetime "updated_at",                                                    :null => false
     t.string   "urldisplay"
     t.text     "note"
-    t.string   "docotype",                                  :default => "text"
+    t.string   "docotype",                                  :default => "text", :null => false
   end
 
   add_index "docos", ["docotype"], :name => "index_docos_on_docotype"
-  add_index "docos", ["id", "docotype"], :name => "index_docos_on_id_and_docotype"
+  add_index "docos", ["docotype"], :name => "index_docos_on_id_and_docotype"
 
   create_table "emailers", :force => true do |t|
     t.integer  "board_id"
