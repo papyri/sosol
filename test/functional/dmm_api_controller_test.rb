04c812247d9dcc97b2fb3bd3e2fbbf31aac04896
Bridget Almas <balmas@balmas-ThinkPad-X1.(none)>
2014-05-22 21:20:19 -0400

    tests! for dmm_api_create

---
+++ test/functional/dmm_api_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class DmmApiControllerTest < ActionController::TestCase
+  def setup
+    @creator = Factory(:user, :name => "Creator")
+    @request.session[:user_id] = @creator.id
+    @valid_tb = File.read(File.join(File.dirname(__FILE__), 'data', 'validtb.xml'))
+
+  end
+  
+  def teardown
+    @request.session[:user_id] = nil
+    @creator.destroy
+  end
+   
+  def test_should_create_publication_and_treebank_identifier
+    @request.env['RAW_POST_DATA'] = @valid_tb
+    post :api_item_create, :identifier_type => 'TreebankCite', :urn => 'urn:cite:perseus:testcoll'
+    assert_match(/<item>..*?<\/item>/,@response.body) 
+    assert_equal 1, assigns(:publication).identifiers.size 
+  end
+  
+   def test_should_fail_create_duplicate_identifier
+    @request.env['RAW_POST_DATA'] = @valid_tb
+    post :api_item_create, :identifier_type => 'TreebankCite', :urn => 'urn:cite:perseus:testcoll'
+    assert_match(/<item>..*?<\/item>/,@response.body) 
+    assert_equal 1, assigns(:publication).identifiers.size 
+    post :api_item_create, :identifier_type => 'TreebankCite', :urn => assigns(:publication).identifiers[0].urn_attribute
+    assert_match(/<error>Conflicting identifier/,@response.body) 
+    assert_equal 1, assigns(:publication).identifiers.size
+  end
+  
+    
+end
