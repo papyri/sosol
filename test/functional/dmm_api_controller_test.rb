ff2a08a5e95c9224659d57b0a72c3489c5d2c0c1
Bridget Almas <balmas@balmas-ThinkPad-X1.(none)>
2014-05-27 21:18:58 -0400

    WIP on PerseusDL/perseids_docs#54 and PerseusDL/perseids_docs#91

--- test/functional/dmm_api_controller_test.rb
+++ test/functional/dmm_api_controller_test.rb
@@ -30,5 +30,16 @@ class DmmApiControllerTest < ActionController::TestCase
     assert_equal 1, assigns(:publication).identifiers.size
   end
   
+  def test_should_treebank_identifier_in_existing_publication
+    @publication = Factory(:publication, :owner => @creator, :creator => @creator, :status => "new")
+      # branch from master so we aren't just creating an empty branch
+    @publication.branch_from_master
+    @cts_identifier = EpiCTSIdentifier.new_from_template(@publication,'epifacs','urn:cts:greekEpi:igvii.2543-2545.test','edition','grc')
+    assert_equal 1, @publication.identifiers.size
+    @request.env['RAW_POST_DATA'] = @valid_tb
+    post :api_item_create, :identifier_type => 'TreebankCite', :publication_id => @publication.id.to_s
+    assert_match(/<item>..*?<\/item>/,@response.body)   
+    assert_equal 2, @publication.identifiers.size
+  end
     
 end
