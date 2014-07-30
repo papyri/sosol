614552ed7b9b04f18f1f2f3f3df49f7d57613876
Bridget Almas <balmas@gmail.com>
2014-07-30 15:37:56 -0400

    tests for PerseusDL/perseids_docs#152 and PerseusDL/perseids_docs#148

--- test/functional/dmm_api_controller_test.rb
+++ test/functional/dmm_api_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class DmmApiControllerTest < ActionController::TestCase
   def setup
     @creator = Factory(:user, :name => "Creator")
+    @creatorb = Factory(:user, :name => "CreatorB")
     @request.session[:user_id] = @creator.id
     @valid_tb = File.read(File.join(File.dirname(__FILE__), 'data', 'validtb.xml'))
     @valid_align = File.read(File.join(File.dirname(__FILE__), 'data', 'validalign.xml'))
@@ -26,6 +27,28 @@ class DmmApiControllerTest < ActionController::TestCase
     assert_match(/<item>..*?<\/item>/,@response.body) 
     assert_equal 1, assigns(:publication).identifiers.size 
   end
+
+  def test_should_succeed_update
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    assert_equal 1, assigns(:publication).identifiers.size 
+    identifier = assigns(:publication).identifiers[0]
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_patch, :identifier_type => 'AlignmentCite', :id => identifier.id.to_s, :q => 's=1'
+    assert_response(:success)
+  end
+
+  def test_should_fail_update_invalid_user
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    assert_equal 1, assigns(:publication).identifiers.size 
+    identifier = assigns(:publication).identifiers[0]
+    @request.env['RAW_POST_DATA'] = @valid_align
+    @request.session[:user_id] = @creatorb.id
+    post :api_item_patch, :identifier_type => 'AlignmentCite', :id => identifier.id.to_s
+    assert_response(403)
+  end
+  
   
    def test_should_fail_create_duplicate_identifier
     @request.env['RAW_POST_DATA'] = @valid_tb
@@ -37,7 +60,7 @@ class DmmApiControllerTest < ActionController::TestCase
     assert_equal 1, assigns(:publication).identifiers.size
   end
   
-  def test_should_treebank_identifier_in_existing_publication
+  def test_should_create_treebank_identifier_in_existing_publication
     @publication = Factory(:publication, :owner => @creator, :creator => @creator, :status => "new")
       # branch from master so we aren't just creating an empty branch
     @publication.branch_from_master
@@ -46,5 +69,42 @@ class DmmApiControllerTest < ActionController::TestCase
     assert_match(/<item>..*?<\/item>/,@response.body)   
     assert_equal 1, @publication.identifiers.size
   end
+
+  def test_should_succeed_comment_create
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment=>"test", :reason => "review"
+    assert_match(/"comment_id":(.*?),/,@response.body)
     
+  end
+
+  def test_should_fail_comment_update_wrong_user
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment=>"test", :reason => "review"
+    comment_id = @response.body.match(/"comment_id":(.*?),/).captures
+    @request.session[:user_id] = @creatorb.id
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment_id => comment_id, :comment=>"test update", :reason => "review"
+    assert_response(403)
+  end
+  
+  def test_should_succeed_comment_update_owner_user
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment=>"test", :reason => "review"
+    comment_id = @response.body.match(/"comment_id":(.*?),/).captures
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment_id => comment_id, :comment=>"test update", :reason => "review"
+    assert_match(/"comment_id":#{comment_id}/,@response.body)
+    assert_match(/"comment":"test update"/,@response.body)
+  end
+
+  def test_should_fail_invalid_reason
+    @request.env['RAW_POST_DATA'] = @valid_align
+    post :api_item_create, :identifier_type => 'AlignmentCite'
+    post :api_item_comments_post, :identifier_type => 'AlignmentCite', :id => assigns(:publication).identifiers[0].id.to_s, :comment=>"test", :reason => "spam"
+    assert_response(:error)
+  end
+
+  # TODO TEST BOARD OWNERSHIP UPDATE and COMMENT
+
 end
