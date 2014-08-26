1109e8a11c85d3c303301cc81d44700fb4a2b379
Bridget Almas <balmas@gmail.com>
2014-08-26 15:30:22 -0400

    more for #35 - self-signup communities should show up in selection list on submit form even if you already belong to one

--- public/javascripts/communities.js
+++ public/javascripts/communities.js
@@ -1,9 +1,11 @@
 function confirmSignup() {
      var community = jQuery('#community_id option:selected');
+     var is_signup = jQuery("#do_community_signup").val().split(/,/);
      if (community.val() == 0) {
          return confirm("Selecting this option will submit your publication to the master board. If you are submitting as part of a class or community please select the correct one from the list");
-     } else {
+     } else if (jQuery.inArray(community.val(),is_signup) >= 0) {
          return confirm("Selecting this option will sign you up for the "+ community.text() + " community.");
+     } else {
+         return true;
      }
-
 }
