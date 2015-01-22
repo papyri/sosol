d456c9bde426a677003529b0b08f230b0c22f196
Bridget Almas <balmas@gmail.com>
2015-01-22 12:41:42 -0500

    changes for integration with imgcollect toolset
    update to use perseids fuseki endpoint which services image collection
    data that reverses the order of the statement which links an image to
    the text it represents and includes statements which link an image
    collection explicitly to the endpoints which serve it so that we can have
    items from multiple sources served by the same triple store.

--- public/javascripts/perseids-ld.js
+++ public/javascripts/perseids-ld.js
@@ -2,48 +2,117 @@ var PerseidsLD;
 
 PerseidsLD = PerseidsLD || {};
 
+// this query syntax support specification of either
+// a subject or an object (but not both) 
 PerseidsLD.do_simple_query  = function() {
 
-	jQuery(".perseidsld_query_obj_simple").each(
-		function() {
-			var query_parent = this;
-			var sbj = jQuery(this).attr("data-sbj");
-			var verb = jQuery(this).attr("data-verb");
-			var dataset = jQuery(this).attr("data-set");
-			var queryuri = jQuery(this).attr("data-queryuri");
-			var formatter = jQuery(this).attr("data-formatter");
+    jQuery(".perseidsld_query_obj_simple").each(
+        function() {
+            var query_parent = this;
+            var sbj = jQuery(this).attr("data-sbj");
+            var obj = jQuery(this).attr("data-obj");
+            var verb = jQuery(this).attr("data-verb");
+            var dataset = jQuery(this).attr("data-set");
+            var queryuri = jQuery(this).attr("data-queryuri");
+            var formatter = jQuery(this).attr("data-formatter");
+            var eplookup = jQuery(this).attr("data-endpoint-verb");
 
-			// TODO default formatter ??
-			
+            // TODO default formatter ??
+            
             if (!queryuri) { 
-            	queryuri = jQuery("meta[name='SoSOL-Sparql-Endpoint']").attr("content");
+                queryuri = jQuery("meta[name='SoSOL-Sparql-Endpoint']").attr("content");
  
-           	}
-			var dataset_query = "";
-			if (dataset) {
-				dataset_query = "from <" + dataset + "> "
-			}
-			if (queryuri && sbj && verb) {
-				
-				jQuery.get(queryuri
-					+ encodeURIComponent( 
-						"select ?object "
-	        			+ dataset_query
-	        			+ "where { <" + sbj + "> " + "<"  + verb + "> ?object}")
-	        		+ "&format=json", 
-	        		function(data) {
-	        			var results = [];
-	            		if (data.results.bindings.length > 0) {
-	                		jQuery.each(data.results.bindings, function(i, row) {
-	                			results.push(row.object.value);
-	                		})
-	            		}
-	            		PerseidsTools.LDResults[formatter](query_parent,results);
-				
-	    			}, "json");
-    		}	
-			
+            }
+            var dataset_query = "";
+            if (dataset) {
+                dataset_query = "from <" + dataset + "> "
+            }
+            if (queryuri && (sbj || obj) && verb) {
+       
+                var sovquery = "";
+                if (sbj) {
+                    sovquery = "<" + sbj + "> " + "<"  + verb + "> ?result ."
+                } else {
+                    sovquery = "?result " + "<"  + verb + "> <" + obj +"> ."
+                }
+                var epquery = "";
+                // this is an awful hack to pull in the endpoint url for the collection with the results
+                // which makes this code only work with cite collections that use this terminology
+                // we should probably first pull in teh endpoints for all the collections and then
+                // map the results, like we do in the preview but this avoids one more call to the sparql endpoint
+                // and is backwards compatible with other uses of the library (like the epifacs demo)
+                if (eplookup) {
+                  epquery = "?result <http://www.homermultitext.org/cite/rdf/belongsTo> ?collection . ?collection <" + eplookup + "> ?url .";
+                }
+                 
+                jQuery.get(queryuri
+                    + encodeURIComponent( 
+                        "select ?result ?url "
+                        + dataset_query
+                        + "where {" 
+                        + sovquery
+                        + epquery
+                        + "}") 
+                        + "&format=json", 
+                    function(data) {
+                        var results = [];
+                        if (data.results.bindings.length > 0) {
+                            jQuery.each(data.results.bindings, 
+                                function(i, row) {
+                                    var url = row.url && row.url.value ? row.url.value : "";
+                                    results.push(url + row.result.value); 
+                            })
+                        }
+                        PerseidsTools.LDResults[formatter](query_parent,results);
+                    }, "json");
+            }    
+            
     });
 };
 
+// query given only a verb
+PerseidsLD.do_verb_query  = function() {
+    jQuery(".perseidsld_query_verb_simple").each(
+        function() {
+            var query_parent = this;
+            var verb = jQuery(this).attr("data-verb");
+            var dataset = jQuery(this).attr("data-set");
+            var queryuri = jQuery(this).attr("data-queryuri");
+            var formatter = jQuery(this).attr("data-formatter");
+
+            // TODO default formatter ??
+            if (!queryuri) { 
+                queryuri = jQuery("meta[name='SoSOL-Sparql-Endpoint']").attr("content");
+ 
+            }
+            var dataset_query = "";
+            if (dataset) {
+                dataset_query = "from <" + dataset + "> ";
+            }
+            if (queryuri && verb) {
+       
+                var sovquery = "?sbj <" + verb + "> ?obj .";
+
+                jQuery.get(queryuri
+                    + encodeURIComponent( 
+                        "select ?sbj ?obj "
+                        + dataset_query
+                        + "where {" 
+                        + sovquery
+                        + "}") 
+                        + "&format=json", 
+                    function(data) {
+                        var results = {};
+                        if (data.results.bindings.length > 0) {
+                            jQuery.each(data.results.bindings, 
+                                function(i, row) {
+                                    results[row.sbj.value] = decodeURI(row.obj.value).replace('&amp;','&');
+                            })
+                        }
+                        PerseidsTools.LDResults[formatter](query_parent,results);
+                    }, "json");
+            }    
+            
+    });
+}
 
--- public/javascripts/perseids-tools.js
+++ public/javascripts/perseids-tools.js
@@ -4,79 +4,53 @@ PerseidsTools = PerseidsTools || {};
 
 PerseidsTools.LDResults = {};
 
+PerseidsTools.endpointMap = {};
+
+PerseidsTools.LDResults.set_endpoint_map = function(a_elem, a_results) {
+    var key = jQuery(a_elem).attr('data-result-id');
+    PerseidsTools.endpointMap[key] = a_results;
+};
+
 // adds thumbnails for each returned image urn, and triggers the IMGSPECT-LINKS_LOADED event
 PerseidsTools.LDResults.imgspect_link = function( _elem, _results ) {
-	var url = jQuery("#Cite_Image_Service").attr("data-content");
-	if (url) {
-		jQuery( _elem ).append( '<div id="imgspectHint">Click an image to inspect.</div>' );
-		if ( _results.length == 0 ) {
-			jQuery( '.perseidsld_query_obj_simple' ).remove();
-		}
-		for ( var i=0, ii=_results.length; i<ii; i++ ) {
-			var imgUrn = '<a id="imgUrn_'+i+'" class="imgUrn" href="' + url + _results[i] + '&w=3000"><img src="'+ url + _results[i] + '&w=100"/></a>';
-			jQuery( _elem ).append( imgUrn );
-			jQuery( document ).trigger( 'IMGSPECT-LINK_LOADED',['imgUrn_'+i] );
-		}
-	}
+    var imgsvc = jQuery("#Cite_Image_Service").attr("data-content");
+    jQuery( _elem ).append( '<div id="imgspectHint">Click an image to inspect.</div>' );
+    if ( _results.length == 0 ) {
+        jQuery( '.perseidsld_query_obj_simple' ).remove();
+    }
+    for ( var i=0, ii=_results.length; i<ii; i++ ) {
+        var src = _results[i];
+        if (src.match(/^urn:cite:/) != null && imgsvc) {
+            src = imgsvc + src;
+        }
+        var imgUrn = '<a id="imgUrn_'+i+'" class="imgUrn" href="' +  src + '"><img src="'+ src + '" height="100px"/></a>';
+        jQuery( _elem ).append( imgUrn );
+        jQuery( document ).trigger( 'IMGSPECT-LINK_LOADED',['imgUrn_'+i] );
+    }
 }
-
-PerseidsTools.LDResults.make_ICT_link  = function(a_parentElem,a_results) {
-
-	//  - if there is just one image, it should automatically populate the frame
-	//  - if there is more than one, it should offer a select box to populate the frame
-	//  - if there is none, it should either hide the frame and expand the area for text entry
-	//        or it should offer the user the ability to supply their own url and save as a link
-	var ictUrl = jQuery("meta[name='Perseids-ICT-Url']").attr("content");
-	
-	var num_results = a_results.length;
-	if (num_results == 0) {
-		jQuery(a_parentElem).append('<p>No images available for this text. In the future you will be able to search an external triple store for related images</p>');
-		jQuery("#ict_frame").hide();
-	} else if(num_results > 1) {
-		jQuery(a_parentElem).append('<select name="ict_select"></select>');
-		var select = jQuery("select",a_parentElem);
-		select.append('<option value="">Select an image...</option>');
-		for (var i=0; i<num_results; i++) {
-			var val = a_results[i];
-			if (val.match(/^urn:cite:/) != null && ictUrl) {
-				// TODO there could be some lookup functions here to use a different ICT per urn namespace
-				val = ictUrl + val;
-			}
-			select.append('<option value="' + val + '">' + val + '</option>');
-		}
-		select.change(
-			function() {
-				jQuery("#ict_frame").attr("src",jQuery("option:selected",this).val()); 
-				return true;
-			}
-		);
-	} else {
-		val = a_results[0];
-		if (val.match(/^urn:cite:/) != null && ictUrl) {
-				// TODO there could be some lookup functions here to use a different ICT per urn namespace
-				val = ictUrl + val;
-		}
-		jQuery("#ict_frame").attr("src",val); 
-	}
-	
-};
 
 // click handler for a span which has a data-facs attribute 
 PerseidsTools.do_facs_link = function(a_elem) {
-	var citeUrl = jQuery("#Cite_Image_Service").attr("data-content");
-	var uri = jQuery(a_elem).attr("data-facs");
-	var url = null;
-	// if the facs value is a full URL, just use it
-	if (uri.match(/^http/)) {
-		url = uri;
-	// if the facs value references a CITE urn, try to bring it up in the Image Viewer
-	} else if (uri.match(/^urn:cite:/)) {
-		url = citeUrl + uri;
-	} else {
-		// otherwise do nothing
-	}
-	if (url != null) {
-		jQuery('#ict_frame').attr("src",url);
-	}
-	return false;
+    var citeUrl = jQuery("#Cite_Image_Service").attr("data-content");
+    var uri = jQuery(a_elem).attr("data-facs");
+    var url = null;
+    // if the facs value is a full URL, just use it
+    if (uri.match(/^http/)) {
+        url = uri;
+    // if the facs value references a CITE urn, try to bring it up in the Image Viewer
+    } else if (uri.match(/^urn:cite:/)) {
+        var collection = uri.match(/^(urn:cite:.*?)\./);
+        if (collection && PerseidsTools.endpointMap.viewer[collection[1]]) {
+            url = PerseidsTools.endpointMap.viewer[collection[1]] + uri;
+        } else {
+            url = citeUrl + uri;
+        }
+    } else {
+        // otherwise do nothing
+    }
+    if (url != null) {
+        jQuery('#ict_frame').attr("src",url);
+    }
+    return false;
 };
+
