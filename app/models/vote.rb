e4ba9c90852976de889f881fbee2eae5e6ed4349
Hugh Cayless <philomousos@gmail.com>
2015-05-07 08:36:29 -0400

    Trying out asynchronous vote tally.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,29 +4,31 @@ class Vote < ActiveRecord::Base
   belongs_to :identifier
   belongs_to :user
   belongs_to :board
-  
+
 
   #Ensures vote is tallied after it is saved.
   after_save :tally
-  
+
   #Ensures vote is tallied for publication.
   def tally
     if self.identifier # && self.identifier.status == "editing"
       #need to tally votes and see if any action will take place
       #should only be voting while the publication is owned by the correct board
       #related_votes = self.identifier.votes
-      
+
       #choose vote based on publication votes
-      #TODO add votes to be related to publication 
+      #TODO add votes to be related to publication
       #related_votes = self.publication.votes
-      
+
       #todo add check to ensure board is correct
       #decree_action = self.publication.tally_votes(related_votes)
       #self.publication.tally_votes(related_votes)
-      
+
       #let publication decide how to access votes
-      self.publication.tally_votes()
-    end    
+      Thread.new do
+        self.publication.tally_votes()
+      end
+    end
     return nil
   end
 
