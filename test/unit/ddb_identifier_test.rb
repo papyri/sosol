97cd20d19d83370c1c4d72965d8cde7afc9ef941
Ryan Baumann <ryan.baumann@gmail.com>
2015-06-17 15:13:51 -0400

    Only test configured identifier classes

--- test/unit/ddb_identifier_test.rb
+++ test/unit/ddb_identifier_test.rb
@@ -1,140 +1,142 @@
 require 'test_helper'
 
-class DDBIdentifierTest < ActiveSupport::TestCase
-  context "collection names" do
-    setup do
-      @collection_names = DDBIdentifier.collection_names
-    end
-    
-    should "be unique" do
-      assert_equal @collection_names.uniq, @collection_names
-    end
-  end
-  
-  context "identifier renaming" do
-    setup do
-      @creator = FactoryGirl.create(:user, :name => "Creator")
-      @publication = FactoryGirl.create(:publication, :owner => @creator, :creator => @creator, :status => "new")
-      # branch from master so we aren't just creating an empty branch
-      @publication.branch_from_master
-      
-      @ddb_identifier = DDBIdentifier.new_from_template(@publication)
-      @original_name = @ddb_identifier.name
-      @original_content = @ddb_identifier.content
-      @original_path = @ddb_identifier.to_path
-    end
-    
-    teardown do
-      @publication.destroy
-      @creator.destroy
-    end
-    
-    should "raise an error when the destination exists" do
-      assert_raise RuntimeError do
-        @ddb_identifier.rename('papyri.info/ddbdp/bgu;1;1')
-      end
-    end
-    
-    context "with a valid target" do
+if Sosol::Application.config.site_identifiers.split(',').include?('DDBIdentifier')
+  class DDBIdentifierTest < ActiveSupport::TestCase
+    context "collection names" do
       setup do
-        @new_name = "papyri.info/ddbdp/bgu;1;1000"
-        @ddb_identifier.rename(@new_name)
+        @collection_names = DDBIdentifier.collection_names
       end
       
-      should "have the new name" do
-        assert_equal @new_name, @ddb_identifier.name
-      end
-      
-      should "have the correct new title" do
-        expected_title = FactoryGirl.build(:DDBIdentifier, :name => @new_name).titleize
-        assert_equal expected_title, @ddb_identifier.title
-      end
-      
-      should "have the correct content at the new path" do
-        assert_equal @original_content, @ddb_identifier.content
-      end
-      
-      should "have nothing at the original path" do
-        assert_equal nil, @creator.repository.get_file_from_branch(@original_path, @publication.branch)
+      should "be unique" do
+        assert_equal @collection_names.uniq, @collection_names
       end
     end
-  end
-  
-  context "identifier mapping" do
-    setup do
-      @path_prefix = DDBIdentifier::PATH_PREFIX
-    end
     
-    # TODO: write a DDBIdentifier method for reversing a collection name
-    # into a series number and use that here instead?
-    # (e.g. #{DDBIdentifier.collection_to_series('bgu')} instead of 0001)
-    
-    should "map the first identifier" do
-      bgu_1_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;1;1")
-      assert_path_equal %w{bgu bgu.1 bgu.1.1.xml}, bgu_1_1.to_path
-    end
-    
-    should "map ambiguous collections" do
-      bgu_ppetr_2_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr;2;1")
-      assert_path_equal %w{p.petr p.petr.2 p.petr.2.1.xml}, bgu_ppetr_2_1.to_path
+    context "identifier renaming" do
+      setup do
+        @creator = FactoryGirl.create(:user, :name => "Creator")
+        @publication = FactoryGirl.create(:publication, :owner => @creator, :creator => @creator, :status => "new")
+        # branch from master so we aren't just creating an empty branch
+        @publication.branch_from_master
+        
+        @ddb_identifier = DDBIdentifier.new_from_template(@publication)
+        @original_name = @ddb_identifier.name
+        @original_content = @ddb_identifier.content
+        @original_path = @ddb_identifier.to_path
+      end
       
-      bgu_ppetr2_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr.2;;1")
-      assert_path_equal %w{p.petr.2 p.petr.2.1.xml}, bgu_ppetr2_1.to_path
+      teardown do
+        @publication.destroy
+        @creator.destroy
+      end
+      
+      should "raise an error when the destination exists" do
+        assert_raise RuntimeError do
+          @ddb_identifier.rename('papyri.info/ddbdp/bgu;1;1')
+        end
+      end
+      
+      context "with a valid target" do
+        setup do
+          @new_name = "papyri.info/ddbdp/bgu;1;1000"
+          @ddb_identifier.rename(@new_name)
+        end
+        
+        should "have the new name" do
+          assert_equal @new_name, @ddb_identifier.name
+        end
+        
+        should "have the correct new title" do
+          expected_title = FactoryGirl.build(:DDBIdentifier, :name => @new_name).titleize
+          assert_equal expected_title, @ddb_identifier.title
+        end
+        
+        should "have the correct content at the new path" do
+          assert_equal @original_content, @ddb_identifier.content
+        end
+        
+        should "have nothing at the original path" do
+          assert_equal nil, @creator.repository.get_file_from_branch(@original_path, @publication.branch)
+        end
+      end
     end
     
-    # TODO: update this test for + solution
-    # should "map files with '+' in the identifier" do
-    #   chla_5_299FrA_B_C = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/0279:5:299FrA+B+C")
-    #   assert_path_equal %w{chla chla.5 chla.5.299FrA+B+C.xml}, chla_5_299FrA_B_C.to_path
-    # end
-    
-    should "map files with ',' in the identifier" do
-      bgu_13_2230_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;13;2230,1")
-      assert_path_equal %w{bgu bgu.13 bgu.13.2230-1.xml}, bgu_13_2230_1.to_path
+    context "identifier mapping" do
+      setup do
+        @path_prefix = DDBIdentifier::PATH_PREFIX
+      end
+      
+      # TODO: write a DDBIdentifier method for reversing a collection name
+      # into a series number and use that here instead?
+      # (e.g. #{DDBIdentifier.collection_to_series('bgu')} instead of 0001)
+      
+      should "map the first identifier" do
+        bgu_1_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;1;1")
+        assert_path_equal %w{bgu bgu.1 bgu.1.1.xml}, bgu_1_1.to_path
+      end
+      
+      should "map ambiguous collections" do
+        bgu_ppetr_2_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr;2;1")
+        assert_path_equal %w{p.petr p.petr.2 p.petr.2.1.xml}, bgu_ppetr_2_1.to_path
+        
+        bgu_ppetr2_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/p.petr.2;;1")
+        assert_path_equal %w{p.petr.2 p.petr.2.1.xml}, bgu_ppetr2_1.to_path
+      end
+      
+      # TODO: update this test for + solution
+      # should "map files with '+' in the identifier" do
+      #   chla_5_299FrA_B_C = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/0279:5:299FrA+B+C")
+      #   assert_path_equal %w{chla chla.5 chla.5.299FrA+B+C.xml}, chla_5_299FrA_B_C.to_path
+      # end
+      
+      should "map files with ',' in the identifier" do
+        bgu_13_2230_1 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/bgu;13;2230,1")
+        assert_path_equal %w{bgu bgu.13 bgu.13.2230-1.xml}, bgu_13_2230_1.to_path
+      end
+      
+      should "map files with '/' in the identifier" do
+        o_bodl_2_1964_1967 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/o.bodl;2;1964/1967")
+        assert_path_equal %w{o.bodl o.bodl.2 o.bodl.2.1964_1967.xml}, o_bodl_2_1964_1967.to_path
+      end
+      
+      # Irrelevant now?
+      # should "raise an error if series is non-existent" do
+      #   no_series = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/;1;1")
+      #   assert_raise RuntimeError do
+      #     no_series.to_path
+      #   end
+      #   
+      #   nonsense_series = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/3735928559:1:1")
+      #   assert_raise RuntimeError do
+      #     nonsense_series.to_path
+      #   end
+      # end
+      
+      # TODO: populate this test
+      should "raise an error for illegal characters" do
+        assert true
+      end
+      
+      # NOTE: this is an extremely slow exhaustive test you should probably
+      # only run to discover additional tests that need to be written.
+      # As of 8/20/09, this will fail for every file in p.vind.eirene.
+      # See: http://idp.atlantides.org/trac/idp/ticket/82
+      # should "map every identifier found in a canonical DDB file back to the same file" do
+      #       canonical = Repository.new
+      #       files = canonical.get_all_files_from_path_on_branch(@path_prefix)
+      #       files.each do |filename|
+      #         Rails.logger.info "Filename: #{filename}"
+      #         # reusing the same Repository instance here leads to an eventual
+      #         # Grit timeout; possible Grit bug/leak?
+      #         xml_content = REXML::Document.new(Repository.new.get_file_from_branch(filename))
+      #         ddb_hybrid = REXML::XPath.first(xml_content, '/TEI//idno[@type = "ddb-hybrid"]').text
+      #         Rails.logger.info "DDB Hybrid: #{ddb_hybrid}"
+      #         identifier_name = "papyri.info/ddbdp/#{ddb_hybrid}"
+      #         Rails.logger.info "Identifier to hash: #{NumbersRDF::NumbersHelper.identifiers_to_hash(NumbersRDF::NumbersHelper.identifier_to_identifiers(identifier_name)).inspect}"
+      #         this_ddb = FactoryGirl.build(:DDBIdentifier, :name => identifier_name)
+      #         assert_equal filename, this_ddb.to_path
+      #       end
+      #     end
     end
-    
-    should "map files with '/' in the identifier" do
-      o_bodl_2_1964_1967 = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/o.bodl;2;1964/1967")
-      assert_path_equal %w{o.bodl o.bodl.2 o.bodl.2.1964_1967.xml}, o_bodl_2_1964_1967.to_path
-    end
-    
-    # Irrelevant now?
-    # should "raise an error if series is non-existent" do
-    #   no_series = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/;1;1")
-    #   assert_raise RuntimeError do
-    #     no_series.to_path
-    #   end
-    #   
-    #   nonsense_series = FactoryGirl.build(:DDBIdentifier, :name => "papyri.info/ddbdp/3735928559:1:1")
-    #   assert_raise RuntimeError do
-    #     nonsense_series.to_path
-    #   end
-    # end
-    
-    # TODO: populate this test
-    should "raise an error for illegal characters" do
-      assert true
-    end
-    
-    # NOTE: this is an extremely slow exhaustive test you should probably
-    # only run to discover additional tests that need to be written.
-    # As of 8/20/09, this will fail for every file in p.vind.eirene.
-    # See: http://idp.atlantides.org/trac/idp/ticket/82
-    # should "map every identifier found in a canonical DDB file back to the same file" do
-    #       canonical = Repository.new
-    #       files = canonical.get_all_files_from_path_on_branch(@path_prefix)
-    #       files.each do |filename|
-    #         Rails.logger.info "Filename: #{filename}"
-    #         # reusing the same Repository instance here leads to an eventual
-    #         # Grit timeout; possible Grit bug/leak?
-    #         xml_content = REXML::Document.new(Repository.new.get_file_from_branch(filename))
-    #         ddb_hybrid = REXML::XPath.first(xml_content, '/TEI//idno[@type = "ddb-hybrid"]').text
-    #         Rails.logger.info "DDB Hybrid: #{ddb_hybrid}"
-    #         identifier_name = "papyri.info/ddbdp/#{ddb_hybrid}"
-    #         Rails.logger.info "Identifier to hash: #{NumbersRDF::NumbersHelper.identifiers_to_hash(NumbersRDF::NumbersHelper.identifier_to_identifiers(identifier_name)).inspect}"
-    #         this_ddb = FactoryGirl.build(:DDBIdentifier, :name => identifier_name)
-    #         assert_equal filename, this_ddb.to_path
-    #       end
-    #     end
   end
 end
--- test/unit/oac_identifier_test.rb
+++ test/unit/oac_identifier_test.rb
@@ -1,139 +1,141 @@
 require 'test_helper'
 
-class OACIdentifierTest < ActiveSupport::TestCase
-  
-  context "identifier test" do
-    setup do
-      @creator = FactoryGirl.create(:user, :name => "CreatorA")
-      @creator2 = FactoryGirl.create(:user, :name => "CreatorB")
-      @publication = FactoryGirl.create(:publication, :owner => @creator, :creator => @creator, :status => "new")
-      # branch from master so we aren't just creating an empty branch
-      @publication.branch_from_master
-      @parent = FactoryGirl.create(:EpiCTSIdentifier, :title => 'Test Text')
-      @oac_identifier = OACIdentifier.new_from_template(@publication,@parent)
-      @test_uri1 = 'http://data.perseus.org/annotations/abcd'
-      @test_uri2 = 'http://data.perseus.org/annotations/efgh'
-      @test_tb1 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg001.perseus-grc1:1.1'
-      @test_tb2 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg002.perseus-grc1:1.1'
-      @test_title = 'Test Annotation'
-      @creator_uri = Sosol::Application.config.site_user_namespace + @creator.name
-      @creator2_uri = Sosol::Application.config.site_user_namespace + @creator2.name
-      @oac_identifier.add_annotation(@test_uri1,[@test_tb1],[@test_tb2],"oa:linking",@creator_uri,"test:agent",'test add annotation')
-    end
+if Sosol::Application.config.site_identifiers.split(',').include?('OACIdentifier')
+  class OACIdentifierTest < ActiveSupport::TestCase
     
-    teardown do
-      unless (@publication.nil?)
-        @publication.destroy
+    context "identifier test" do
+      setup do
+        @creator = FactoryGirl.create(:user, :name => "CreatorA")
+        @creator2 = FactoryGirl.create(:user, :name => "CreatorB")
+        @publication = FactoryGirl.create(:publication, :owner => @creator, :creator => @creator, :status => "new")
+        # branch from master so we aren't just creating an empty branch
+        @publication.branch_from_master
+        @parent = FactoryGirl.create(:EpiCTSIdentifier, :title => 'Test Text')
+        @oac_identifier = OACIdentifier.new_from_template(@publication,@parent)
+        @test_uri1 = 'http://data.perseus.org/annotations/abcd'
+        @test_uri2 = 'http://data.perseus.org/annotations/efgh'
+        @test_tb1 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg001.perseus-grc1:1.1'
+        @test_tb2 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg002.perseus-grc1:1.1'
+        @test_title = 'Test Annotation'
+        @creator_uri = Sosol::Application.config.site_user_namespace + @creator.name
+        @creator2_uri = Sosol::Application.config.site_user_namespace + @creator2.name
+        @oac_identifier.add_annotation(@test_uri1,[@test_tb1],[@test_tb2],"oa:linking",@creator_uri,"test:agent",'test add annotation')
       end
-      unless (@creator.nil?)
-        @creator.destroy
+      
+      teardown do
+        unless (@publication.nil?)
+          @publication.destroy
+        end
+        unless (@creator.nil?)
+          @creator.destroy
+        end
       end
-    end
-    
-    should "retrieve the target by urn" do
-        matches = @oac_identifier.matching_targets("#{Regexp.quote('urn:cts:greekLang:tlg0012.tlg001.perseus-grc1:1.1#')}?",@creator_uri)
-        assert matches.size == 1
-        Rails.logger.info("Match = #{matches.inspect}")
-        assert matches[0]['target'] = @test_uri1
+      
+      should "retrieve the target by urn" do
+          matches = @oac_identifier.matching_targets("#{Regexp.quote('urn:cts:greekLang:tlg0012.tlg001.perseus-grc1:1.1#')}?",@creator_uri)
+          assert matches.size == 1
+          Rails.logger.info("Match = #{matches.inspect}")
+          assert matches[0]['target'] = @test_uri1
+          
+      end
+      
+      should "retrieve the target by edition urn" do
+          assert @oac_identifier.matching_targets('urn:cts:greekLang:tlg0012.tlg001.perseus-grc1',@creator_uri).size == 1
+      end
+      
+      should "have annotations" do 
+        assert OacHelper::get_all_annotations(@oac_identifier.rdf).size > 0
+        assert @oac_identifier.has_anyannotation?
+
+      end
+
+      should "have an annotation by creator" do 
+        assert OacHelper::get_annotators(@oac_identifier.get_annotation(@test_uri1))[0] == @creator_uri
+      end
+
+      should "have one annotation by the creator" do 
+        assert OacHelper::get_annotations_by_annotator(@oac_identifier.rdf,@creator_uri).size == 1
+        assert @oac_identifier.get_annotations().size == 1
+      end
+
+      should "have the target" do
+          Rails.logger.info("In should have")
+          assert @oac_identifier.has_target?(@test_tb1,@creator_uri)
+      end
+      
+      
+      should "not have the target" do
+          assert !@oac_identifier.has_target?(@test_tb2,@creator_uri)
+      end
         
-    end
-    
-    should "retrieve the target by edition urn" do
-        assert @oac_identifier.matching_targets('urn:cts:greekLang:tlg0012.tlg001.perseus-grc1',@creator_uri).size == 1
-    end
-    
-    should "have annotations" do 
-      assert OacHelper::get_all_annotations(@oac_identifier.rdf).size > 0
-      assert @oac_identifier.has_anyannotation?
-
-    end
-
-    should "have an annotation by creator" do 
-      assert OacHelper::get_annotators(@oac_identifier.get_annotation(@test_uri1))[0] == @creator_uri
-    end
-
-    should "have one annotation by the creator" do 
-      assert OacHelper::get_annotations_by_annotator(@oac_identifier.rdf,@creator_uri).size == 1
-      assert @oac_identifier.get_annotations().size == 1
-    end
-
-    should "have the target" do
-        Rails.logger.info("In should have")
-        assert @oac_identifier.has_target?(@test_tb1,@creator_uri)
-    end
-    
-    
-    should "not have the target" do
-        assert !@oac_identifier.has_target?(@test_tb2,@creator_uri)
-    end
-      
-    should "raise an error when the annotation exists" do
-      assert_raise RuntimeError do
-      @oac_identifier.add_annotation(@test_uri1,[@test_tb1],[@test_tb2],@test_title,@creator_uri,nil,'test add annotation')
-      end
-    end
-    
-    should "raise an error when the annotation does not exist" do
-      assert_raise RuntimeError do
-      @oac_identifier.add_annotation(@test_uri1,[@test_tb2],[@test_tb2],@test_title,@creator_uri,nil,'test add annotation')
-      end
-    end
-    
-    should "find the parent identifier" do
-      assert OACIdentifier.find_from_parent(@publication,@parent) == @oac_identifier
-      
-    end
-    
-    context "with a new annotation " do
-      setup do
-        @oac_identifier.add_annotation(@test_uri2,[@test_tb2],[@test_tb1],@test_title,@creator_uri,nil,'test add annotation')
+      should "raise an error when the annotation exists" do
+        assert_raise RuntimeError do
+        @oac_identifier.add_annotation(@test_uri1,[@test_tb1],[@test_tb2],@test_title,@creator_uri,nil,'test add annotation')
+        end
       end
       
-      should "have the new annotation" do
-        assert @oac_identifier.has_target?(@test_tb2,@creator_uri)
-      end   
-      
-      should "retrieve the annotation" do
-        annotation =  @oac_identifier.get_annotation(@test_uri2)
-        assert ! annotation.nil?
-        assert OacHelper::get_targets(annotation).size == 1
-        assert OacHelper::get_targets(annotation)[0] == @test_tb2
-        assert OacHelper::get_bodies(annotation)[0] == @test_tb1
-        assert OacHelper::get_motivation(annotation) == @test_title
-        assert OacHelper::get_annotators(annotation)[0] == @creator_uri
-        assert OacHelper::get_annotated_at(annotation) != ""
+      should "raise an error when the annotation does not exist" do
+        assert_raise RuntimeError do
+        @oac_identifier.add_annotation(@test_uri1,[@test_tb2],[@test_tb2],@test_title,@creator_uri,nil,'test add annotation')
+        end
       end
       
-      should "delete the annotation" do
-        @oac_identifier.delete_annotation(@test_uri2,"test delete")
-        assert @oac_identifier.get_annotation(@test_uri2).nil?
-        assert ! @oac_identifier.get_annotation(@test_uri1).nil?
-      end
-    end
-    
-    context "with an updated annotation " do
-      setup do
-        @oac_identifier.update_annotation(@test_uri1,[@test_tb2],[@test_tb2],@test_title,@creator_uri,nil,'test update annotation')
+      should "find the parent identifier" do
+        assert OACIdentifier.find_from_parent(@publication,@parent) == @oac_identifier
+        
       end
       
-      should "have the updated annotation" do
-        assert @oac_identifier.has_target?(@test_tb2,@creator_uri)
+      context "with a new annotation " do
+        setup do
+          @oac_identifier.add_annotation(@test_uri2,[@test_tb2],[@test_tb1],@test_title,@creator_uri,nil,'test add annotation')
+        end
+        
+        should "have the new annotation" do
+          assert @oac_identifier.has_target?(@test_tb2,@creator_uri)
+        end   
+        
+        should "retrieve the annotation" do
+          annotation =  @oac_identifier.get_annotation(@test_uri2)
+          assert ! annotation.nil?
+          assert OacHelper::get_targets(annotation).size == 1
+          assert OacHelper::get_targets(annotation)[0] == @test_tb2
+          assert OacHelper::get_bodies(annotation)[0] == @test_tb1
+          assert OacHelper::get_motivation(annotation) == @test_title
+          assert OacHelper::get_annotators(annotation)[0] == @creator_uri
+          assert OacHelper::get_annotated_at(annotation) != ""
+        end
+        
+        should "delete the annotation" do
+          @oac_identifier.delete_annotation(@test_uri2,"test delete")
+          assert @oac_identifier.get_annotation(@test_uri2).nil?
+          assert ! @oac_identifier.get_annotation(@test_uri1).nil?
+        end
       end
       
-      should "not have the old annotation" do
-        assert !@oac_identifier.has_target?(@test_tb1,@creator_uri)
-      end   
-    end
-    
-    context "with an external agent " do
-      setup do 
-        @oac_identifier.add_annotation(@test_uri2,[@test_tb2],[@test_tb1],@test_title,@creator_uri,'http://myannottool.org','test add annotation')
+      context "with an updated annotation " do
+        setup do
+          @oac_identifier.update_annotation(@test_uri1,[@test_tb2],[@test_tb2],@test_title,@creator_uri,nil,'test update annotation')
+        end
+        
+        should "have the updated annotation" do
+          assert @oac_identifier.has_target?(@test_tb2,@creator_uri)
+        end
+        
+        should "not have the old annotation" do
+          assert !@oac_identifier.has_target?(@test_tb1,@creator_uri)
+        end   
       end
       
-      should "not allow update from nil agent" do
-        orig =  @oac_identifier.get_annotation(@test_uri2)
-        update = @oac_identifier.get_annotation(@test_uri1)
-        assert ! @oac_identifier.can_update?(orig,update)
+      context "with an external agent " do
+        setup do 
+          @oac_identifier.add_annotation(@test_uri2,[@test_tb2],[@test_tb1],@test_title,@creator_uri,'http://myannottool.org','test add annotation')
+        end
+        
+        should "not allow update from nil agent" do
+          orig =  @oac_identifier.get_annotation(@test_uri2)
+          update = @oac_identifier.get_annotation(@test_uri1)
+          assert ! @oac_identifier.can_update?(orig,update)
+        end
       end
     end
   end
