35042bd7c01a14ba36323bcb2e24be33494cf344
Ryan Baumann <ryan.baumann@gmail.com>
2015-08-03 13:25:48 -0400

    Remove unused Grit-based Repository#get_all_files_from_path_on_branch and Repository#recurse_git_tree (see #81)

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -129,24 +129,6 @@ class Repository
     return get_blob_from_branch(file, branch)
   end
 
-  def get_all_files_from_path_on_branch(path = '', branch = 'master')
-    root_tree = @repo.tree(branch, [path]).contents.first
-    return recurse_git_tree(root_tree, [path])
-  end
-
-  def recurse_git_tree(tree, path)
-    files = []
-    tree.blobs.each do |blob|
-      files << File.join(path, blob.name)
-    end
-    tree.trees.each do |this_tree|
-      path.push(this_tree.name)
-      files += recurse_git_tree(this_tree, path)
-      path.pop
-    end
-    return files
-  end
-
   def get_log_for_file_from_branch(file, branch = 'master', limit = 1)
     `#{git_command_prefix} log -n #{limit} --follow --pretty=format:%s #{Shellwords.escape(branch)} -- #{Shellwords.escape(file)}`
   end
