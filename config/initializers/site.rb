e091bec0bf7bc5a0e0027e8f139e659dafa5ce99
Bridget Almas <balmas@gmail.com>
2015-08-05 15:24:01 -0400

    move site config into initializer this is the base papyri config perseids site config is in private_config_repo to be copied in at deploy time

--- config/initializers/site.rb
+++ config/initializers/site.rb
@@ -1,13 +1,14 @@
 # This is where we configure site specific details
 Sosol::Application.configure do
+  config.site_layout = 'pn'
   config.site_identifiers = 'DDBIdentifier,HGVMetaIdentifier,HGVTransIdentifier,BiblioIdentifier,APISIdentifier'
   config.site_name = 'SoSOL'
   config.site_full_name = 'Son of Suda On Line'
   config.site_wiki_link = 'the <a href="http://idp.atlantides.org/trac/idp/wiki">Integrating Digital Papyrology wiki</a>'
   config.site_catalog_search = 'View in PN'
   config.site_email_from = 'admin@localhost'
-  config.site_tag_line = 'powered by Son of Suda Online'
-  config.site_user_namespace = 'http://data.perseus.org/users/'
-  config.site_oac_namespace = 'http://data.perseus.org/annotations/sosoloacprototype'
-  config.site_cite_collection_namespace ='http://data.perseus.org/collections'
+  config.site_tag_line = ''
+  config.site_user_namespace = 'http://papyri.info'
+  config.site_oac_namespace = ''
+  config.site_cite_collection_namespace =''
 end
