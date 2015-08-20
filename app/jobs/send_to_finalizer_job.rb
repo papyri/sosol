207bec9f372a3041d3243a5bc72bff08f3b283f2
Ryan Baumann <ryan.baumann@gmail.com>
2015-08-20 14:19:47 -0400

    More robust job logging
    (cherry picked from commit afc1159f5ff9d8603ed9e62de0e5e633ff42560d)

--- app/jobs/send_to_finalizer_job.rb
+++ app/jobs/send_to_finalizer_job.rb
@@ -2,17 +2,20 @@ class SendToFinalizerJob
   include SuckerPunch::Job
 
   def perform(publication_id, user_id = nil)
-    Rails.logger.debug('SendToFinalizerJob started')
-    Rails.logger.flush if Rails.logger.respond_to? :flush
-    ActiveRecord::Base.connection_pool.clear_reloadable_connections!
-    ActiveRecord::Base.connection_pool.with_connection do
-      publication = Publication.find(publication_id)
-      publication.with_lock do
-        user = User.find(user_id) unless user_id.nil?
-        publication.send_to_finalizer(user)
+    begin
+      Rails.logger.debug("SendToFinalizerJob started (publication_id: #{publication_id} user_id: #{user_id})")
+      Rails.logger.flush if Rails.logger.respond_to? :flush
+      ActiveRecord::Base.connection_pool.clear_reloadable_connections!
+      ActiveRecord::Base.connection_pool.with_connection do
+        publication = Publication.find(publication_id)
+        publication.with_lock do
+          user = User.find(user_id) unless user_id.nil?
+          publication.send_to_finalizer(user)
+        end
       end
+    ensure
+      Rails.logger.debug("SendToFinalizerJob finished (publication_id: #{publication_id} user_id: #{user_id})")
+      Rails.logger.flush if Rails.logger.respond_to? :flush
     end
-    Rails.logger.debug('SendToFinalizerJob finished')
-    Rails.logger.flush if Rails.logger.respond_to? :flush
   end
 end
--- app/jobs/tally_votes_job.rb
+++ app/jobs/tally_votes_job.rb
@@ -2,16 +2,19 @@ class TallyVotesJob
   include SuckerPunch::Job
 
   def perform(publication_id)
-    Rails.logger.debug('TallyVotesJob started')
-    Rails.logger.flush if Rails.logger.respond_to? :flush
-    ActiveRecord::Base.connection_pool.clear_reloadable_connections!
-    ActiveRecord::Base.connection_pool.with_connection do
-      publication = Publication.find(publication_id)
-      publication.with_lock do
-        publication.tally_votes()
+    begin
+      Rails.logger.debug("TallyVotesJob started (publication_id: #{publication_id})")
+      Rails.logger.flush if Rails.logger.respond_to? :flush
+      ActiveRecord::Base.connection_pool.clear_reloadable_connections!
+      ActiveRecord::Base.connection_pool.with_connection do
+        publication = Publication.find(publication_id)
+        publication.with_lock do
+          publication.tally_votes()
+        end
       end
+    ensure
+      Rails.logger.debug("TallyVotesJob finished (publication_id: #{publication_id})")
+      Rails.logger.flush if Rails.logger.respond_to? :flush
     end
-    Rails.logger.debug('TallyVotesJob finished')
-    Rails.logger.flush if Rails.logger.respond_to? :flush
   end
 end
