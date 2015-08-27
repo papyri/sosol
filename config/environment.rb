affb155c0d0147e16c6598739274081834c1c322
Bridget Almas <balmas@gmail.com>
2015-08-26 20:39:54 -0400

    disabling parsing of xml params

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,10 @@ require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
+# See https://groups.google.com/forum/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
+# Disabling because Rails 3 seems to be parsing the xml of posted treebank
+# data into JSON which takes a long time for large files
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
 # Initialize the rails application
 Sosol::Application.initialize!
