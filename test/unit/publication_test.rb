669ec80a25e93495ed8ddd20390b1cbb233094b0
Ryan Baumann <ryan.baumann@gmail.com>
2015-11-20 11:39:42 -0500

    Wrap publication destroy in nil-ity check

--- test/unit/publication_test.rb
+++ test/unit/publication_test.rb
@@ -47,7 +47,7 @@ class PublicationTest < ActiveSupport::TestCase
     end
 
     teardown do
-      @publication.destroy unless !Publication.exists?(@publication.id)
+      @publication.destroy unless (@publication.nil? || !Publication.exists?(@publication.id))
       @user.destroy
     end
     
@@ -140,7 +140,7 @@ class PublicationTest < ActiveSupport::TestCase
     end
     
     teardown do
-      @publication.destroy unless !Publication.exists? @publication.id
+      @publication.destroy unless (@publication.nil? || !Publication.exists?(@publication.id))
       @user.destroy
     end
     
