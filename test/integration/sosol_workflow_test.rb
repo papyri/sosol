170406fc2309e4e87fd397f170b563b492bbd798
Ryan Baumann <ryan.baumann@gmail.com>
2016-01-12 14:51:45 -0500

    Rescue RecordNotFound in MMF race bubbling up from session post (see #142)
    (cherry picked from commit 19dafe8b55ce679fb13f686c88de26e5b3126199)

--- test/integration/sosol_workflow_test.rb
+++ test/integration/sosol_workflow_test.rb
@@ -588,7 +588,11 @@ class SosolWorkflowTest < ActionController::IntegrationTest
                 ActiveRecord::Base.connection_pool.clear_reloadable_connections!
                 ActiveRecord::Base.connection_pool.with_connection do |conn|
                   open_session do |make_me_finalizer_session|
-                    make_me_finalizer_session.post 'publications/' + mmf_publication_id + '/become_finalizer?test_user_id=' + different_finalizer
+                    begin
+                      make_me_finalizer_session.post 'publications/' + mmf_publication_id + '/become_finalizer?test_user_id=' + different_finalizer
+                    rescue ActiveRecord::RecordNotFound => e
+                      Rails.logger.info('RecordNotFound inside MMF thread 1')
+                    end
                   end
                 end
               ensure
@@ -605,7 +609,11 @@ class SosolWorkflowTest < ActionController::IntegrationTest
                 ActiveRecord::Base.connection_pool.clear_reloadable_connections!
                 ActiveRecord::Base.connection_pool.with_connection do |conn|
                   open_session do |make_me_finalizer_session|
-                    make_me_finalizer_session.post 'publications/' + mmf_publication_id + '/become_finalizer?test_user_id=' + different_finalizer_2
+                    begin
+                      make_me_finalizer_session.post 'publications/' + mmf_publication_id + '/become_finalizer?test_user_id=' + different_finalizer_2
+                    rescue ActiveRecord::RecordNotFound => e
+                      Rails.logger.info('RecordNotFound inside MMF thread 2')
+                    end
                   end
                 end
               ensure
