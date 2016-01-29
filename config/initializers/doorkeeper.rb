1cf041780a331d14c3998b86cb1b486e7ad15e6b
Bridget Almas <balmas@gmail.com>
2016-01-29 15:21:16 -0500

    remove commented out line

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,7 +2,6 @@ Doorkeeper.configure do
   default_scopes :read
   optional_scopes :write
 
-  #force_ssl_in_redirect_uri !Rails.env.development?
   force_ssl_in_redirect_uri !Rails.env.development?
 
   resource_owner_authenticator do
