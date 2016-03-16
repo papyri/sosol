2cab8afec8ad43fd693e91f874745b064da083e5
Ryan Baumann <ryan.baumann@gmail.com>
2016-03-16 11:02:18 -0400

    Use SuckerPunch 1.x exception handler API until we upgrade to 2.x and Ruby 2.x+ compat

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -1,1 +1,4 @@
-SuckerPunch.exception_handler = ->(ex, klass, args) { Airbrake.notify(ex) }
+# SuckerPunch 2.x
+# SuckerPunch.exception_handler = ->(ex, klass, args) { Airbrake.notify(ex) }
+# SuckerPunch 1.x
+SuckerPunch.exception_handler { |ex| Airbrake.notify(ex) }
