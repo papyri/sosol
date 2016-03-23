c03e8293cb04c3ccb5fadeec1d42b93665c6eb10
Bridget Almas <balmas@gmail.com>
2016-03-23 12:32:36 -0400

    fixing site.rb for tests

--- config/initializers/site.rb
+++ config/initializers/site.rb
@@ -1,7 +1,6 @@
 # This is where we configure site specific details
 Sosol::Application.configure do
   config.site_layout = 'pn'
-  config.site_identifiers = 'DDBIdentifier,HGVMetaIdentifier,HGVTransIdentifier,BiblioIdentifier,APISIdentifier'
   config.site_name = 'SoSOL'
   config.site_full_name = 'Son of Suda On Line'
   config.site_wiki_link = 'the <a href="http://idp.atlantides.org/trac/idp/wiki">Integrating Digital Papyrology wiki</a>'
@@ -10,6 +9,6 @@ Sosol::Application.configure do
   config.site_tag_line = ''
   config.site_user_namespace = 'http://papyri.info'
   config.site_oac_namespace = ''
-  config.site_cite_collection_namespace =''
+  config.site_cite_collection_namespace ='http://data.perseus.org/collections'
   config.current_terms_version = 0
 end
