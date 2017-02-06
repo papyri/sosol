6fab66b19837f9c2ec3ba605613f3ce0928d7011
Bridget Almas <balmas@gmail.com>
2017-02-05 20:48:25 -0500

    update numbers server test

--- test/unit/numbers_rdf_test.rb
+++ test/unit/numbers_rdf_test.rb
@@ -9,7 +9,7 @@ class NumbersRDFTest < ActiveSupport::TestCase
 
   should "respond to an identifier request with the correct related identifiers" do
     response = NumbersRDF::NumbersHelper.identifier_to_identifiers('papyri.info/ddbdp/p.nyu;1;1')
-    assert_equal ["papyri.info/ddbdp/p.nyu;1;1", "www.trismegistos.org/text/12481", "papyri.info/trismegistos/12481", "papyri.info/hgv/12481", "papyri.info/apis/nyu.apis.4782"], response
+    assert_equal ["papyri.info/ddbdp/p.nyu;1;1", "papyri.info/apis/nyu.apis.4782","papyri.info/hgv/12481", "papyri.info/trismegistos/12481", "www.trismegistos.org/text/12481", "papyri.info/apis/nyu.apis.4782/images"], response
   end
 
   should "raise the appropriate timeout error" do
