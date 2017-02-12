3e64006275addeedc48e93b325a7f8a8154c5e85
Bridget Almas <balmas@gmail.com>
2017-02-12 16:33:07 -0500

    fix rule migration

--- db/migrate/20170207000340_create_rules.rb
+++ db/migrate/20170207000340_create_rules.rb
@@ -1,8 +1,8 @@
 class CreateRules < ActiveRecord::Migration
   def change
     create_table :rules do |t|
-      t.int :expire_days
-      t.int :floor
+      t.integer :expire_days
+      t.decimal :floor,      :precision => 5, :scale => 2
       t.references :decree
 
       t.timestamps
