d969849cc364abe82dd6524f6f6356fcd82868ef
Bridget Almas <balmas@gmail.com>
2017-07-24 11:08:14 -0400

    fix logic in site helper tests

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -2,19 +2,19 @@
 module SiteHelper
 
   def self.show_community_pubs?
-    if defined?(Sosol::Application.config.site_show_community_pubs) && Sosol::Application.config.site_show_community_pubs
+    ! defined?(Sosol::Application.config.site_show_community_pubs) || Sosol::Application.config.site_show_community_pubs
   end
 
   def self.show_all_pubs?
-    if defined?(Sosol::Application.config.site_show_assigned_pubs) && Sosol::Application.config.site_show_assigned_pubs
+    ! defined?(Sosol::Application.config.site_show_assigned_pubs) || Sosol::Application.config.site_show_assigned_pubs
   end
 
   def self.show_events?
-    if defined?(Sosol::Application.config.site_show_events) && Sosol::Application.config.site_show_community_events
+    ! defined?(Sosol::Application.config.site_show_events) || Sosol::Application.config.site_show_community_events
   end
 
   def self.keep_comments?
-    if defined?(Sosol::Application.config.site_keep_comments) && Sosol::Application.config.site_keep_comments
+    ! defined?(Sosol::Application.config.site_keep_comments) || Sosol::Application.config.site_keep_comments
   end
 
 end
