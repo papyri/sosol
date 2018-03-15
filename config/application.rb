53ddfbee3975694c501d7df487657d8c539f0945
Elemmire <elemmire@ossiriand.de>
2018-03-15 14:28:07 +0100

    raise priority of DCLPMetaIdentifier and DCLPTextIdentifier so that they prevail over HGVTransIdentifier in the process of calling up an existing DCLP record that comes along with a translation

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Sosol
 
     # Configure custom application parameters
     config.site_layout = 'pn'
-    config.site_identifiers = 'DDBIdentifier,HGVMetaIdentifier,HGVTransIdentifier,BiblioIdentifier,APISIdentifier,DCLPMetaIdentifier,DCLPTextIdentifier'
+    config.site_identifiers = 'DDBIdentifier,HGVMetaIdentifier,DCLPMetaIdentifier,DCLPTextIdentifier,HGVTransIdentifier,BiblioIdentifier,APISIdentifier'
     config.repository_root = File.join(::Rails.root.to_s, 'db', 'git')
     config.canonical_repository = File.join(config.repository_root, 'canonical.git')
     config.rpx_base_url = 'https://rpxnow.com'
