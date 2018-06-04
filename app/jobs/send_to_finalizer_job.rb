90778118abfa7a8f2250b1aed1350e056ca46c94
Ryan Baumann <ryan.baumann@gmail.com>
2018-06-04 15:01:39 -0400

    Force tally_votes to finish before starting become_finalizer

--- app/jobs/send_to_finalizer_job.rb
+++ app/jobs/send_to_finalizer_job.rb
@@ -8,7 +8,12 @@ class SendToFinalizerJob
       ActiveRecord::Base.connection_pool.clear_reloadable_connections!
       ActiveRecord::Base.connection_pool.with_connection do
         publication = Publication.find(publication_id)
+        publication.with_advisory_lock("tally_votes_#{publication_id}") do
+          # this just creates a hard barrier so that the tally votes rename doesn't race with
+          # publication.send_to_finalizer
+        end
         publication.with_advisory_lock("become_finalizer_#{publication_id}") do
+          publication.reload
           publication.transaction do
             user = User.find(user_id) unless user_id.nil?
             publication.send_to_finalizer(user)
