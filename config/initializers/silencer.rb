68c044773c6129ec323df7f96090cc3427fdf8b3
Ryan Baumann <ryan.baumann@gmail.com>
2018-06-11 15:46:51 -0400

    Namespace silencer into Sosol::Application

--- config/initializers/silencer.rb
+++ config/initializers/silencer.rb
@@ -1,5 +1,3 @@
 require 'silencer/logger'
 
-Rails.application.configure do
-  config.middleware.swap Rails::Rack::Logger, Silencer::Logger, :silence => ["/editor/user/info", "/editor/user/info.json", "/user/info", "/user/info.json"]
-end
+Sosol::Application.config.middleware.swap Rails::Rack::Logger, Silencer::Logger, :silence => ["/editor/user/info", "/editor/user/info.json", "/user/info", "/user/info.json"]
