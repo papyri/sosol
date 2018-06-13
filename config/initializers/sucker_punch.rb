f693fd66b72f617f21fe53e3bc7ea3d88befe263
Ryan Baumann <ryan.baumann@gmail.com>
2018-06-13 11:25:14 -0400

    Update SuckerPunch initializer for SuckerPunch 2.x under Rails < 5.0.0

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -1,4 +1,6 @@
+require 'sucker_punch/async_syntax'
+
 # SuckerPunch 2.x
-# SuckerPunch.exception_handler = ->(ex, klass, args) { Airbrake.notify(ex) }
+SuckerPunch.exception_handler = ->(ex, klass, args) { Airbrake.notify(ex) }
 # SuckerPunch 1.x
-SuckerPunch.exception_handler { |ex| Airbrake.notify(ex) }
+# SuckerPunch.exception_handler { |ex| Airbrake.notify(ex) }
