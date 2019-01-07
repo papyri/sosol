52a0a52d47cf20990f3d10edf8d5afa8e63c8877
Ryan Baumann <ryan.baumann@gmail.com>
2019-01-07 17:05:31 -0500

    Normalize collection name in hash lookup

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -178,10 +178,8 @@ class Identifier < ActiveRecord::Base
       if (self.class == DDBIdentifier) || (self.name =~ /#{self.class::TEMPORARY_COLLECTION}/)
         collection_name, volume_number, document_number =
           self.to_components.last.split(';')
-        unless collection_name == self.class::TEMPORARY_COLLECTION
-          collection_name =
-            self.class.collection_names_hash[collection_name]
-        end
+        collection_name =
+          self.class.collection_names_hash[collection_name.downcase]
 
         # strip leading zeros
         document_number.sub!(/^0*/,'')
