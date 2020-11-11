48211d7e06cca1b775aee60a40b1af8b48a7979a
Ryan Baumann <ryan.baumann@gmail.com>
2020-11-11 15:19:01 -0500

    jruby --2.0 ignored in jruby 9.x

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 # workaround for puppet permissions on dev
 chmod a+rx ~
 
-# make sure we can run jruby --2.0 as tomcat before doing anything
-sudo -u tomcat jruby --2.0 --version
+# make sure we can run jruby as tomcat before doing anything
+sudo -u tomcat jruby --version
 if [ $? -eq 0 ]; then
-sudo -u tomcat JRUBY_OPTS="-J-Xmx4g --2.0" jruby --2.0 -S bundle install
-sudo -u tomcat JRUBY_OPTS="--2.0" jruby --2.0 -S bundle exec cap local externals:setup
-sudo -u tomcat JRUBY_OPTS="--2.0" jruby --2.0 -S bundle exec rake db:migrate RAILS_ENV="production"
+sudo -u tomcat JRUBY_OPTS="-J-Xmx4g" jruby -S bundle install
+sudo -u tomcat jruby -S bundle exec cap local externals:setup
+sudo -u tomcat jruby -S bundle exec rake db:migrate RAILS_ENV="production"
 # Only needed for asset pipeline in future versions of Rails:
-# sudo -u tomcat jruby --2.0 -S bundle exec rake assets:precompile RAILS_ENV=production JRUBY_OPTS="--2.0"
-sudo -u tomcat JRUBY_OPTS="--2.0" jruby --2.0 -S bundle exec warble war
+# sudo -u tomcat jruby -S bundle exec rake assets:precompile RAILS_ENV=production
+sudo -u tomcat jruby -S bundle exec warble war
 BACKUP_WAR="editor.war.bak$(date +"%Y%m%d.%H.%M.%S")"
 sudo cp -v /usr/local/tomcat-sosol/webapps/editor.war ${BACKUP_WAR}
 sudo ln -sfv ${BACKUP_WAR} editor.war.previous
