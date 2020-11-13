b080934337fb15d822ba8636c036320d93212e8d
Ryan Baumann <ryan.baumann@gmail.com>
2020-11-13 09:37:43 -0500

    Use HTTPS login returns on production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def rpx_signin_url(signin_method='signin')
-    dest = url_for :controller => :rpx, :action => :login_return, :only_path => false
+    if ENV['RAILS_ENV'] == 'production'
+      dest = url_for :controller => :rpx, :action => :login_return, :only_path => false, :protocol => 'https'
+    else
+      dest = url_for :controller => :rpx, :action => :login_return, :only_path => false
+    end
     @rpx.signin_url(dest, signin_method)
   end
 
   def rpx_associate_url(signin_method='signin')
-    dest = url_for :controller => :rpx, :action => :associate_return, :only_path => false
+    if ENV['RAILS_ENV'] == 'production'
+      dest = url_for :controller => :rpx, :action => :associate_return, :only_path => false, :protocol => 'https'
+    else
+      dest = url_for :controller => :rpx, :action => :associate_return, :only_path => false
+    end
     @rpx.signin_url(dest, signin_method)
   end
 
