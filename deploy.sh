6c8f0450f1017afb5e687a93879b2cb7dff25481
Ryan Baumann <ryan.baumann@gmail.com>
2020-12-22 09:26:21 -0500

    Set RAILS_RELATIVE_URL_ROOT in deploy script assets precompilation

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ sudo -u tomcat JRUBY_OPTS="-J-Xmx4g" jruby -S bundle install
 sudo -u tomcat jruby -S bundle exec cap local externals:setup
 sudo -u tomcat jruby -S bundle exec rake db:migrate RAILS_ENV="production"
 # Needed for asset pipeline:
-sudo -u tomcat jruby -S bundle exec rake assets:precompile RAILS_ENV=production
+sudo -u tomcat jruby -S bundle exec rake assets:precompile RAILS_ENV=production RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS=assets
 sudo -u tomcat jruby -S bundle exec warble war
 BACKUP_WAR="editor.war.bak$(date +"%Y%m%d.%H.%M.%S")"
 sudo cp -v /usr/local/tomcat-sosol/webapps/editor.war ${BACKUP_WAR}
