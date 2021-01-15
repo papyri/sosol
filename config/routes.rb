ea603860518e3f1f838448e4d6cb42e4432cf5c8
Ryan Baumann <ryan.baumann@gmail.com>
2021-01-15 14:40:32 -0500

    Update match routes

--- config/routes.rb
+++ config/routes.rb
@@ -65,14 +65,14 @@ Sosol::Application.routes.draw do
 
   resources :events
   resource :session
-  match 'help' => 'user#help', :as => :help
-  match 'usage' => 'user#usage_stats', :as => :usage
-  match 'all_users_links' => 'user#all_users_links', :as => :all_users_links
-  match 'index_user_admins' => 'user#index_user_admins', :as => :index_user_admins
-  match 'dashboard' => 'user#dashboard', :as => :dashboard
-  match 'developer' => 'user#developer', :as => :developer
-  match 'sendmsg' => 'user#create_email_everybody', :as => :sendmsg
-  match 'master_list' => 'publications#master_list', :as => :master_list
+  match 'help' => 'user#help', :as => :help, :via => :get
+  match 'usage' => 'user#usage_stats', :as => :usage, :via => :get
+  match 'all_users_links' => 'user#all_users_links', :as => :all_users_links, :via => :get
+  match 'index_user_admins' => 'user#index_user_admins', :as => :index_user_admins, :via => :get
+  match 'dashboard' => 'user#dashboard', :as => :dashboard, :via => :get
+  match 'developer' => 'user#developer', :as => :developer, :via => :get
+  match 'sendmsg' => 'user#create_email_everybody', :as => :sendmsg, :via => :get
+  match 'master_list' => 'publications#master_list', :as => :master_list, :via => :get
   match 'publications/archive_all' => 'publications#archive_all', :via => :post
   resources :publications do
     collection do
@@ -355,30 +355,30 @@ Sosol::Application.routes.draw do
   end
 
   match 'documentation' => redirect('http://papyri.info/docs/leiden_plus')
-  match 'users/:user_name' => 'user#show', :user_name => /[^\/]*/
-  match 'peep_user_dashboard/:user_id(/:publication)' => 'user#peep_user_dashboard', :user_id => /\d+/, :publication => /(submitted|editing|new|committed|finalizing|\d+)/
-  match 'editor/user/info' => 'user#info'
-  match 'publications/:publication_id/:controller/:id/show_commit/:commit_id' => '(?-mix:.*_?identifiers)#show_commit', :commit_id => /[0-9a-fA-F]{40}/
-  match 'publications/create_from_identifier/:id' => 'publications#create_from_identifier', :id => /papyri\.info.*/
-  match 'cts_publications/create_from_linked_urn/:urn' => 'cts_publications#create_from_linked_urn', :urn => /[^\/]*/
-  match 'js/:query' => 'ajax_proxy#js', :query => /.*/
-  match 'css/:query' => 'ajax_proxy#css', :query => /.*/
-  match 'images/:query' => 'ajax_proxy#images', :query => /.*/
-  match 'mulgara/sparql/:query' => 'ajax_proxy#sparql', :query => /.*/
-  match 'ajax_proxy/sparql/:query' => 'ajax_proxy#sparql', :query => /.*/
-  match 'sparql' => 'ajax_proxy#sparql'
+  match 'users/:user_name' => 'user#show', :user_name => /[^\/]*/, :via => :get
+  match 'peep_user_dashboard/:user_id(/:publication)' => 'user#peep_user_dashboard', :user_id => /\d+/, :publication => /(submitted|editing|new|committed|finalizing|\d+)/, :via => :get
+  match 'editor/user/info' => 'user#info', :via => :get
+  match 'publications/:publication_id/:controller/:id/show_commit/:commit_id' => '(?-mix:.*_?identifiers)#show_commit', :commit_id => /[0-9a-fA-F]{40}/, :via => :get
+  match 'publications/create_from_identifier/:id' => 'publications#create_from_identifier', :id => /papyri\.info.*/, :via => :get
+  match 'cts_publications/create_from_linked_urn/:urn' => 'cts_publications#create_from_linked_urn', :urn => /[^\/]*/, :via => :get
+  match 'js/:query' => 'ajax_proxy#js', :query => /.*/, :via => :get
+  match 'css/:query' => 'ajax_proxy#css', :query => /.*/, :via => :get
+  match 'images/:query' => 'ajax_proxy#images', :query => /.*/, :via => :get
+  match 'mulgara/sparql/:query' => 'ajax_proxy#sparql', :query => /.*/, :via => :get
+  match 'ajax_proxy/sparql/:query' => 'ajax_proxy#sparql', :query => /.*/, :via => :get
+  match 'sparql' => 'ajax_proxy#sparql', :via => :get
   match 'ajax_proxy/xsugar/' => 'ajax_proxy#xsugar', :via => :post
   match 'ajax_proxy/hgvnum/' => 'ajax_proxy#hgvnum', :via => :post
-  match 'ajax_proxy/:id' => 'ajax_proxy#proxy', :id => /papyri\.info.*/
-  match 'cts/editions/:inventory' => 'cts_proxy#editions', :inventory => /[^\/]*/
-  match 'cts/translations/:inventory/:urn' => 'cts_proxy#translations', :inventory => /[^\/]*/, :urn => /[^\/]*/
-  match 'cts/citations/:inventory/:urn' => 'cts_proxy#citations', :inventory => /[^\/]*/, :urn => /[^\/]*/
-  match 'cts/getpassage/:id/:urn' => 'cts_proxy#getpassage', :urn => /[^\/]*/
-  match 'cts/getcapabilities/:collection' => 'cts_proxy#getcapabilities'
-  match 'cts/getrepos' => 'cts_proxy#getrepos'
-  match '/' => 'welcome#index'
-  match '/:controller(/:action(/:id))'
-  match 'signout' => 'user#signout', :as => :signout
-  match 'signin' => 'user#signin', :as => :signin
-  match 'account' => 'user#account', :as => :account
+  match 'ajax_proxy/:id' => 'ajax_proxy#proxy', :id => /papyri\.info.*/, :via => :get
+  match 'cts/editions/:inventory' => 'cts_proxy#editions', :inventory => /[^\/]*/, :via => :get
+  match 'cts/translations/:inventory/:urn' => 'cts_proxy#translations', :inventory => /[^\/]*/, :urn => /[^\/]*/, :via => :get
+  match 'cts/citations/:inventory/:urn' => 'cts_proxy#citations', :inventory => /[^\/]*/, :urn => /[^\/]*/, :via => :get
+  match 'cts/getpassage/:id/:urn' => 'cts_proxy#getpassage', :urn => /[^\/]*/, :via => :get
+  match 'cts/getcapabilities/:collection' => 'cts_proxy#getcapabilities', :via => :get
+  match 'cts/getrepos' => 'cts_proxy#getrepos', :via => :get
+  match '/' => 'welcome#index', :via => :get
+  match '/:controller(/:action(/:id))', :via => :get
+  match 'signout' => 'user#signout', :as => :signout, :via => :get
+  match 'signin' => 'user#signin', :as => :signin, :via => :get
+  match 'account' => 'user#account', :as => :account, :via => :get
 end
