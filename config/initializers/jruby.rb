de06c415112ccf5a09e9e667ecba64e76c4df19c
Ryan Baumann <ryan.baumann@gmail.com>
2021-01-21 12:18:30 -0500

    Airbrake signature change

--- config/initializers/jruby.rb
+++ config/initializers/jruby.rb
@@ -12,6 +12,6 @@ if(RUBY_PLATFORM == 'java')
   if RUBY_VERSION.to_f < 2.0
     warning_message = "WARNING: Running in an environment where RUBY_VERSION is < 2.0 (actual value: #{RUBY_VERSION}). This is deprecated and may lead to errors. Set JRUBY_OPTS=\"--2.0\" before invoking."
     Rails.logger.error(warning_message)
-    Airbrake.notify(warning_message)
+    Airbrake[:default].notify(warning_message)
   end
 end
