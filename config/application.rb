e8d3701143a080073274d22900ee96727a3b29a6
Ryan Baumann <ryan.baumann@gmail.com>
2021-01-22 09:43:33 -0500

    Set config.active_record.raise_in_transactional_callbacks = true

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,9 @@ module Sosol
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    # Defaults to true in Rails 5
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
