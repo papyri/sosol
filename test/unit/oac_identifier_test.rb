60a403a437aeec88f43e59458c0cd3d73ab49101
Ryan Baumann <ryan.baumann@gmail.com>
2021-01-22 12:31:42 -0500

    ActionController::Integration => ActionDispatch::Integration (https://github.com/rails/rails/blob/v4.1.15/actionpack/CHANGELOG.md)

--- test/unit/oac_identifier_test.rb
+++ test/unit/oac_identifier_test.rb
@@ -19,8 +19,8 @@ class OACIdentifierTest < ActiveSupport::TestCase
       @test_tb1 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg001.perseus-grc1:1.1'
       @test_tb2 = 'http://data.perseus.org/citation/urn:cts:greekLang:tlg0012.tlg002.perseus-grc1:1.1'
       @test_title = 'Test Annotation'
-      @creator_uri = ActionController::Integration::Session.new(Sosol::Application).url_for(:host => Sosol::Application.config.site_user_namespace, :controller => 'user', :action => 'show', :user_name => @creator.name, :only_path => false)
-      @creator2_uri = ActionController::Integration::Session.new(Sosol::Application).url_for(:host => Sosol::Application.config.site_user_namespace, :controller => 'user', :action => 'show', :user_name => @creator2.name, :only_path => false)
+      @creator_uri = ActionDispatch::Integration::Session.new(Sosol::Application).url_for(:host => Sosol::Application.config.site_user_namespace, :controller => 'user', :action => 'show', :user_name => @creator.name, :only_path => false)
+      @creator2_uri = ActionDispatch::Integration::Session.new(Sosol::Application).url_for(:host => Sosol::Application.config.site_user_namespace, :controller => 'user', :action => 'show', :user_name => @creator2.name, :only_path => false)
       @oac_identifier.add_annotation(@test_uri1,[@test_tb1],@test_tb2,@test_title,@creator_uri,'test add annotation')
     end
     
