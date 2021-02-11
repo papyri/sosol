5c49b63d27109e387b89e2d28a07e72a662fe59c
Ryan Baumann <ryan.baumann@gmail.com>
2021-02-11 12:55:57 -0500

    bundle exec rake app:update

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
-# See: https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#cookies-serializer
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
+Rails.application.config.action_dispatch.cookies_serializer = :json
