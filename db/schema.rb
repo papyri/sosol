1d058cc85d907766d92d7a5a2a4de9dde84bd40c
Ryan Baumann <ryan.baumann@gmail.com>
2021-02-15 14:48:58 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210208190016) do
+ActiveRecord::Schema.define(version: 2021_02_08_190016) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
