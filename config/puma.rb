eb07575de882e0806fc95ef697b2a00cd2fe2e00
Ryan Baumann <ryan.baumann@gmail.com>
2021-03-10 13:50:58 -0500

    Use stdout_redirect for Puma in production

--- config/puma.rb
+++ config/puma.rb
@@ -43,5 +43,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
 
+stdout_redirect "#{Rails.root.to_s}/log/production.log", "#{Rails.root.to_s}/log/production.log", true if Rails.env.production?
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
