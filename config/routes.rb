629d45c6d63991d441b86c6c5c0c05223b79a64a
Ryan Baumann <ryan.baumann@gmail.com>
2021-03-11 15:38:13 -0500

    HgvMetaIdentifiersController get_date_preview and get_geo_preview use POST instead of GET

--- config/routes.rb
+++ config/routes.rb
@@ -148,8 +148,8 @@ Sosol::Application.routes.draw do
     resources :hgv_meta_identifiers do
     
         member do
-    get :get_date_preview
-    get :get_geo_preview
+    post :get_date_preview
+    post :get_geo_preview
     get :history
     get :preview
     get :editxml
