5ee82926af18428d2a3f6c632b4c0d415ed59c8e
Ryan Baumann <ryan.baumann@gmail.com>
2021-03-11 10:14:50 -0500

    Split deploy script into deploy.sh (puma) and deploy-tomcat.sh

--- deploy-tomcat.sh
+++ deploy-tomcat.sh
@@ -12,10 +12,16 @@ if [ $? -eq 0 ]; then
     gem install bundler:${BUNDLER_VERSION}
   fi
   sudo -u tomcat JRUBY_OPTS="-J-Xmx4g" jruby -S bundle install
-  # Needed for asset pipeline:
-  sudo -u tomcat jruby -S bundle exec rake assets:precompile RAILS_ENV=production RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS=assets
-  sudo /etc/init.d/papyri.info stop-tc
   sudo -u tomcat jruby -S bundle exec cap local externals:setup
   sudo -u tomcat jruby -S bundle exec rake db:migrate RAILS_ENV="production"
+  # Needed for asset pipeline:
+  sudo -u tomcat jruby -S bundle exec rake assets:precompile RAILS_ENV=production RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS=assets
+  sudo -u tomcat jruby -S bundle exec warble war
+  BACKUP_WAR="editor.war.bak$(date +"%Y%m%d.%H.%M.%S")"
+  sudo cp -v /usr/local/tomcat-sosol/webapps/editor.war ${BACKUP_WAR}
+  sudo ln -sfv ${BACKUP_WAR} editor.war.previous
+  sudo /etc/init.d/papyri.info stop-tc
+  sudo cp -v editor.war /usr/local/tomcat-sosol/webapps/
+  sudo rm -r /usr/local/tomcat-sosol/webapps/editor
   sudo /etc/init.d/papyri.info start-tc
 fi
