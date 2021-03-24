654ee041b773df6ae513d739ca4085c105ecf92c
Ryan Baumann <ryan.baumann@gmail.com>
2021-03-24 12:56:29 -0400

    Production logger must include ActiveRecord::SessionStore::Extension::LoggerSilencer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Sosol::Application.configure do
   # config.logger = SyslogLogger.new
   # STDOUT used for Tomcat/catalina.out logging
   config.logger = Logger.new(STDOUT)
+  config.logger.send :include, ActiveRecord::SessionStore::Extension::LoggerSilencer
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
