f575e960b11fdbf2f240322b62d6eee208f833b9
Ryan Baumann <ryan.baumann@gmail.com>
2021-03-25 09:54:10 -0400

    Re-enable silencer

--- config/initializers/silencer.rb
+++ config/initializers/silencer.rb
@@ -1,3 +1,3 @@
-# require 'silencer/logger'
+require 'silencer/logger'
 
-# Sosol::Application.config.middleware.swap Rails::Rack::Logger, Silencer::Logger, :silence => ["/editor/user/info", "/editor/user/info.json", "/user/info", "/user/info.json"]
+Sosol::Application.config.middleware.swap Rails::Rack::Logger, Silencer::Logger, :silence => ["/editor/user/info", "/editor/user/info.json", "/user/info", "/user/info.json"]
