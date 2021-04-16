6b0cf20253000628dd41fea291e568a8f194245e
Ryan Baumann <ryan.baumann@gmail.com>
2021-04-16 13:35:28 -0400

    Add omniauth callbacks to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Sosol::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :communities do
   
     member do
