761a3953560e87e4d0838facb8c17cf6988412d7
Ryan Baumann <ryan.baumann@gmail.com>
2021-04-22 14:30:23 -0400

    Only set google_oauth2 OmniAuth config if environment variables are set

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,5 +309,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
+  if ENV['GOOGLE_CLIENT_ID'].present? && ENV['GOOGLE_CLIENT_SECRET'].present?
+    config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
+  end
 end
