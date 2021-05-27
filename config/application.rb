846d37910708ef82f80bab82e0398ce96846c7be
Ryan Baumann <ryan.baumann@gmail.com>
2021-05-27 15:38:44 -0400

    Production Rails 5+ needs lib on eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Sosol
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += Dir["#{config.root}/lib"]
+    config.eager_load_paths << Rails.root.join('lib')
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
