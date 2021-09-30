5128552ddd580db97f7b2bf233381c640784807e
Ryan Baumann <ryan.baumann@gmail.com>
2021-09-30 11:38:33 -0400

    Move Airbrake configuration into environment variables (fixes #288)

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,8 @@
+if defined?(Airbrake) && ENV['AIRBRAKE_HOST'].present? && ENV['AIRBRAKE_PROJECT_KEY'].present?
+  Airbrake.configure do |config|
+    config.host = ENV['AIRBRAKE_HOST']
+    config.project_id = 1
+    config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+    config.environment = Rails.env
+  end
+end
