e36ba7d7b32e2e6cc48f232e83360f5e8c63cb3a
Ryan Baumann <ryan.baumann@gmail.com>
2021-10-04 10:37:18 -0400

    Add explicit cast

--- db/migrate/20211004142258_change_emailer_id_to_be_integer_in_emailers_users.rb
+++ db/migrate/20211004142258_change_emailer_id_to_be_integer_in_emailers_users.rb
@@ -1,9 +1,9 @@
 class ChangeEmailerIdToBeIntegerInEmailersUsers < ActiveRecord::Migration[5.2]
   def up
-    change_column :emailers_users, :emailer_id, :integer
+    change_column :emailers_users, :emailer_id, "integer USING NULLIF(emailer_id, '')::int"
   end
 
   def down
-    change_column :emailers_users, :emailer_id, :string
+    change_column :emailers_users, :emailer_id, "string USING NULLIF(emailer_id, '')::string"
   end
 end
