44012dbab9a5fece786dda28f6b2c0159acd9d5e
Ryan Baumann <ryan.baumann@gmail.com>
2021-10-04 10:50:47 -0400

    Guard Postgres-specific cast

--- db/migrate/20211004142258_change_emailer_id_to_be_integer_in_emailers_users.rb
+++ db/migrate/20211004142258_change_emailer_id_to_be_integer_in_emailers_users.rb
@@ -1,9 +1,17 @@
 class ChangeEmailerIdToBeIntegerInEmailersUsers < ActiveRecord::Migration[5.2]
   def up
-    change_column :emailers_users, :emailer_id, "integer USING NULLIF(emailer_id, '')::int"
+    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      change_column :emailers_users, :emailer_id, "integer USING NULLIF(emailer_id, '')::int"
+    else
+      change_column :emailers_users, :emailer_id, :integer
+    end
   end
 
   def down
-    change_column :emailers_users, :emailer_id, "string USING NULLIF(emailer_id, '')::string"
+    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      change_column :emailers_users, :emailer_id, "string USING NULLIF(emailer_id, '')::string"
+    else
+      change_column :emailers_users, :emailer_id, :string
+    end
   end
 end
