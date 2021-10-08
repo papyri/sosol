a8c26b3600bf6b4e41ef5234ffaf08794123130c
Ryan Baumann <ryan.baumann@gmail.com>
2021-10-08 10:59:03 -0400

    Update deploy.sh for new production: pass -i to sudo, let ansible handle jruby/bundler version, use systemctl for editor process control

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-BUNDLER_VERSION=$(fgrep -A1 'BUNDLED WITH' Gemfile.lock | tail -1 | sed -e 's/^ *//')
+# BUNDLER_VERSION=$(fgrep -A1 'BUNDLED WITH' Gemfile.lock | tail -1 | sed -e 's/^ *//')
 EDITOR_USER="papyri"
 
-# make sure we can run jruby as $EDITOR_USER before doing anything
-rbenv install -s && sudo -u $EDITOR_USER jruby --version
+# make sure we can run jruby/bundle as $EDITOR_USER before doing anything
+sudo -u $EDITOR_USER -i jruby --version && sudo -u $EDITOR_USER -i bundle --version
 if [ $? -eq 0 ]; then
-  if ! command -v bundle || ! bundle --version | fgrep ${BUNDLER_VERSION}; then
-    gem install bundler:${BUNDLER_VERSION}
-  fi
-  sudo -u $EDITOR_USER JRUBY_OPTS="-J-Xmx4g" jruby -S bundle install
+  sudo -u $EDITOR_USER -i bundle install --deployment
   # Needed for asset pipeline:
-  sudo -u $EDITOR_USER jruby -S bundle exec rake assets:precompile RAILS_ENV=production RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS=assets
-  sudo /etc/init.d/papyri.info stop-tc
-  sudo -u $EDITOR_USER jruby -S bundle exec cap local externals:setup
-  sudo -u $EDITOR_USER jruby -S bundle exec rake db:migrate RAILS_ENV="production"
-  sudo /etc/init.d/papyri.info start-tc
+  sudo -u $EDITOR_USER -i bundle exec rake assets:precompile RAILS_ENV="production" RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS="assets"
+  sudo systemctl stop papyri.editor
+  sudo -u $EDITOR_USER -i bundle exec cap local externals:setup
+  sudo -u $EDITOR_USER -i bundle exec rake db:migrate RAILS_ENV="production"
+  sudo systemctl start papyri.editor
 fi
