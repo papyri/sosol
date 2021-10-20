ed33df500aad4d49e55eff2b32f19dd7b33f5862
Ryan Baumann <ryan.baumann@gmail.com>
2021-10-20 17:53:43 -0400

    Switch editor user back to non-interactive tomcat user

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # BUNDLER_VERSION=$(fgrep -A1 'BUNDLED WITH' Gemfile.lock | tail -1 | sed -e 's/^ *//')
-EDITOR_USER="papyri"
+EDITOR_USER="tomcat"
+JRUBY="/usr/local/jruby/bin/jruby"
 
 # make sure we can run jruby/bundle as $EDITOR_USER before doing anything
-sudo -u $EDITOR_USER -i jruby --version && sudo -u $EDITOR_USER -i bundle --version
+sudo -u $EDITOR_USER $JRUBY --version && sudo -u $EDITOR_USER $JRUBY -S bundle --version
 if [ $? -eq 0 ]; then
-  sudo -u $EDITOR_USER -i bundle install --deployment
+  sudo -u $EDITOR_USER $JRUBY -S bundle install --deployment
   # Needed for asset pipeline:
-  sudo -u $EDITOR_USER -i bundle exec rake assets:precompile RAILS_ENV="production" RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS="assets"
-  sudo -u $EDITOR_USER -i bundle exec cap local externals:setup
-  sudo -u $EDITOR_USER -i bundle exec rake db:migrate RAILS_ENV="production"
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec rake assets:precompile RAILS_ENV="production" RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS="assets"
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec cap local externals:setup
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec rake db:migrate RAILS_ENV="production"
   sudo systemctl restart papyri.editor
 fi
