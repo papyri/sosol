446b70dbe2a1f6dd3c9ec04bf8dd3536bb27845e
Ryan Baumann <ryan.baumann@gmail.com>
2021-12-09 17:46:53 -0500

    Revert "Use jruby from PATH"
    This reverts commit c072ed5f4b18ec50b46921adc704e51c8d4421ce.

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,15 @@
 
 # BUNDLER_VERSION=$(fgrep -A1 'BUNDLED WITH' Gemfile.lock | tail -1 | sed -e 's/^ *//')
 EDITOR_USER="tomcat"
+JRUBY="/usr/local/jruby/bin/jruby"
 
 # make sure we can run jruby/bundle as $EDITOR_USER before doing anything
-sudo -u $EDITOR_USER jruby --version && sudo -u $EDITOR_USER jruby -S bundle --version
+sudo -u $EDITOR_USER $JRUBY --version && sudo -u $EDITOR_USER $JRUBY -S bundle --version
 if [ $? -eq 0 ]; then
-  sudo -u $EDITOR_USER jruby -S bundle install --deployment
+  sudo -u $EDITOR_USER $JRUBY -S bundle install --deployment
   # Needed for asset pipeline:
-  sudo -u $EDITOR_USER jruby -S bundle exec rake assets:precompile RAILS_ENV="production" RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS="assets"
-  sudo -u $EDITOR_USER jruby -S bundle exec cap local externals:setup
-  sudo -u $EDITOR_USER jruby -S bundle exec rake db:migrate RAILS_ENV="production"
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec rake assets:precompile RAILS_ENV="production" RAILS_RELATIVE_URL_ROOT='/editor' RAILS_GROUPS="assets"
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec cap local externals:setup
+  sudo -u $EDITOR_USER $JRUBY -S bundle exec rake db:migrate RAILS_ENV="production"
   sudo systemctl restart papyri.editor
 fi
