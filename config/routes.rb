f9151c3f4733bc04049ee2da55cadf287e9718ad
Ryan Baumann <ryan.baumann@gmail.com>
2022-03-11 12:35:46 -0500

    bundle add rapporteur

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Sosol::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  mount Rapporteur::Engine, at: '/status'
   resources :communities do
     member do
       get :find_member
