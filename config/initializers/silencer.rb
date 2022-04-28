0f6b600fd0026f7c7cfc90790a72e5377b3a15bc
Ryan Baumann <ryan.baumann@gmail.com>
2022-04-28 16:08:42 -0400

    Update silencer config

--- config/initializers/silencer.rb
+++ config/initializers/silencer.rb
@@ -1,4 +1,4 @@
-require 'silencer/logger'
+require 'silencer/rails/logger'
 
-Sosol::Application.config.middleware.swap Rails::Rack::Logger, Silencer::Logger,
+Sosol::Application.config.middleware.swap Rails::Rack::Logger, Silencer::Rails::Logger,
                                           silence: ['/editor/user/info', '/editor/user/info.json', '/user/info', '/user/info.json']
