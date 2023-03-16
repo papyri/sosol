61d8af263ea793e3d9841a593912368484dabb08
Ryan Baumann <ryan.baumann@gmail.com>
2023-03-16 16:56:57 -0400

    First pass at bundle exec rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_004_142_258) do
+ActiveRecord::Schema[6.1].define(version: 20_211_004_142_258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
