ea65211f1145f0f9fdf28fdfb61338acd94c3588
Ryan Baumann <ryan.baumann@gmail.com>
2023-03-17 15:08:19 -0400

    Add explicit CTS/TEI/OAC inflections

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -20,6 +20,10 @@ Rails.autoloaders.each do |autoloader|
     "dclp_text_identifier_helper" => "DCLPTextIdentifierHelper",
     "apis_identifier" => "APISIdentifier",
     "apis_identifier_helper" => "APISIdentifierHelper",
+    "cts_inventory_identifier" => "CTSInventoryIdentifier",
+    "cts_oac_identifier" => "CTSOACIdentifier",
+    "tei_cts_identifier" => "TEICTSIdentifier",
+    "tei_trans_cts_identifier" => "TEITransCTSIdentifier",
     "numbers_rdf" => "NumbersRDF",
     "jruby_xml" => "JRubyXML",
     "jgit" => "JGit"
