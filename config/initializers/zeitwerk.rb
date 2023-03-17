6b96f020d3db89571d4b0a1df5ff5de49294316e
Ryan Baumann <ryan.baumann@gmail.com>
2023-03-17 11:48:12 -0400

    Add explicit Zeitwerk inflections

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -2,6 +2,9 @@ Rails.autoloaders.each do |autoloader|
   autoloader.inflector = Zeitwerk::Inflector.new
   autoloader.inflector.inflect(
     "jruby" => "JRuby",
-    "cts" => "CTS"
+    "cts" => "CTS",
+    "ddb" => "DDB",
+    "hgv" => "HGV",
+    "dclp" => "DCLP"
   )
 end
