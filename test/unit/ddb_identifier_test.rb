9d3907430cb1bbe208d15d5279ea25b5a05e0a3e
Ryan Baumann <ryan.baumann@gmail.com>
2025-08-08 14:42:19 -0400

    Add debug log from publication branch

--- test/unit/ddb_identifier_test.rb
+++ test/unit/ddb_identifier_test.rb
@@ -33,6 +33,7 @@ class DDBIdentifierTest < ActiveSupport::TestCase
       bgu_1_1 = FactoryBot.build(:DDBIdentifier, name: 'papyri.info/ddbdp/bgu;1;1')
       assert_not_nil bgu_1_1.content
       Rails.logger.info("RENAME CONFLICT TEST: #{bgu_1_1.content.inspect}")
+      Rails.logger.info("RENAME CONFLICT TEST on publication branch: #{@creator.repository.get_file_from_branch(bgu_1_1.to_path, @publication.branch).inspect}")
 
       assert_raise RuntimeError do
         @ddb_identifier.rename('papyri.info/ddbdp/bgu;1;1')
